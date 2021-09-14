1f09f0c222c089791eb791d44d2f135059950ffd
Mikko Ohtamaa <mikko@opensourcehacker.com>
2021-09-14 10:24:02 +0200

    Logo margin madness

--- src/lib/header/Navbar.svelte
+++ src/lib/header/Navbar.svelte
@@ -23,71 +23,77 @@
 </script>
 
 <!-- Navbar -->
-<nav class="navbar navbar-light white scrolling-navbar">
-  <div class="container">
+<div class="container">
+  <nav class="navbar navbar-expand-lg navbar-light white scrolling-navbar">
 
-    <!-- Brand -->
-    <a class="navbar-brand" href="/">
-      <img class="img-logo img-logo-desktop" src={logo} alt="On-chain quantitative finance" />
-      <img class="img-logo img-logo-mobile" src={logoMobile} alt="On-chain quantitative finance" />
-    </a>
 
-    <!-- Collapse -->
-    <button
-            class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-            on:click={clickCollapse}
-      >
-      <span class="navbar-toggler-icon"></span>
-    </button>
+      <!-- Brand -->
+      <a class="navbar-brand" href="/">
+        <img class="img-logo img-logo-desktop" src={logo} alt="On-chain quantitative finance" />
+        <img class="img-logo img-logo-mobile" src={logoMobile} alt="On-chain quantitative finance" />
+      </a>
 
-    <!-- Links -->
-    <div class="{'collapse navbar-collapse ' + (isOpen ? 'show' : '')}" id="navbarSupportedContent">
+      <!-- Collapse -->
+      <button
+              class="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+              on:click={clickCollapse}
+        >
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-      <!-- Left -->
-      <ul class="navbar-nav mr-auto">
+      <!-- Links -->
+      <div class="{'collapse navbar-collapse ' + (isOpen ? 'show' : '')}" id="navbarSupportedContent">
 
+        <!-- Left -->
+        <ul class="navbar-nav mr-auto">
+
+            <li class="nav-item">
+              <a rel="external" class="nav-link waves-effect" href="/datasets">Datasets</a>
+            </li>
+
+            <li class="nav-item">
+              <a rel="external" class="nav-link waves-effect" href="https://tradingstrategy.ai/docs/index.html">Documentation</a>
+            </li>
+
+            <!--
+            <li class="nav-item">
+              <a class="nav-link waves-effect" href="https://capitalgram.com/about">About</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link waves-effect" href="https://capitalgram.com/blog">Blog</a>
+            </li>
+            -->
+        </ul>
+
+        <!-- Right -->
+        <!--
+        <ul class="navbar-nav nav-flex-icons">
           <li class="nav-item">
-            <a rel="external" class="nav-link waves-effect" href="/datasets">Datasets</a>
+            <a href="https://twitter.com/moo9000" class="nav-link waves-effect" target="_blank">
+              <i class="fab fa-twitter"></i> Twitter
+            </a>
           </li>
+        </ul>
+        -->
 
-          <li class="nav-item">
-            <a rel="external" class="nav-link waves-effect" href="https://tradingstrategy.ai/docs/index.html">Documentation</a>
-          </li>
-
-          <!--
-          <li class="nav-item">
-            <a class="nav-link waves-effect" href="https://capitalgram.com/about">About</a>
-          </li>
-
-          <li class="nav-item">
-            <a class="nav-link waves-effect" href="https://capitalgram.com/blog">Blog</a>
-          </li>
-          -->
-      </ul>
-
-      <!-- Right -->
-      <!--
-      <ul class="navbar-nav nav-flex-icons">
-        <li class="nav-item">
-          <a href="https://twitter.com/moo9000" class="nav-link waves-effect" target="_blank">
-            <i class="fab fa-twitter"></i> Twitter
-          </a>
-        </li>
-      </ul>
-      -->
-
-    </div>
-
-  </div>
-</nav>
+      </div>
+  </nav>
+</div>
 
 <style>
+
+    /* Fix left aligment issues */
+    .navbar {
+      padding: 5px;
+    }
+
     .img-logo {
         height: 36px;
     }
