9723ce8caa867a0ab7e5f2cfd063e7c17f26cb3e
Max revolution <maxrevolution8@gmail.com>
2021-09-15 21:05:41 +0200

    Comment out exchange navigation

--- src/lib/header/Navbar.svelte
+++ src/lib/header/Navbar.svelte
@@ -57,9 +57,9 @@
               <a rel="external" class="nav-link" href="/datasets">Datasets</a>
             </li>
 
-            <li class="nav-item">
+            <!-- <li class="nav-item">
               <a rel="external" class="nav-link" href="/exchanges">Exchanges</a>
-            </li>
+            </li> -->
 
             <li class="nav-item">
               <a rel="external" class="nav-link" href="https://tradingstrategy.ai/docs/index.html">Documentation</a>
