ffe416671f3c6dbc5abd64d46292edd6f6cf552a
Mikko Ohtamaa <mikko@opensourcehacker.com>
2021-10-04 21:18:58 +0200

    Updating the beta logo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -142,27 +142,27 @@
 
 	@-webkit-keyframes pulse {
 	  0% {
-		-webkit-box-shadow: 0 0 0 0 rgba(255, 255, 255, 0.4);
+		-webkit-box-shadow: 0 0 0 0 rgba(0, 0, 255, 0.4);
 	  }
 	  70% {
-		  -webkit-box-shadow: 0 0 0 10px rgba(255,255,255, 0);
+		  -webkit-box-shadow: 0 0 0 10px rgba(0,0,255, 0);
 	  }
 	  100% {
-		  -webkit-box-shadow: 0 0 0 0 rgba(255,255,255, 0);
+		  -webkit-box-shadow: 0 0 0 0 rgba(0,0,255, 0);
 	  }
 	}
 	@keyframes pulse {
 	  0% {
-		-moz-box-shadow: 0 0 0 0 rgba(255,255,255, 0.4);
-		box-shadow: 0 0 0 0 rgba(255,255,255, 0.4);
+		-moz-box-shadow: 0 0 0 0 rgba(0,0,255, 0.4);
+		box-shadow: 0 0 0 0 rgba(0,0,255, 0.4);
 	  }
 	  70% {
-		  -moz-box-shadow: 0 0 0 10px rgba(255,255,255, 0);
-		  box-shadow: 0 0 0 10px rgba(255,255,255, 0);
+		  -moz-box-shadow: 0 0 0 10px rgba(0,0,255, 0);
+		  box-shadow: 0 0 0 10px rgba(0,0,255, 0);
 	  }
 	  100% {
-		  -moz-box-shadow: 0 0 0 0 rgba(255,255,255, 0);
-		  box-shadow: 0 0 0 0 rgba(255,255,255, 0);
+		  -moz-box-shadow: 0 0 0 0 rgba(0,0,255, 0);
+		  box-shadow: 0 0 0 0 rgba(0,0,255, 0);
 	  }
 	}
 </style>
