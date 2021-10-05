7b51ce7e1b492f3d6bfcc8d1daa70ae7789a06b4
Mikko Ohtamaa <mikko@opensourcehacker.com>
2021-10-05 17:04:07 +0200

    Don't commit buikd

--- build/assets/_app/chunks/env-a13806e5.js
+++
@@ -1,1 +0,0 @@
-const s=!0,e=!1;export{s as b,e as d};
--- build/assets/_app/pages/[chain]/[exchange]/[pair]/TimeSpanPerformance.svelte-bc6d10b6.js
+++
@@ -1,1 +0,0 @@
-import"../../../../chunks/vendor-475ceb10.js";import{T as a}from"../../../../chunks/TimeSpanPerformance-96a57b47.js";import"../../../../chunks/formatters-70d542d8.js";export{a as default};
