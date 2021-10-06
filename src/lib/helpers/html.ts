12eb9c0c8e3442d6641205895363e6acb0f796cb
Mikko Ohtamaa <mikko@opensourcehacker.com>
2021-10-06 23:27:49 +0200

    Pages render, the data is still whatever it is

---
+++ src/lib/helpers/html.ts
@@ -0,0 +1,9 @@
+// https://stackoverflow.com/a/6234804/315168
+export function escapeHtml(unsafe: string): string {
+    return unsafe
+         .replace(/&/g, "&amp;")
+         .replace(/</g, "&lt;")
+         .replace(/>/g, "&gt;")
+         .replace(/"/g, "&quot;")
+         .replace(/'/g, "&#039;");
+ }
--- src/routes/[chain]/[exchange_id].svelte
+++ src/routes/[chain]/[exchange_id].svelte
@@ -25,7 +25,7 @@
                 return {
                     status: resp.status,
                     error: new Error(
-                        `Could not load data for trading pair: ${apiUrl}. See console for details.`
+                        `Could not load data for the exchange details: ${apiUrl}. See console for details.`
                     )
                 };
             }
@@ -37,55 +37,114 @@
             props: {
                 exchange_slug,
                 chain_slug,
-                details
+                details,
+                backendUrl,
             }
         };
     }
 </script>
 
 <script>
-    import Datatable from '$lib/Datatable/datatable.svelte';
-    import {formatNumber} from '$lib/helpers/formatters';
-    import {onMount, tick} from 'svelte';
+    import Datatable from '$lib/datatable/datatable.svelte';
+    import { formatDollar, formatPriceChange } from "$lib/helpers/formatters";
+    import { escapeHtml } from "$lib/helpers/html";
 
     export let exchange_slug;
     export let chain_slug;
     export let details;
+    export let backendUrl;
 
-    const columnsPairs = ['Quote', 'Volume 24h', 'Liquidity', 'Price'];
-    const optionsPairs = {
-        order: [[1, 'desc']],
-        serverSide: false,
+    // const colums = ['Quote', 'Volume 24h', 'Liquidity', 'Price'];
+
+	// https://tradingstrategy.ai/api/explorer/#/Pair/web_pairs
+	// See https://datatables.net/reference/option/columns
+	const columns = [
+		{
+			name: "Trading pair",
+			className: "col-exchange",
+			data: "pair_name",
+			// https://datatables.net/reference/option/columns.render
+			render: function(data, type, row, meta) {
+                const chain = escapeHtml(row.chain_slug);
+                const exchange = escapeHtml(row.exchange_slug);
+                const pair = escapeHtml(row.pair_slug);
+                const symbols = escapeHtml(row.pair_symbol);
+				return `<a href="/${chain}/${exchange}/${pair}">${symbols}</a>`;
+			}
+		},
+
+		{
+			name: "Price (USD)",
+			data: "usd_price_latest",
+			className: "col-price",
+			type: "num", // https://datatables.net/reference/option/columns.type
+			render: function(data, type, row, meta) {
+				return formatDollar(data);
+			}
+		},
+
+		{
+			name: "Price Δ",
+			data: "price_change_24h",
+			className: "col-price-change",
+			type: "num", // https://datatables.net/reference/option/columns.type
+			render: function(data, type, row, meta) {
+				return formatPriceChange(data);
+			}
+		},
+
+		{
+			name: "Volume 24h (USD)",
+			data: "usd_volume_24h",
+			className: "col-volume",
+			type: "num", // https://datatables.net/reference/option/columns.type
+			render: function(data, type, row, meta) {
+				return formatDollar(data);
+			}
+		},
+
+		{
+			name: "Liquidity (USD)",
+			data: "usd_liquidity_latest",
+			className: "col-liquidity",
+			type: "num", // https://datatables.net/reference/option/columns.type
+			render: function(data, type, row, meta) {
+				return formatDollar(data);
+			}
+		},
+
+		{
+			name: "Liq Δ",
+			data: "liquidity_change_24h",
+			className: "col-liquidity-change",
+			type: "num", // https://datatables.net/reference/option/columns.type
+			render: function(data, type, row, meta) {
+				return formatPriceChange(data);
+			}
+		}
+	];
+
+    const options = {
+        order: [[3, 'desc']],
+		searching: false,
+		serverSide: false,
+		lengthChange: false,
         ajax: {
-            url: `https://matilda.tradingstrategy.ai/pairs?chain_slugs=${chain_slug}&exchange_slugs=${exchange_slug}`,
+            url: `${backendUrl}/pairs?chain_slugs=${chain_slug}&exchange_slugs=${exchange_slug}`,
             type: 'GET',
-            dataSrc: function (exchangePairs) {
-                return exchangePairs.results.map((exchangePair) => [
-                    `<a class="nav-link" href="/${chain}/${exchangeId.toLowerCase()}/${
-                        exchangePair.base_token_symbol
-                    }-${exchangePair.quote_token_symbol}">${exchangePair.base_token_symbol}-${
-                        exchangePair.quote_token_symbol
-                    }</a>`,
-                    `${exchangePair.base_token_symbol}/${exchangePair.quote_token_symbol}`,
-                    exchangePair.usd_volume_24h,
-                    exchangePair.liquidity_change_24h,
-                    exchangePair.usd_price_latest
-                ]);
+            dataSrc: function (pairResult) {
+                // {total: 57476, pages: 575, results: Array(100)}
+                console.log(pairResult);
+                return pairResult.results;
             }
         }
     };
 
-    onMount(async () => {
-        if (browser) {
-            const initDt = (await import('datatables.net-dt')).default();
-            initDt();
-        }
-    });
 </script>
 
 <svelte:head>
     <title>
-        {details.human_readable_name} trading on {details.chain_slug}
+        {details.human_readable_name} trading on {details.human_readable_name}
     </title>
     <meta
             name="description"
@@ -94,8 +153,20 @@
 </svelte:head>
 
 <div class="container">
-    <h1>{details.human_readable_name} on {chain_slug}</h1>
+    <div class="exchange-content">
+        <h1>{details.human_readable_name} on {chain_slug}</h1>
 
-    <h2>Trading Pairs</h2>
-    <Datatable columns={columnsPairs} options={optionsPairs}/>
+        <h2>Trading Pairs</h2>
+        <Datatable
+                columns={columns}
+                options={options}
+                clickableRows={true}
+                />
+    </div>
 </div>
+
+<style>
+    .exchange-content {
+	    margin: 60px 0;
+    }
+</style>
