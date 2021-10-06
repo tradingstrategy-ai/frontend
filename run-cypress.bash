97edb7c9c57ccd4eed23914914eebff624a6f49b
Mikko Ohtamaa <mikko@opensourcehacker.com>
2021-10-06 23:59:01 +0200

    Got Cypress running locally

--- run-cypress.bash
+++ run-cypress.bash
@@ -1,32 +1,35 @@
 #!/bin/bash
+#
+# Start SvelteKit dev server and run Cypress test suite against it
+#
 
 set -e
 set -x
 
 echo "Using Cypress integration test suite using backend server $VITE_PUBLIC_BACKEND_URL"
 
+# Kill the dev server when the bash script exits
+# https://stackoverflow.com/a/2173421/315168
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
 # Start dev server
 npm run dev &
 
-PID_SVELTE=$!
+sleep 3
 
-sleep 1
-
+# Did not figure out why curl returns 000 in scripted, though works from the command lien
 # https://stackoverflow.com/a/44364396/315168
-test_command='curl -sL \
-    -w "%{http_code}\\n" \
-    "http://localhost:3000/" \
-    -o /dev/null \
-    --connect-timeout 3 \
-    --max-time 5'
+#test_command='curl -sL -w "%{http_code}\\n" "http://localhost:3000/" -o /dev/null --connect-timeout 15 --max-time 15'
 
-if [ $(test_command) == "200" ] ;
-then
-   echo "Test server is up" ;
-else
-   echo "FAIL to start server"
-   exit 1
-fi
+#echo $test_command
+#res=`$test_command`
+
+#if [ $res == "200" ] ; then
+#   echo "Test server is up" ;
+#else
+#   echo "FAILED to start server"
+#   exit 1
+#fi
 
 
 # Run Cypress
