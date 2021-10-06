d69ae5717cdcfd5cebfecb18b1a2b3ca330569e5
Mikko Ohtamaa <mikko@opensourcehacker.com>
2021-10-06 23:34:21 +0200

    Check the test server before running Cypress

--- run-cypress.bash
+++ run-cypress.bash
@@ -9,6 +9,24 @@ npm run dev &
 
 PID_SVELTE=$!
 
+sleep 1
+
+test_command='curl -sL \
+    -w "%{http_code}\\n" \
+    "http://localhost:3000/" \
+    -o /dev/null \
+    --connect-timeout 3 \
+    --max-time 5'
+
+if [ $(test_command) == "200" ] ;
+then
+   echo "Test server is up" ;
+else
+   echo "FAIL to start server"
+   exit 1
+fi
+
+
 # Run Cypress
 npm run cypress:run
 
