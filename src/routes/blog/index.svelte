2e966350e555aafc568be88ee3bd5a3b2dbabfb2
Mikko Ohtamaa <mikko@opensourcehacker.com>
2021-10-07 20:27:39 +0200

    Styling the blog roll

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -53,43 +53,66 @@
 
 <div class="container">
 
-  <div class="row">
-    <div class="col-md-12">
-      <h1>Trading Strategy blog</h1>
+  <div class="section-blog-roll">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Trading Strategy blog</h1>
 
-      <p class="lead">
-        Follow the protocol development.
-      </p>
+        <p class="lead">
+          Follow the protocol development.
+        </p>
+      </div>
     </div>
-  </div>
 
-  <div class="row">
-    <div class="col-md-12">
-      {#each posts as post}
+    <div class="row">
+      <div class="col-md-8">
+        {#each posts as post}
 
-        <div class="card bg-primary border-light shadow-soft card-post">
+          <div class="card bg-primary border-light shadow-soft card-post">
 
-          {#if post.feature_image}
-            <img class="card-img-top rounded-top" src={post.feature_image} alt={post.feature_image_alt}>
-          {/if}
+            {#if post.feature_image}
+              <img class="card-img-top rounded-top" src={post.feature_image} alt={post.feature_image_alt}>
+            {/if}
 
-          <div class="card-body">
+            <div class="card-body">
 
-            <h5 class="card-title">{post.title}</h5>
+              <h5 class="card-title">{post.title}</h5>
 
-            <p>
-              Published <Time relative timestamp="{Date.parse(post.published_at)}" />
-            </p>
+              <p class="text-published text-muted text-sm">
+                Published: <Time relative timestamp="{Date.parse(post.published_at)}" />
+              </p>
 
-            <p class="card-text">
-              {post.excerpt}
-            </p>
+              <p class="card-text">
+                {post.excerpt}
+              </p>
+
+              <a class="btn btn-primary btn-sm btn-read" href={`/blog/${post.slug}`}>Read post</a>
+            </div>
+
           </div>
-
-          <a class="btn btn-primart btn-sm" href={`/blog/${post.slug}`}>Read post</a>
-
-        </div>
-      {/each}
+        {/each}
+      </div>
     </div>
   </div>
 </div>
+
+<style>
+
+  .section-blog-roll {
+    margin: 60px 0;
+  }
+
+  .card-img-top {
+    max-height: 220px;
+    object-fit: cover;
+  }
+
+  .text-published {
+    font-size: 70%;
+    text-transform: uppercase;
+  }
+
+  .btn-read {
+    float: right;
+  }
+</style>
