60a5a35c69321916273bab95077204da7af9c27e
Josep Servat <jservat@outlook.es>
2021-10-08 08:51:23 +0200

    check path

--- run-cypress.bash
+++ run-cypress.bash
@@ -12,9 +12,9 @@ echo "Using Cypress integration test suite using backend server $VITE_PUBLIC_BAC
 # https://stackoverflow.com/a/2173421/315168
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-
 #install theme
-cd theme && npm install && npx gulp build:dist
+cd theme && pwd
+npm install && npx gulp build:dist
 cd ..
 
 # Start dev server
