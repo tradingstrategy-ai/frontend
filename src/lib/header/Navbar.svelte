dfb9b65b0b34a794ef00075f45eaf2a730f561b1
Mikko Ohtamaa <mikko@opensourcehacker.com>
2021-10-11 11:26:01 +0200

    Update the logo styling

--- src/lib/header/Navbar.svelte
+++ src/lib/header/Navbar.svelte
@@ -7,7 +7,7 @@
 
 <script lang="ts">
 
-  import logo from '../../lib/assets/logo-web-beta.svg';
+  import logo from '../../lib/assets/logo-web-beta-2.svg';
   import logoMobile from '../../lib/assets/logo-two-lines.png';
 
   import {
