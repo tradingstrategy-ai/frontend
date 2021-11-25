1c61361d4c28d7678aa33d1fddc964feade4c7d4
Mikko Ohtamaa <mikko@opensourcehacker.com>
2021-11-25 11:08:09 +0100

    Add ad hoc upgrade note, as we do not have a backend to push these changes to frontend SSR server

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,8 +7,20 @@
 </script>
 
 <AppHead />
+
+<div class="site-alert text-center">
+    We are currently upgrading the service to a new version. Some pages and APIs might not be accessible.
+</div>
+
 <Navbar />
 
 <slot />
 <SiteMode />
 
+<style>
+    .site-alert {
+        padding: 10px;
+        background: red;
+        color: white;
+    }
+</style>
