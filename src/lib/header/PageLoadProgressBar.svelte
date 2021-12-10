452be099d0bb8063996c04f186e17d6caa67ba03
Mikko Ohtamaa <mikko@opensourcehacker.com>
2021-12-10 21:01:42 +0100

    Got the progress bar working

--- src/lib/header/PageLoadProgressBar.svelte
+++ src/lib/header/PageLoadProgressBar.svelte
@@ -4,46 +4,100 @@
      * Svelte uses internal router, not server-side loading.
 	 * Thus, we need to manually give some indication in the user interface if the loading takes more than a blink of an eye.
 	 *
-	 * Based on the original implementation https://github.com/shajidhasan/sveltekit-page-progress-demo by Shajid Hasan
+	 * Based on the original implementation https://github.com/shajidhasan/sveltekit-page-progress-demo by Shajid Hasan.
+	 *
+	 * As this component is absolutely position, you can put it at any part of your __layout.svelte.
      */
 	import { onDestroy, onMount } from 'svelte';
+	import { writable } from 'svelte/store';
 	import { tweened } from 'svelte/motion';
 	import { cubicOut } from 'svelte/easing';
-	import navigationState from '../../stores/navigation';
+
+	const navigationState = writable();
 
 	const progress = tweened(0, {
 		duration: 3500,
 		easing: cubicOut
 	});
+
 	const unsubscribe = navigationState.subscribe((state) => {
-		if (state === 'loaded') {
+
+		// You will always get state=undefined
+		// event on the server-side rendering, so
+		// safely ignore it
+		console.log("The loading state is", state);
+
+		if (state === 'loading-with-progress-bar') {
+			progress.set(0, { duration: 0 });
+			progress.set(0.8, { duration: 5000 });
+		} else if (state === 'loaded') {
 			progress.set(1, { duration: 1000 });
 		}
 	});
+
 	onMount(() => {
-		progress.set(0.7);
+		// progress.set(0.7);
 	});
 	onDestroy(() => {
 		unsubscribe();
 	});
 </script>
 
+<svelte:window
+	on:sveltekit:navigation-start={() => {
 
-<div class="progress-bar">
+		// If the page loads fast enough in the preloading state,
+		// never display the progress bar
+		$navigationState = 'preloading';
+
+        // Delay the progress bar to become visible an eyeblink... only show if the page load takes too long
+        setTimeout(function() {
+			// After 250ms switch preloading to loading-with-progress-bar
+            if($navigationState === 'preloading') {
+                $navigationState = 'loading-with-progress-bar';
+            }
+        }, 500);
+	}}
+	on:sveltekit:navigation-end={() => {
+		$navigationState = 'loaded';
+	}}
+/>
+
+<!--
+	Make sure the container component is always in the DOM structure.
+
+	If we make changes to the page structure during the navigation, we get a page double rendered error:
+	https://stackoverflow.com/questions/70051025/sveltekit-adds-new-page-on-top-of-old-one
+
+	Not sure if this is a bug or a feature.
+-->
+<div class="page-progress-bar" class:loaded={$navigationState === 'loaded'} class:preloading={$navigationState === 'preloading'} class:loading={$navigationState === 'loading-with-progress-bar'}>
 	<div class="progress-sliver" style={`--width: ${$progress * 100}%`} />
 </div>
 
 <style>
-	.progress-bar {
+	 /* Always stay fixed at the top, but stay transparent if no activity is going on */
+	.page-progress-bar {
 		position: fixed;
 		top: 0;
 		left: 0;
 		right: 0;
 		height: 0.5rem;
+		background: transparent;
+		z-index: 100;
+		opacity: 0;
+		transition: opacity 0.5s;
 	}
+
+	 /* After transitioning from preloading to loading state, make the progress bar visible with CSS transition on opacity */
+	.page-progress-bar.loading {
+		opacity: 1;
+		transition: opacity 0.5s;
+	}
+
 	.progress-sliver {
 		width: var(--width);
-		background-color: #f8485e;
+		background-color: var(--price-up-green);
 		height: 100%;
 	}
 </style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,37 +2,21 @@
     /**
      * The main website layout frame.
      */
-    import { fade } from 'svelte/transition';
 	import Navbar from '$lib/header/Navbar.svelte';
 	import AppHead from '$lib/header/AppHead.svelte';
     import SiteMode from "$lib/header/SiteMode.svelte";
     import PageLoadProgressBar from "$lib/header/PageLoadProgressBar.svelte";
-    import navigationState from '../stores/navigation';
 
 	import '../../theme/dist/css/neumorphism.css';
 </script>
 
-<svelte:window
-	on:sveltekit:navigation-start={() => {
-		$navigationState = 'loading';
-	}}
-	on:sveltekit:navigation-end={() => {
-		$navigationState = 'loaded';
-	}}
-/>
-
-
 <AppHead />
 
-    {#if $navigationState === 'loading'}
-        <div out:fade={{ delay: 500 }}>
-            <PageLoadProgressBar />
-        </div>
-    {/if}
-
+<PageLoadProgressBar />
 <Navbar />
 <slot />
 <SiteMode />
+
 
 <style>
     .site-alert {
