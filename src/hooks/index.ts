ab994733b5240676a7b687540f590d08fc06f57f
Mikko Ohtamaa <mikko@opensourcehacker.com>
2022-01-06 19:47:54 +0200

    Remove Cheerio as a server-side dep as it is no longer needed

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,58 +1,8 @@
 import type { Handle } from '@sveltejs/kit';
-import * as cheerio from 'cheerio';
 import {siteMode} from "$lib/config";
 
 
-/**
- * Modifies the response in-place to fix the link rel="modulepreload" issue.
- *
- * Note that this fix causes significant server-side processing time increase -
- * do not use long term, only proper fix is to fix Vite.
- *
- * For the source of the issue of loading times please see https://github.com/vitejs/vite/issues/5120
- *
- */
-function fixLinkModulePreloadIssue(response: ServerResponse) {
-
-	if(!response.body) {
-		throw new Error("text/html response was missing body");
-	}
-	const $ = cheerio.load(response.body);
-
-	const body = $("body");
-
-	// Replace <link rel="modulepreload"> with <script type="module">
-	// https://api.jquery.com/replacewith/
-	//$('link[rel="modulepreload"]').replaceWith(function() {
-	//  const src = $(this).attr('href');
-	//// The defer attribute has no effect on module scripts — they defer by default.
-	//  return `<script type="module" src="${src}"></script>`;
-	//});
-
-	$('link[rel="modulepreload"]').each(function(idx, elem) {
-		const $this = $(this);
-		const src = $this.attr('href');
-		$(`<script type="module" async src="${src}"></script>`).appendTo(body);
-		$this.remove();
-	});
-
-	// TODO: Does not have effect if we put scripts at the end of the body
-	// Move starter section to the <body> end
-	// Assume we have only one <script type="module"> in our generated <head>
-	// const starter = $("head script[type='module']");
-	//starter.appendTo(body);
-	//starter.remove();
-
-	response.body = $.html();
-}
-
-
 export const handle: Handle = async ({ request, resolve }) => {
-
-	// TODO https://github.com/sveltejs/kit/issues/1046
-	// if (request.url.searchParams.has('_method')) {
-	//	request.method = request.url.searchParams.get('_method').toUpperCase();
-	//}
 
 	const response = await resolve(request);
 
