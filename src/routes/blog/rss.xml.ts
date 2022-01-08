6596eae9712b2862a08aac7e6ace5a43883940cb
Mikko Ohtamaa <mikko@opensourcehacker.com>
2022-01-08 22:44:04 +0100

    Fix RSS feed title

--- src/routes/blog/rss.xml.ts
+++ src/routes/blog/rss.xml.ts
@@ -29,7 +29,7 @@ const render = (posts) => `<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 <channel>
 <atom:link href="https://tradingstrategy.ai/rss" rel="self" type="application/rss+xml" />
-<title>Algorithmic trading protocol for decentralised markets</title>
+<title>Trading Strategy</title>
 <link>https://tradingstrategy.ai</link>
 <description>Algorithmic trading protocol for decentralised markets</description>
 ${posts
