8e892f169598d6c44b8263db272851952de3f692
Ken Kunz <kennethkunz@gmail.com>
2022-02-22 20:23:33 -0600

    add type badges (styling tbd)

--- src/lib/search/Search.svelte
+++ src/lib/search/Search.svelte
@@ -2,6 +2,10 @@
   import { Input, ListGroup, ListGroupItem } from "sveltestrap";
   import tradingEntities from "./trading-entities";
 
+  function getLabel({ type }) {
+    return type === "exchange" ? "DEX" : type;
+  }
+
   let isOpen = false;
 </script>
 
@@ -18,7 +22,10 @@
     <div class="card bg-primary shadow-soft border-light">
       <ListGroup flush>
         {#each $tradingEntities as { document } (document.id)}
-          <ListGroupItem>{document.description}</ListGroupItem>
+          <ListGroupItem>
+            <span class="tag {document.type}">{getLabel(document)}</span>
+            {document.description}
+          </ListGroupItem>
         {:else}
           <ListGroupItem>Search exchanges, tokens and pairs</ListGroupItem>
         {/each}
@@ -38,5 +45,21 @@
     top: 3rem;
     right: 0;
     width: 450px;
+  }
+
+  .card :global(.list-group-item) {
+    border: none;
+  }
+
+  .tag {
+    display: inline-block;
+    width: 4em;
+    margin-right: 0.5ex;
+    border-radius: 6px;
+    background-color: #666;
+    color: white;
+    font-size: 0.8rem;
+    font-weight: 600;
+    text-align: center;
   }
 </style>
