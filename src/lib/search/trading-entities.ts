34c3043fb255621b45137ca884d2f86e20bd871d
Ken Kunz <kennethkunz@gmail.com>
2022-02-23 15:25:03 -0600

    improve tradingEntities dev experience
    - document API
    - add type defs for `search`
    - fix build issue

--- src/lib/search/trading-entities.ts
+++ src/lib/search/trading-entities.ts
@@ -1,3 +1,12 @@
+/**
+ * This module exports a single default object: a svelte store for searching
+ * the Typesense `trading-entities` collection.
+ *
+ * usage:
+ *   tradingEntities.search('foo')        // search the collection
+ *   tradingEntities.subscribe(callback)  // receive updated search results
+ *   $tradingEntities                     // within a component - Svelte's reactive store sugar
+ */
 import { writable } from "svelte/store";
 import searchClient from "./client";
 
@@ -7,17 +16,16 @@ const searchOptions = {
   group_by: "type"
 };
 
+const collection = searchClient?.collections("trading-entities").documents();
+
 const { subscribe, set } = writable([]);
-let collection;
 
-if (searchClient) {
-  collection = searchClient.collections("trading-entities").documents();
-}
+async function search(query: string): Promise<void> {
+  if (!collection) return;
 
-async function search(query) {
   const q = query.trim();
 
-  if (!collection || q === "") {
+  if (q === "") {
     set([]);
     return;
   }
