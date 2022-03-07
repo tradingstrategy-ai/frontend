2a73b50097c915796a57a6da1b96449cebb4b262
Ken Kunz <kennethkunz@gmail.com>
2022-03-07 16:53:29 -0600

    refactor - extract common _Filter component

---
+++ src/routes/search/_Filter.svelte
@@ -0,0 +1,54 @@
+<script>
+  export let fieldName, options;
+  export let selected = [];
+</script>
+
+<h2>{fieldName.replace(/_/g, " ")}</h2>
+<div class="option-group">
+    {#each options as { label, value, count } (value)}
+        {#if value}
+            <label>
+                <input type="checkbox" bind:group={selected} {value} />
+                <div class="label">{label || value}</div>
+                {#if count}
+                    <div class="count">{count.toLocaleString('en')}</div>
+                {/if}
+            </label>
+        {/if}
+    {/each}
+</div>
+
+<style>
+  h2 {
+    font-size: 1rem;
+    text-transform: capitalize;
+  }
+
+  .option-group {
+    margin-bottom: 1.25em;
+    max-width: 200px;
+  }
+
+  label {
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    gap: 1ex;
+    line-height: 1.2;
+  }
+
+  .label {
+    flex: 1;
+    overflow: hidden;
+    text-transform: capitalize;
+  }
+
+  .count {
+    padding: 0px 4px;
+    border-radius: 4px;
+    font-size: 0.75rem;
+    line-height: 1.4;
+    font-weight: 400;
+    background-color: #99999988;
+  }
+</style>
--- src/routes/search/_RangeFilter.svelte
+++ src/routes/search/_RangeFilter.svelte
@@ -1,5 +1,8 @@
 <script>
-  export let fieldName, breakpoints, filter;
+  import Filter from "./_Filter.svelte";
+
+  export let fieldName, breakpoints;
+  export let filter = null;
 
   // default formatter returns value unchanged
   export let formatter = (val) => val;
@@ -10,7 +13,9 @@
 
   const options = [];
   for (let i = 0; i < breakpoints.length - 1; i++) {
-    options.push(breakpoints.slice(i, i + 2));
+    const value = breakpoints.slice(i, i + 2);
+    const label = getLabel(value);
+    options.push({ label, value });
   }
 
   function getLabel([ min, max ]) {
@@ -35,39 +40,4 @@
   }
 </script>
 
-<h2>{fieldName.replace(/_/g, " ")}</h2>
-<div class="option-group">
-    {#each options as option, idx (idx)}
-        <label>
-          <input type="checkbox" bind:group={selected} value={option} />
-          <div class="value">{getLabel(option)}</div>
-        </label>
-    {/each}
-</div>
-
-
-<style>
-  h2 {
-    font-size: 1rem;
-    text-transform: capitalize;
-  }
-
-  .option-group {
-    margin-bottom: 1.25em;
-    max-width: 200px;
-  }
-
-  label {
-    display: flex;
-    justify-content: left;
-    align-items: center;
-    gap: 1ex;
-    line-height: 1.2;
-  }
-
-  .value {
-    flex: 1;
-    overflow: hidden;
-    text-transform: capitalize;
-  }
-</style>
+<Filter bind:selected {fieldName} {options} />
