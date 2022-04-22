959bf730b392578035ed96c64911d1d312a8757e
Mikko Ohtamaa <mikko@opensourcehacker.com>
2022-04-22 18:33:27 +0200

    Remove trade-executor-frontend as an explicit dependency, use vite alias for now

--- src/routes/strategy/__layout.svelte
+++ src/routes/strategy/__layout.svelte
@@ -7,3 +7,27 @@
 </div>
 
 <slot />
+
+<!--
+
+    Styles that are applied to within any page of strategy section.
+
+-->
+<style>
+	:global(:root) {
+		--profit-green: #458b00;
+		--profit-red: #cc0000;
+		--profit-black: #000;
+	}
+
+	/**
+     * Price action and candle colors
+     */
+	:global(.profit-green) {
+		color: var(--profit-green);
+	}
+
+	:global(.profit-red) {
+		color: var(--profit-red);
+	}
+</style>
