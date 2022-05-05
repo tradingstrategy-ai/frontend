d88826bac6400b0f1d03fbd03da0f097e73c7432
Ken Kunz <kennethkunz@gmail.com>
2022-05-04 20:18:54 -0500

    link price and liquidity charts - crosshairs

--- src/lib/chart/ChartLinker.ts
+++ src/lib/chart/ChartLinker.ts
@@ -8,26 +8,66 @@ export default class ChartLinker {
   linked = new Map();
 
   add(chartEngine): void {
-    const ref = chartEngine.append('draw', this.handleDraw.bind(this, chartEngine));
-    this.linked.set(chartEngine, ref);
+    const refs = [
+      chartEngine.prepend('mousemoveinner', this.mouseMove.bind(this, chartEngine)),
+      chartEngine.prepend('doDisplayCrosshairs', this.displayCrosshairs.bind(this, chartEngine)),
+      chartEngine.append('draw', this.draw.bind(this, chartEngine)),
+    ];
+    this.linked.set(chartEngine, refs);
   }
 
   remove(chartEngine): boolean {
-    chartEngine.removeInjection(this.linked.get(chartEngine));
+    this.linked.get(chartEngine).forEach(chartEngine.removeInjection);
     return this.linked.delete(chartEngine);
   }
 
-  handleDraw(conductor): void {
-    if (conductor.isFollowing || !conductor.chart?.dataSegment) return;
-
+  eachFollower(conductor, callback): void {
     for (const follower of this.linked.keys()) {
       if (follower === conductor || !follower.chart?.dataSegment) continue;
       follower.isFollowing = true;
+      callback(follower);
+      follower.isFollowing = false;
+    }
+  }
+
+  draw(conductor): void {
+    if (conductor.isFollowing || !conductor.chart?.dataSegment) return;
+
+    this.eachFollower(conductor, (follower) => {
       follower.micropixels = conductor.micropixels;
       follower.chart.scroll = conductor.chart.scroll;
       follower.setCandleWidth(conductor.layout.candleWidth);
       follower.draw();
-      follower.isFollowing = false;
-    }
+    });
+  }
+
+  mouseMove(conductor): void {
+    if (conductor.isFollowing || !conductor.chart?.dataSegment) return;
+
+    this.eachFollower(conductor, (follower) => {
+      // mirror mouse position on followers
+      follower.mousemoveinner(conductor.cx + follower.left, conductor.cy + follower.top);
+      // hide crosshairs and cooresponding price/date labels
+      follower.controls.crossX.style.display = 'none';
+      follower.controls.floatDate.style.display = 'none';
+      follower.controls.crossY.style.display = 'none';
+      follower.chart.yAxis.drawPriceLabels = false;
+    });
+  }
+
+  displayCrosshairs(conductor) {
+    if (conductor.isFollowing || !conductor.chart?.dataSegment) return;
+
+    // reset values that may have been hidden as a follower
+    conductor.controls.crossX.style.display = '';
+    conductor.controls.floatDate.style.display = '';
+    conductor.controls.crossY.style.display = '';
+    conductor.chart.yAxis.drawPriceLabels = true;
+
+    this.eachFollower(conductor, (follower) => {
+      // show vertical crosshair and date label if conductor crosshairs shown
+      follower.controls.crossX.style.display = '';
+      follower.controls.floatDate.style.display = '';
+    });
   }
 }
