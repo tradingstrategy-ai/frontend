3024e94a4f76a296df1a2e8b2c4f54c82edf0ba1
Ken Kunz <kennethkunz@gmail.com>
2022-05-05 15:55:51 -0500

    improve /slow-load test page

--- src/routes/slow-load.svelte
+++ src/routes/slow-load.svelte
@@ -1,27 +1,32 @@
-<script context="module">
-    /**
-     * A test endpoint for the page load progress bar.
-     *
-     */
-	export async function load({ }) {
-		// Sleep for 2 seconds
-		const sleep = new Promise((resolve) => setTimeout(resolve, 4000));
-		await sleep;
-		return {};
+<!--
+A test endpoint for the page load progress bar. Page load is delayed 2 seconds
+by default. Increment the `?page=n` param to delay by n * 2 seconds.
+-->
+<script context="module" lang="ts">
+	export async function load({ url }) {
+		const page = parseInt(url.searchParams.get('page')) || 1;
+		await new Promise((resolve) => setTimeout(resolve, page * 2000));
+		return { props: { page } };
 	}
+</script>
+
+<script lang="ts">
+	export let page: number;
 </script>
 
 <div class="container">
 	<div class="content">
-		<h1>You're on Page 1.</h1>
+		<h1>You're on Page {page}.</h1>
 		<p>
-			In this page's <code>load</code> function, we waited for two seconds to mimick a real-world API
-			call before rendering. You can go back to the home page, which should load instantaneously because
-			it doesn't do much server side. Page 2, however, will have a longer delay of 5 seconds.
+			In this page's <code>load</code> function, we waited for {page * 2}
+			seconds to mimick a real-world API call before rendering. You can go
+			back to the home page, which should load instantaneously because it
+			doesn't do much server side. Page 2 (and beyond) will have a longer
+			delay of {(page + 1) * 2} seconds.
 		</p>
 		<div class="links">
 			<a href="/">Home</a>
-			<a href="/page-2">Page 2</a>
+			<a href="?page={page + 1}">Page {page + 1}</a>
 		</div>
 	</div>
 </div>
