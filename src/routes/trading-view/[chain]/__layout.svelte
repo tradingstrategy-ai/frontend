7473acb21a4e4a90892f6947551b88946fa26734
Ken Kunz <kennethkunz@gmail.com>
2022-05-25 12:24:03 -0500

    return HTTP 503 status for chain maintenance

--- src/routes/trading-view/[chain]/__layout.svelte
+++ src/routes/trading-view/[chain]/__layout.svelte
@@ -4,7 +4,11 @@
   /** @type {import('@sveltejs/kit').Load} */
   export function load({ params }) {
     // Check if we should tell user to go away from this page
-    checkChainMaintenance(params.chain);
+    try {
+      checkChainMaintenance(params.chain);
+    } catch(error) {
+      return { status: 503, error };
+    }
     return {};
   }
 </script>
