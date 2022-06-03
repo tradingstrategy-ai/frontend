6bb82a6e7734e829fcaadbdf82e5c5fee2f4acba
Mikko Ohtamaa <mikko@opensourcehacker.com>
2022-06-03 09:21:13 +0200

    Update executor frontend

--- src/routes/strategy/[strategy_id]/open-positions/index.svelte
+++ src/routes/strategy/[strategy_id]/open-positions/index.svelte
@@ -8,6 +8,6 @@
 <PositionList
 	positions={openPositions}
 	stats={$stats}
-	columns={{ opened_at: true, value: true, profitability: true }}
+	columns={{ opened_at: true, value: true, profitability: true, details: true }}
 	pagination={false}
 />
