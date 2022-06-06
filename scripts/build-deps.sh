ade7c43a456bd17bd11ec0a0c0442b646501608a
Mikko Ohtamaa <mikko@opensourcehacker.com>
2022-06-06 20:34:33 +0200

    Use npm run build instead of install - fix usage of VITE_PUBLIC_STRATEGIES as the trade-executor-frontend build step

--- scripts/build-deps.sh
+++ scripts/build-deps.sh
@@ -15,6 +15,11 @@ fi
 # Create the Bootstrap precompiled CSS bundle
 (cd deps/theme && npm install && npx gulp build:dist)
 
+
+# Needed to correctly build the executor frontend
+source .env
+export VITE_PUBLIC_STRATEGIES
+
 # Package trade-executor-frontend for SvelteKit
-(cd deps/trade-executor-frontend && npm install)
+(cd deps/trade-executor-frontend && npm run build)
 
