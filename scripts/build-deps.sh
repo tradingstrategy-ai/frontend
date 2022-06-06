7dd91475e9c9f915d4b491487759620aa24de2ff
Mikko Ohtamaa <mikko@opensourcehacker.com>
2022-06-06 20:35:59 +0200

    Figure out better way to pass VITE_PUBLIC_STRATEGIES to trade-executor-frontend build

--- scripts/build-deps.sh
+++ scripts/build-deps.sh
@@ -16,9 +16,12 @@ fi
 (cd deps/theme && npm install && npx gulp build:dist)
 
 
-# Needed to correctly build the executor frontend
-source .env
-export VITE_PUBLIC_STRATEGIES
+# Needed to correctly build the executor frontend.
+# On prod this comes from the pre-set env
+if [ -z "VITE_PUBLIC_STRATEGIES" ]; then
+  source .env
+  export VITE_PUBLIC_STRATEGIES
+fi
 
 # Package trade-executor-frontend for SvelteKit
 (cd deps/trade-executor-frontend && npm run build)
