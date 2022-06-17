638664fe98329653c90224c1c8c642444aa89ee5
Ken Kunz <kennethkunz@gmail.com>
2022-06-17 17:03:03 -0500

    add SSR preload hinds for most common fonts

--- src/lib/header/AppHead.svelte
+++ src/lib/header/AppHead.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-	import favicon from '../../lib/assets/brand-mark.svg';
+	import favicon from '$lib/assets/brand-mark.svg';
+	import FontPreloadHints from './FontPreloadHints.svelte';
 </script>
 
 <svelte:head>
 	<link rel="icon" type="image/svg+xml" href={favicon} />
 	<link rel="alternate" type="application/rss+xml" title="RSS Feed for tradingstrategy.ai" href="/blog/rss.xml" />
+	<FontPreloadHints />
 </svelte:head>
---
+++ src/lib/header/FontPreloadHints.svelte
@@ -0,0 +1,39 @@
+<!--
+@component
+Generate <link rel="preload" /> hints for the most commonly used fonts in our design system.
+This optimizes font-loading speed on first page load so the preferred font is more
+likely to be displayed vs. fallback system font. Fonts should be cached after first
+page load. Only rendered during SSR, and only if `deps/fonts` submodule is installed.
+
+#### Usage:
+```tsx
+<FontPreloadHints />
+```
+-->
+<script lang="ts" context="module">
+  import { browser, dev } from '$app/env';
+
+  const fonts = [
+    'NeueHaasGroteskDisplay/65.woff2',
+    'NeueHaasGroteskDisplay/75.woff2',
+    'NeueHaasGroteskText/55.woff2',
+    'SourceSerifPro/latin-400-normal.woff2'
+  ];
+
+  // get all design-system-font asset paths (optional module - may be empty)
+  const modules = import.meta.globEager('design-system-fonts/**/*.woff2');
+
+  const fontPaths = [];
+
+  for (const path in modules) {
+    for (const font of fonts) {
+      if (path.includes(font)) fontPaths.push(modules[path].default);
+    }
+  }
+</script>
+
+{#if !(browser || dev)}
+  {#each fontPaths as fontPath}
+    <link rel="preload" href={fontPath} as="font" crossorigin="anonymous" />
+  {/each}
+{/if}
