6cf30a63642709baa37285ae0d81140716a2bc62
Ken Kunz <kennethkunz@gmail.com>
2022-06-29 18:24:16 -0500

    add config type declarations

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,4 @@
-import config from './config';
+import config from '$lib/config';
 
 /** @type {import('@sveltejs/kit').GetSession} */
 export function getSession(event) {
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,13 +1,14 @@
 /**
- * Loads environment variables and sets global config values.
+ * Loads TS_PUBLIC environment variables and sets global config values.
  *
  * Default values should be configured in .env, and can be overridden using
  * shell environment variables.
  *
  * Values that can be exposed client-side without security concerns should be
  * prefixed `TS_PUBLIC_`. Values that should only be available server-side
- * should be prefixed `TS_PRIVATE_`, and should not be included in .env (since
- * it is checked into version control).
+ * should be prefixed `TS_` (no `PUBLIC_`), and should not be included in .env
+ * (since it is checked into version control). Those variables don't require
+ * special processing and may be accessed server-side as `process.env.TS_VAR`.
  *
  * This file cannot be imported directly into client-side code! It is imported
  * by `hooks/index.ts` and public-facing config values are exposed client-side
@@ -16,22 +17,30 @@
  */
 import 'dotenv/config';
 
-const publicPrefix = 'TS_PUBLIC_';
+const prefix = 'TS_PUBLIC_';
 
-const publicEnvKeys = Object.keys(process.env)
-	.filter((key) => key.startsWith(publicPrefix))
-	.map((key) => key.replace(publicPrefix, ''));
+// get list of environment keys beginning with TS_PUBLIC_
+const envKeys = Object.keys(process.env)
+	.filter((key) => key.startsWith(prefix))
+	.map((key) => key.replace(prefix, ''));
 
-const publicConfig = {};
+const config = {};
 
-function configurePublicVar(configKey: string, pattern: string, setFn: Function) {
-	const envData = {};
-	for (const key of publicEnvKeys) {
+interface Env {
+	[key: string]: string;
+}
+
+/**
+ * Utility function for safely adding TS_PUBLIC_ environment variables to config
+ */
+function configureEnvVar(configKey: string, pattern: string, setFn: Function) {
+	const env: Env = {};
+	for (const key of envKeys) {
 		if (key.startsWith(pattern)) {
-			envData[key] = process.env[`${publicPrefix}${key}`];
+			env[key] = process.env[`${prefix}${key}`];
 		}
 	}
-	publicConfig[configKey] = setFn(envData);
+	config[configKey] = setFn(env);
 }
 
 /**
@@ -39,17 +48,19 @@ function configurePublicVar(configKey: string, pattern: string, setFn: Function)
  * - some site features depend on whether we run prod, staging or local dev
  * - defaults to "local"
  */
-configurePublicVar('siteMode', 'SITE_MODE', ({ SITE_MODE = 'local' }) => {
+type SiteMode = 'production' | 'staging' | 'local';
+
+configureEnvVar('siteMode', 'SITE_MODE', ({ SITE_MODE = 'local' }) => {
 	if (!['production', 'staging', 'local'].includes(SITE_MODE)) {
 		throw new Error(`Bad site mode ${SITE_MODE}`);
 	}
-	return SITE_MODE;
+	return SITE_MODE as SiteMode;
 });
 
 /**
  * Load Backend URL and fail loudly if not set
  */
-configurePublicVar('backendUrl', 'BACKEND_URL', ({ BACKEND_URL }) => {
+configureEnvVar('backendUrl', 'BACKEND_URL', ({ BACKEND_URL }) => {
 	if (!BACKEND_URL) {
 		throw new Error('TS_PUBLIC_BACKEND_URL missing');
 	} else if (BACKEND_URL.endsWith('/')) {
@@ -62,7 +73,7 @@ configurePublicVar('backendUrl', 'BACKEND_URL', ({ BACKEND_URL }) => {
  * Backend internal URL (optional); set this when running frontend and backend on the
  * same host or same local network (e.g., production). See: hooks/index.ts:externalFetch
  */
-configurePublicVar('backendInternalUrl', 'BACKEND_INTERNAL_URL', ({ BACKEND_INTERNAL_URL }) => {
+configureEnvVar('backendInternalUrl', 'BACKEND_INTERNAL_URL', ({ BACKEND_INTERNAL_URL }) => {
 	if (BACKEND_INTERNAL_URL?.endsWith('/')) {
 		throw new Error(`Backend internal URL cannot end with slash: ${BACKEND_INTERNAL_URL}`);
 	}
@@ -72,25 +83,35 @@ configurePublicVar('backendInternalUrl', 'BACKEND_INTERNAL_URL', ({ BACKEND_INTE
 /**
  * Load Ghost API credentials and warn if not available.
  */
-configurePublicVar('ghost', 'GHOST_', (env) => {
+interface GhostConfig {
+	contentApiKey: string;
+	apiUrl: string;
+}
+
+configureEnvVar('ghost', 'GHOST_', (env: Env) => {
 	const contentApiKey = env.GHOST_CONTENT_API_KEY;
 	const apiUrl = env.GHOST_API_URL;
 	if (!contentApiKey || !apiUrl) {
 		console.warn('You need configure Ghost API keys to render the blog');
 	}
-	return { contentApiKey, apiUrl };
+	return { contentApiKey, apiUrl } as GhostConfig;
 });
 
 /**
  * Load Typesense config options and warn if not available.
  */
-configurePublicVar('typesense', 'TYPESENSE_API_', (env) => {
+interface TypesenseConfig {
+	apiKey: string;
+	apiUrl: string;
+}
+
+configureEnvVar('typesense', 'TYPESENSE_API_', (env: Env) => {
 	const apiKey = env.TYPESENSE_API_KEY;
 	const apiUrl = env.TYPESENSE_API_URL;
 	if (!apiKey || !apiUrl) {
 		console.warn('You need to configure Typesense options to enable search');
 	}
-	return { apiKey, apiUrl };
+	return { apiKey, apiUrl } as TypesenseConfig;
 });
 
 /**
@@ -99,14 +120,27 @@ configurePublicVar('typesense', 'TYPESENSE_API_', (env) => {
  *
  * See: checkChainMaintenance
  */
-configurePublicVar('chainsUnderMaintenance', 'CHAINS_UNDER_MAINTENANCE', (env) => {
+interface ChainsUnderMaintenance {
+	[key: string]: string;
+}
+
+configureEnvVar('chainsUnderMaintenance', 'CHAINS_UNDER_MAINTENANCE', (env: Env) => {
 	const jsonStr = env.CHAINS_UNDER_MAINTENANCE || '{}';
 	try {
-		return JSON.parse(jsonStr);
+		return JSON.parse(jsonStr) as ChainsUnderMaintenance;
 	} catch (e) {
 		console.warn('TS_PUBLIC_CHAINS_UNDER_MAINTENANCE is not valid JSON', jsonStr);
-		return {};
+		return {} as ChainsUnderMaintenance;
 	}
 });
 
-export default publicConfig;
+export interface Config {
+	siteMode: SiteMode;
+	backendUrl: string;
+	backendInternalUrl: string;
+	ghost: GhostConfig;
+	typesense: TypesenseConfig;
+	chainsUnderMaintenance: ChainsUnderMaintenance;
+}
+
+export default config as Config;
