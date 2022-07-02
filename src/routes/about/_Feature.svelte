b0649f690e0cb0411b92a8ca4555e976dd1d3971
Ken Kunz <kennethkunz@gmail.com>
2022-07-01 23:29:23 -0500

    about page - platform / features (with images)

---
+++ src/routes/about/_Feature.svelte
@@ -0,0 +1,77 @@
+<script context="module" lang="ts">
+	const imageAssets = import.meta.globEager('./feature-images/*.svg');
+
+	function getImageAsset(fileName: string) {
+		for (const key of Object.keys(imageAssets)) {
+			if (key.endsWith(fileName)) return imageAssets[key].default;
+		}
+	}
+</script>
+
+<script lang="ts">
+	export let title: string;
+	export let image: string;
+
+	const imageAsset = getImageAsset(image);
+</script>
+
+<div class="row">
+	<div class="col-md col-image">
+		<img src={imageAsset} alt={title} />
+	</div>
+
+	<div class="col-md col-text">
+		<h5>{title}</h5>
+		<ul>
+			<slot />
+		</ul>
+	</div>
+</div>
+
+<style>
+	.row {
+		display: flex;
+		margin-top: 4em;
+	}
+
+	.row:nth-child(even) {
+		flex-direction: row-reverse;
+	}
+
+	.col-text {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
+
+	.col-text > * {
+		margin: 0 1rem;
+	}
+
+	.col-text h5 {
+		margin-bottom: 1rem;
+	}
+
+	.col-image {
+		display: flex;
+		justify-content: center;
+	}
+
+	.col-image img {
+		object-fit: scale-down;
+		width: 65%;
+		margin: 2rem 0;
+	}
+
+	@media (max-width: 768px) {
+		.row {
+			margin-top: 2rem;
+		}
+	}
+
+	@media (max-width: 576px) {
+		.col-image img {
+			width: 80%;
+		}
+	}
+</style>
--- src/routes/about/_Platform.svelte
+++ src/routes/about/_Platform.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	import Feature from './_Feature.svelte';
+</script>
+
 <section class="container">
 	<div class="row">
 		<div class="col text-center">
@@ -6,6 +10,58 @@
 			<p>Read how Trading Strategy protocol compares to yield farming and quantative hedge funds.</p>
 		</div>
 	</div>
+
+	<Feature title="Active on-chain trading" image="blockchain-as-train.svg">
+		<li>
+			Protocol compatibility with 1m+ trading pairs on all major decentralised markets and blockchains such as Uniswap,
+			PancakeSwap, Ethereum and Binance Smart Chain
+		</li>
+		<li>
+			Market signal analysis and trade entries/exits are executed on the decentralised Trading Strategy Oracle network,
+			which is running off-chain and utilises server capacity not available on “pure” smart contracts
+		</li>
+		<li>Multipair and portfolio rebalancing strategies are supported.</li>
+	</Feature>
+
+	<Feature title="100% control of your funds" image="database-1.svg">
+		<li>
+			Deposit and withdraw in real-time. As a non-custodial protocol, there is no counter-party risk with fund
+			management or trade execution
+		</li>
+		<li>Invest into and exit a strategy whenever you chose</li>
+	</Feature>
+
+	<Feature title="Protocol security" image="security-2.svg">
+		<li>Smart contracts allow fund transfer only into trades and withdrawals back to the investor’s own wallet</li>
+		<li>
+			Smart contract based fund management and Oracle consensus: No centralised parties that can enable insider fraud
+		</li>
+		<li>Strategies with low liquidity assets receive higher risk score</li>
+		<li>Community vetting of strategies</li>
+		<li>Before public access, testing live trading with private capital</li>
+		<li>Audit partners to audit and verify smart contract code</li>
+	</Feature>
+
+	<Feature title="Market data" image="analyze-data-3.svg">
+		<li>
+			We collect raw market data for 1m+ trading pairs directly from decentralised exchanges on multiple integrated
+			blockchains
+		</li>
+		<li>Converts raw data to the familiar OHLCV candles and XY liquidity format</li>
+	</Feature>
+
+	<Feature title="Real-time API (REST)" image="analyze-data-1.svg">
+		<li>Price, volume and liquidity data for decentralised exchanges</li>
+		<li>Trading pairs</li>
+		<li>OHLCV candles for pricing</li>
+		<li>OHLC candles for exchange liquidity</li>
+	</Feature>
+
+	<Feature title="Backtesting data" image="database-5.svg">
+		<li>Price, volume and liquidity data for decentralised exchanges (DEX)</li>
+		<li>Parquet file format</li>
+		<li>Large files</li>
+	</Feature>
 </section>
 
 <style>
