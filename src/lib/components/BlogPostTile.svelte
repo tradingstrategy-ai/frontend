f0aa37c3ace58a5bb2a726234312a9bd348255c6
Ken Kunz <kennethkunz@gmail.com>
2022-07-25 15:00:14 -0500

    add BlogPostTile component

---
+++ src/lib/components/BlogPostTile.svelte
@@ -0,0 +1,111 @@
+<script lang="ts">
+	import { formatDistanceToNow } from 'date-fns';
+
+	export let featured = false;
+	export let slug: string;
+	export let title: string;
+	export let excerpt: string;
+	export let imageUrl: string;
+	export let imageAltText: string;
+	export let publishedAt: string;
+
+	const publishedDate = new Date(publishedAt);
+</script>
+
+<a class:featured href={`/blog/${slug}`}>
+	<img src={imageUrl} alt={imageAltText} />
+
+	<div class="info">
+		<h3>{title}</h3>
+
+		<time datetime={publishedAt}>
+			{publishedDate.toDateString()},
+			{formatDistanceToNow(publishedDate, { addSuffix: true })}
+		</time>
+
+		<p>{excerpt}</p>
+	</div>
+</a>
+
+<style>
+	a {
+		display: grid;
+		grid-template-rows: auto 1fr;
+		row-gap: 1rem;
+		align-items: start;
+		text-decoration: none;
+		--transition-duration: 0.25s;
+	}
+
+	img {
+		width: 100%;
+		aspect-ratio: 7 / 4;
+		object-fit: cover;
+		transition: opacity var(--transition-duration);
+	}
+
+	a:hover img {
+		opacity: 0.9;
+	}
+
+	.info {
+		display: grid;
+		gap: 0.75rem;
+	}
+
+	h3 {
+		font: var(--f-h2-medium);
+		text-decoration: underline transparent;
+		transition: text-decoration-color var(--transition-duration);
+	}
+
+	a:hover h3 {
+		text-decoration-color: inherit;
+	}
+
+	time {
+		font: 400 var(--fs-ui-lg);
+	}
+
+	p {
+		font: var(--f-text-body-regular);
+	}
+
+	/* Desktop overrides */
+	@media (min-width: 1024px) {
+		img {
+			aspect-ratio: 5 / 3;
+		}
+
+		.info {
+			gap: 1.25rem;
+		}
+
+		a.featured {
+			/* Span across container's columns */
+			grid-column: 1 / -1;
+			/* Use horizontal layout (image left, info right) */
+			grid-template-columns: 1fr 1fr;
+			grid-template-rows: auto;
+			/* Set --blog-post-tile--column-gap to control the column gap */
+			column-gap: var(--blog-post-tile--column-gap, min(2.5rem, 3vw));
+			align-items: center;
+		}
+
+		.featured img {
+			aspect-ratio: 7 / 6;
+		}
+
+		.featured .info {
+			gap: 0.75rem;
+		}
+
+		.featured h3 {
+			font: var(--f-h1-medium);
+		}
+
+		.featured time {
+			font: 400 var(--fs-ui-xl);
+		}
+	}
+</style>
