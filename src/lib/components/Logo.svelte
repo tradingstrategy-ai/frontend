b2b86967502cb9112a4e94bbbcdabcc0616f6257
Ken Kunz <kennethkunz@gmail.com>
2022-07-25 13:09:05 -0500

    add Logo component

---
+++ src/lib/components/Logo.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import svg from '$lib/assets/logo-horizontal.svg?raw';
+</script>
+
+<div>
+	{@html svg}
+</div>
+
+<style>
+	div {
+		display: contents;
+	}
+
+	div :global(svg) {
+		width: var(--logo-width, auto);
+		height: var(--logo-height, auto);
+	}
+
+	div :global(.logotype) {
+		fill: var(--c-logotype);
+	}
+</style>
