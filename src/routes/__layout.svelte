1f8efea9c827dfe816c6b0ff7689173389e46252
Mikko Ohtamaa <mikko@opensourcehacker.com>
2022-08-06 00:26:55 +0200

    Prettify

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,114 +2,112 @@
 	Root layout
 -->
 <script lang="ts">
-    import Navbar from '$lib/header/Navbar.svelte';
-    import AppHead from '$lib/header/AppHead.svelte';
-    import SiteMode from '$lib/header/SiteMode.svelte';
-    import PageLoadProgressBar from '$lib/header/PageLoadProgressBar.svelte';
-    import Footer from '$lib/components/Footer.svelte';
-    import { beforeNavigate } from "$app/navigation";
-    import { browser } from "$app/env";
-    import 'bootstrap-theme/css/neumorphism.css';
-    import '$lib/components/css/index.css';
+	import Navbar from '$lib/header/Navbar.svelte';
+	import AppHead from '$lib/header/AppHead.svelte';
+	import SiteMode from '$lib/header/SiteMode.svelte';
+	import PageLoadProgressBar from '$lib/header/PageLoadProgressBar.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+	import { beforeNavigate } from '$app/navigation';
+	import { browser } from '$app/env';
+	import 'bootstrap-theme/css/neumorphism.css';
+	import '$lib/components/css/index.css';
 
+	/**
+	 * Lazily load fonts as per issue 9.
+	 *
+	 * See also app.html for <body> <script> handle.
+	 *
+	 * - https://github.com/tradingstrategy-ai/design-system/issues/9
+	 * - https://kit.svelte.dev/docs/modules#$app-navigation-beforenavigation
+	 *
+	 * To test this set `fontWarmup` to `false` in dev console:
+	 *
+	 * ```javascript
+	 * window.localStorage.setItem("fontWarmup", "false");
+	 * ```
+	 */
+	function toggleFontLoad() {
+		if (browser) {
+			const fontStylesheet = document.getElementById('font-stylesheet');
+			if (fontStylesheet) {
+				fontStylesheet.setAttribute('rel', 'stylesheet');
+			}
+		}
+	}
 
-    /**
-     * Lazily load fonts as per issue 9.
-     *
-     * See also app.html for <body> <script> handle.
-     *
-     * - https://github.com/tradingstrategy-ai/design-system/issues/9
-     * - https://kit.svelte.dev/docs/modules#$app-navigation-beforenavigation
-     *
-     * To test this set `fontWarmup` to `false` in dev console:
-     *
-     * ```javascript
-     * window.localStorage.setItem("fontWarmup", "false");
-     * ```
-     */
-    function toggleFontLoad() {
-        if(browser) {
-            const fontStylesheet = document.getElementById("font-stylesheet");
-            if(fontStylesheet) {
-                fontStylesheet.setAttribute("rel", "stylesheet");
-            }
-        }
-    }
-
-    beforeNavigate((navigation) => {
-        toggleFontLoad();
-    });
-
+	beforeNavigate((navigation) => {
+		toggleFontLoad();
+	});
 </script>
 
-<AppHead/>
+<AppHead />
 
-<PageLoadProgressBar/>
-<Navbar/>
-<slot/>
-<Footer/>
-<SiteMode/>
+<PageLoadProgressBar />
+<Navbar />
+<slot />
+<Footer />
+<SiteMode />
 
 <style global>
-    /**
+	/**
      * Custom media declarations (via PostCSS Custom Media plugin) - enables `@media (--var-name) {}`
      * Must be declared in __layout to ensure proper CSS load order in SSR.
      */
-    @custom-media --viewport-md-up (width >= 768px);
-    @custom-media --viewport-lg-up (width >= 1024px);
-    @custom-media --viewport-md-down (width < 1024px);
-    @custom-media --viewport-sm-down (width < 768px);
+	@custom-media --viewport-md-up (width >= 768px);
+	@custom-media --viewport-lg-up (width >= 1024px);
+	@custom-media --viewport-md-down (width < 1024px);
+	@custom-media --viewport-sm-down (width < 768px);
 
-    :root {
-        /* Old CSS color vars still used in various components (refactor/remove) */
-        --price-up-green: var(--c-bullish);
-        --price-down-red: var(--c-bearish);
-        --link-underline: var(--c-bullish-dark);
-        --badge-exchange: var(--c-bullish-dark);
-        --badge-token: #b99537;
-        --badge-pair: #496abf;
-    }
+	:root {
+		/* Old CSS color vars still used in various components (refactor/remove) */
+		--price-up-green: var(--c-bullish);
+		--price-down-red: var(--c-bearish);
+		--link-underline: var(--c-bullish-dark);
+		--badge-exchange: var(--c-bullish-dark);
+		--badge-token: #b99537;
+		--badge-pair: #496abf;
+	}
 
-    a.body-link {
-        border-bottom: 1px solid var(--link-underline);
-    }
+	a.body-link {
+		border-bottom: 1px solid var(--link-underline);
+	}
 
-    a.body-link:hover {
-        color: var(--link-underline);
-    }
+	a.body-link:hover {
+		color: var(--link-underline);
+	}
 
-    /* Price action and candle colors */
-    .price-change-green {
-        color: var(--c-bullish-dark);
-    }
+	/* Price action and candle colors */
+	.price-change-green {
+		color: var(--c-bullish-dark);
+	}
 
-    .price-change-red {
-        color: var(--c-bearish-dark);
-    }
+	.price-change-red {
+		color: var(--c-bearish-dark);
+	}
 
-    /* Entity badge colors */
-    .badge-exchange {
-        background-color: var(--badge-exchange);
-    }
+	/* Entity badge colors */
+	.badge-exchange {
+		background-color: var(--badge-exchange);
+	}
 
-    .badge-token {
-        background-color: var(--badge-token);
-    }
+	.badge-token {
+		background-color: var(--badge-token);
+	}
 
-    .badge-pair {
-        background-color: var(--badge-pair);
-    }
+	.badge-pair {
+		background-color: var(--badge-pair);
+	}
 
-    /* Inline SVG icon set colors */
-    .icons-duotone-1 {
-        fill: var(--c-bullish);
-    }
+	/* Inline SVG icon set colors */
+	.icons-duotone-1 {
+		fill: var(--c-bullish);
+	}
 
-    .icons-duotone-2 {
-        fill: var(--c-bearish);
-    }
+	.icons-duotone-2 {
+		fill: var(--c-bearish);
+	}
 
-    .icons-duotone-1-stroke {
-        stroke: var(--c-bullish);
-    }
+	.icons-duotone-1-stroke {
+		stroke: var(--c-bullish);
+	}
 </style>
