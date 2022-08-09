7214cf384b8c021ebc90427aeaa64e606c4a7353
Ken Kunz <kennethkunz@gmail.com>
2022-08-09 11:14:48 -0500

    Theme 0.1 - update About page partners section

--- src/routes/about/_Partners.svelte
+++ src/routes/about/_Partners.svelte
@@ -1,61 +1,68 @@
 <script lang="ts">
-	import { imageUrl } from '$lib/helpers/assets';
+	import AvalancheImage from '$lib/assets/partners/avalanche.svg?raw';
+	import TypesenseImage from '$lib/assets/partners/typesense.svg?raw';
+	import TimescaleImage from '$lib/assets/partners/timescale.svg?raw';
+	import ChartIqImage from '$lib/assets/partners/chartiq.svg?raw';
+	import RepublicCapitalImage from '$lib/assets/partners/republic-capital.svg?raw';
 </script>
 
-<section class="jumbotron jumbotron-fluid">
+<section class="ds-container">
+	<h2>Our partners</h2>
+
 	<div>
-		<h2 class="text-center">Our partners</h2>
+		<a href="https://www.avax.network/" target="_blank" style:width="208px">
+			{@html AvalancheImage}
+		</a>
 
-		<div class="partner-logos">
-			<a href="https://www.avax.network/" target="_blank">
-				<img src={imageUrl('partners/avalanche.svg')} alt="Avalanche" style="width: 208px; height: 43px;" />
-			</a>
+		<a href="https://typesense.org/" target="_blank" style:width="150px">
+			{@html TypesenseImage}
+		</a>
 
-			<a href="https://typesense.org/" target="_blank">
-				<img src={imageUrl('partners/typesense.svg')} alt="Typesense" style="width: 150px; height: 33px;" />
-			</a>
+		<a href="https://www.timescale.com/" target="_blank" style:width="170px">
+			{@html TimescaleImage}
+		</a>
 
-			<a href="https://www.timescale.com/" target="_blank">
-				<img src={imageUrl('partners/timescale.svg')} alt="Timescale" style="width: 170px; height:45px;" />
-			</a>
+		<a href="https://cosaic.io/chartiq/" target="_blank" style:width="127px">
+			{@html ChartIqImage}
+		</a>
 
-			<a href="https://cosaic.io/chartiq/" target="_blank">
-				<img src={imageUrl('partners/chartiq.svg')} alt="ChartIQ" style="width: 127px; height: 47px;" />
-			</a>
-
-			<a href="https://www.republiccapital.co/" target="_blank">
-				<img
-					src={imageUrl('partners/republic-capital.svg')}
-					alt="Republic Capital"
-					style="width: 60px; height: 60px;"
-				/>
-			</a>
-		</div>
+		<a href="https://www.republiccapital.co/" target="_blank" style:width="60px">
+			{@html RepublicCapitalImage}
+		</a>
 	</div>
 </section>
 
 <style>
 	section {
-		background-color: var(--c-parchment);
+		grid-template-columns: auto;
+		justify-content: center;
+		gap: 4rem;
+		padding-top: 2.5rem;
+		padding-bottom: 2.5rem;
 	}
 
-	section > div {
-		margin: 0 auto;
-		max-width: 720px;
+	h2 {
+		text-align: center;
 	}
 
-	.partner-logos {
+	div {
+		max-width: 800px;
 		display: flex;
-		flex-wrap: wrap;
+		flex-direction: column;
+		gap: 3rem 4rem;
 		justify-content: center;
 		align-items: center;
-		gap: 4rem;
-		margin: 5rem 0 1rem 0;
 	}
 
-	@media (max-width: 576px) {
-		.partner-logos {
-			flex-direction: column;
+	@media (--viewport-md-up) {
+		section {
+			gap: 5rem;
+			padding-top: 5rem;
+			padding-bottom: 5rem;
+		}
+
+		div {
+			flex-flow: row wrap;
 		}
 	}
 </style>
