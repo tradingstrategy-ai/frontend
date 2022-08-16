9344cba1d584b5332a354185b4de5d87196514e6
Ken Kunz <kennethkunz@gmail.com>
2022-08-16 11:14:17 -0500

    revert change to StaleDataWarning.svelte from 685f00

--- src/lib/chain/StaleDataWarning.svelte
+++ src/lib/chain/StaleDataWarning.svelte
@@ -1,28 +1,27 @@
-<!--
-@component
-Stale data warning
-
-Show a warning if any specific chains we have lagging or stale data.
-
-Gets list of chain slugs for which we display data and check if any of those
-slugs are on the red list.
--->
 <script>
+	/**
+	 * Show a warning if any specific chains we have lagging or stale data.
+	 *
+	 * Gets list of chain slugs for which we display data and check if any of those
+	 * slugs are on the red list.
+	 */
+
 	// List of chain slugs we give a warning for
-	// See config.ts for details
-	export const chainSlugs = Object.keys(config.chainsBadData);
+	export const chainSlugs = [];
 
 	// Set to true to display a warning if any of the blockchain sources is having an issue
 	export const allChains = false;
 </script>
 
-{#if chainSlugs.includes('polygon') || allChains}
-	<div class="alert alert-danger">
-		<a href="/trading-view/polygon">Polygon blockchain</a> data is being processed at the moment. Real-time and historical
-		prices may not be available at the moment.
-	</div>
-{/if}
+<!-- A placeholder until we get this information from the SSR hook -->
 
+<!--
+{#if chainSlugs.includes("binance") || allChains }
+    <div class="alert alert-danger">
+        <a href="/trading-view/binance">Binance Smart Chain</a> data is lagging. Real-time prices and price changes may not be available at the moment.
+    </div>
+{/if}
+-->
 <style>
 	.alert-danger a {
 		color: #a91e2c;
