ae15214796f5d0d802d166b60506c0c9c797ee3f
Ken Kunz <kennethkunz@gmail.com>
2022-08-23 17:49:06 -0500

    use wide layout on exchange index

--- src/routes/trading-view/exchanges.svelte
+++ src/routes/trading-view/exchanges.svelte
@@ -12,39 +12,46 @@
 	<meta name="description" content="Top decentralised exchanges" />
 </svelte:head>
 
-<div class="container container-main exchanges">
-	<Breadcrumbs />
-	<div class="row">
-		<div class="col-md-12">
-			<div class="exchanges">
-				<h1>Decentralised exchanges</h1>
+<main>
+	<header class="ds-container">
+		<Breadcrumbs />
+		<h1>Decentralised exchanges</h1>
+		<p>
+			Browse supported decentralised exchanges across all <a href="/trading-view/blockchains">blockchains</a> below.
+		</p>
+	</header>
 
-				<p>
-					Browse supported decentralised exchanges across all <a href="/trading-view/blockchains">blockchains</a> below.
-				</p>
+	<section class="ds-container">
+		<StaleDataWarning allChains={true} />
 
-				<StaleDataWarning allChains={true} />
-
-				<ExchangeExplorer
-					enabledColumns={['human_readable_name', 'chain_name', 'pair_count', 'usd_volume_30d']}
-					orderColumnIndex={3}
-					filterJunk={false}
-				/>
-			</div>
-		</div>
-	</div>
-</div>
+		<ExchangeExplorer
+			enabledColumns={['human_readable_name', 'chain_name', 'pair_count', 'usd_volume_30d']}
+			orderColumnIndex={3}
+			filterJunk={false}
+		/>
+	</section>
+</main>
 
 <style>
-	.exchanges :global(.col-exchange) {
-		padding-left: 0;
+	.ds-container {
+		grid-template-columns: auto;
+		gap: 0;
 	}
 
-	.exchanges :global(.col-volume) {
+	header h1 {
+		font: var(--f-h2-medium);
+		margin-bottom: 0.5rem;
+	}
+
+	section {
+		margin-top: 1rem;
+	}
+
+	section :global .col-volume {
 		width: 10%;
 	}
 
-	.exchanges :global(.col-chain-name) {
+	section :global .col-chain-name {
 		width: 25%;
 	}
 </style>
