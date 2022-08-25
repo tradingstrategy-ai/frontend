36970d3c0a33139a077fd58f13e88d6820c98032
Ken Kunz <kennethkunz@gmail.com>
2022-08-24 21:03:52 -0500

    use wide layout on token detail page

--- src/routes/trading-view/[chain]/tokens/[token]/index.svelte
+++ src/routes/trading-view/[chain]/tokens/[token]/index.svelte
@@ -58,60 +58,51 @@
 
 <Breadcrumbs labels={{ [address]: tokenDetails.name }} />
 
-<div class="container">
-	<div class="text-section">
-		<div class="row">
-			<div class="col-md-12">
-				<h1>
-					{tokenDetails.name} ({tokenDetails.symbol})
-				</h1>
-			</div>
+<main>
+	<header class="ds-container">
+		<h1>{tokenDetails.name} ({tokenDetails.symbol})</h1>
+	</header>
+
+	<section class="ds-container token-info">
+		<TokenInfoTable summary={tokenDetails} />
+
+		<div class="text-summary">
+			<p>
+				<strong>{tokenDetails.name}</strong> is a {tokenStandardName} token on
+				<a class="body-link" href="/trading-view/{chain_slug}">{tokenDetails.chain_name} blockchain</a>. It trades under
+				<strong>{tokenDetails.symbol}</strong> ticker.
+			</p>
+
+			<p>
+				<strong>{tokenDetails.name}</strong> token supply is
+				{formatAmount(parseFloat(tokenDetails.total_supply))}
+				<strong>{tokenDetails.symbol}s</strong>.
+			</p>
+
+			{#if tokenDetails.pair_count}
+				<p>
+					There are total {formatAmount(tokenDetails.pair_count)} pairs trading against
+					<strong>{tokenDetails.symbol}</strong>.
+				</p>
+			{/if}
+
+			<p class="smart-contract-address">
+				The token smart contract address is
+				<a class="body-link" href={tokenDetails.explorer_link}> {tokenDetails.address}</a>.
+			</p>
+
+			<p>
+				The information on this page is for <a class="body-link" href="/trading-view/{chain_slug}"
+					>{tokenDetails.chain_name}</a
+				>.
+				<strong>{tokenDetails.symbol}</strong> presentations bridged and wrapped on other blockchains are not included in
+				the figures.
+			</p>
 		</div>
+	</section>
 
-		<div class="row">
-			<div class="col-lg-5">
-				<TokenInfoTable summary={tokenDetails} />
-			</div>
-
-			<div class="col-lg-7">
-				<p>
-					<strong>{tokenDetails.name}</strong> is a {tokenStandardName} token on
-					<a class="body-link" href="/trading-view/{chain_slug}">{tokenDetails.chain_name} blockchain</a>. It trades
-					under
-					<strong>{tokenDetails.symbol}</strong> ticker.
-				</p>
-
-				<p>
-					<strong>{tokenDetails.name}</strong> token supply is
-					{formatAmount(parseFloat(tokenDetails.total_supply))}
-					<strong>{tokenDetails.symbol}s</strong>.
-				</p>
-
-				{#if tokenDetails.pair_count}
-					<p>
-						There are total {formatAmount(tokenDetails.pair_count)} pairs trading against
-						<strong>{tokenDetails.symbol}</strong>.
-					</p>
-				{/if}
-
-				<p class="smart-contract-address">
-					The token smart contract address is
-					<a class="body-link" href={tokenDetails.explorer_link}> {tokenDetails.address}</a>.
-				</p>
-
-				<p>
-					The information on this page is for <a class="body-link" href="/trading-view/{chain_slug}"
-						>{tokenDetails.chain_name}</a
-					>.
-					<strong>{tokenDetails.symbol}</strong> presentations bridged and wrapped on other blockchains are not included
-					in the figures.
-				</p>
-			</div>
-		</div>
-	</div>
-
-	<div class="pair-explorer-wrapper">
-		<h1>Trading pairs</h1>
+	<section class="ds-container trading-pairs">
+		<h2>Trading pairs</h2>
 
 		<StaleDataWarning allChains={true} />
 
@@ -130,7 +121,9 @@
 			tokenSymbol={tokenDetails.symbol}
 			tokenAddress={address}
 		/>
+	</section>
 
+	<section class="ds-container">
 		<p>
 			<small>
 				* Trading pairs with complications, like the lack of liquidity, might not be displayed.
@@ -139,17 +132,28 @@
 				</a>
 			</small>
 		</p>
-	</div>
-</div>
+	</section>
+</main>
 
 <style>
-	h1 {
-		font-size: 2rem;
-		margin-bottom: 20px;
+	main {
+		display: grid;
+		gap: 1rem;
 	}
 
-	.text-section {
-		margin-top: 20px;
+	h1,
+	h2 {
+		font: var(--f-h3-medium);
+	}
+
+	.token-info {
+		grid-template-columns: auto;
+	}
+
+	.text-summary {
+		align-self: start;
+		display: grid;
+		gap: 1rem;
 	}
 
 	.smart-contract-address {
@@ -157,11 +161,18 @@
 		text-overflow: ellipsis;
 	}
 
-	.pair-explorer-wrapper {
-		margin-bottom: 60px;
+	.trading-pairs {
+		grid-template-columns: auto;
+		gap: 0.5rem;
 	}
 
 	small {
 		opacity: 0.3;
+	}
+
+	@media (--viewport-lg-up) {
+		.token-info {
+			grid-template-columns: 4fr 5fr;
+		}
 	}
 </style>
