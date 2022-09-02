63a058627cff799b46b10974e250aaa3e8cbf5ec
Ken Kunz <kennethkunz@gmail.com>
2022-09-02 14:39:37 -0500

    svelte-migrate: sitemap.xml

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,6 +1,27 @@
 /**
- * sitemap.xml generation
+ * Generate a sitemap for static pages and other sitemaps
  */
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = ({ url }) => {
+	let { protocol, host } = url;
+
+	// TODO: configure adapter-node to use correct Cloudflare X-Forwarded- headers
+	// https://github.com/sveltejs/kit/tree/master/packages/adapter-node#origin-protocol_header-and-host_header
+	if (!host || host === 'undefined') {
+		host = 'tradingstrategy.ai';
+		protocol = 'https:';
+	}
+
+	const baseUrl = `${protocol}//${host}`;
+
+	const headers = {
+		'content-type': 'application/xml',
+		'cache-control': 'public, max-age=600'
+	};
+
+	return new Response(render(baseUrl), { headers });
+};
 
 // This sitemap is manually maintained and page paths included here one by one
 const staticPages = [
@@ -10,9 +31,14 @@ const staticPages = [
 	'trading-view/backtesting',
 	'trading-view/exchanges',
 	'trading-view/api',
+	'trading-view/avalanche',
+	'trading-view/avalanche/tokens',
+	'trading-view/binance',
 	'trading-view/binance/tokens',
-	'trading-view/polygon/tokens',
+	'trading-view/ethereum',
 	'trading-view/ethereum/tokens',
+	'trading-view/polygon',
+	'trading-view/polygon/tokens',
 	'trading-view/blockchains',
 	'trading-view/trading-pairs',
 	'trading-view/top-list',
@@ -30,56 +56,22 @@ const staticPages = [
 	'api/sitemap/pairs/paged/0.xml',
 	'api/sitemap/pairs/paged/1.xml',
 	'api/sitemap/pairs/paged/2.xml',
+	'api/sitemap/pairs/paged/3.xml',
+	'api/sitemap/pairs/paged/4.xml',
 	'api/sitemap/exchanges/sitemap.xml'
 ];
 
-/***
- * Sitemap.xml HTTP GET endpoint.
- *
- * Generates a sitemap for static pages and other sitemaps.
- *
- * For more information see https://stackoverflow.com/a/69523302/315168
- *
- * @type {import('@sveltejs/kit').RequestHandler}
- *
- */
-export async function GET({ url }) {
-	console.log('Downloading sitemap for', url);
+// See https://en.wikipedia.org/wiki/Sitemaps
+const render = (baseUrl: string) => `<?xml version="1.0" encoding="utf-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+${renderFragments(baseUrl).join('')}
+</urlset>`;
 
-	// https://developer.mozilla.org/en-US/docs/Web/API/URL
-	const proto = url.protocol;
-
-	let host = url.host;
-	if (!host || host === 'undefined') {
-		// TODO: No idea why we are getting "undefined" as string on the production server
-		// throw new Error(`Sitemap generator did not get host: ${url}`);
-		host = 'tradingstrategy.ai';
-	}
-
-	// Generate the sitemap.xml file with string fiddling
-	const fragments = [];
-	for (let path of staticPages) {
-		const fullUrl = `${proto}//${host}/${path}`;
-		fragments.push(`<url><loc>${fullUrl}</loc></url>`);
-	}
-
-	// Build the XML for pages
-	const urlXml = ''.concat(...fragments);
-
-	// See https://en.wikipedia.org/wiki/Sitemaps
-	const xml = `<?xml version="1.0" encoding="utf-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-        ${urlXml}
-    </urlset>`;
-
-	return {
-		status: 200,
-		body: xml,
-		//https://stackoverflow.com/questions/3272534/what-content-type-value-should-i-send-for-my-xml-sitemap
-		headers: {
-			'Content-Type': 'application/xml'
-		}
-	};
-}
+const renderFragments = (baseUrl: string) => {
+	return staticPages.map((path) => {
+		const fullUrl = `${baseUrl}/${path}`;
+		return `<url><loc>${fullUrl}</loc></url>`;
+	});
+};
