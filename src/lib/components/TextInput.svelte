399c05d89327fe11a93f0b8fa7b18aba768ccd06
Ken Kunz <kennethkunz@gmail.com>
2022-10-07 13:34:49 -0500

    add DropDown component

--- src/lib/components/TextInput.svelte
+++ src/lib/components/TextInput.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
 	import { Icon } from '$lib/components';
 
-	type TextInputType = 'email' | 'text' | 'search';
-	export let type: TextInputType = 'text';
+	export let type: 'email' | 'text' | 'search' = 'text';
 	export let disabled = false;
-	type TextInputSize = 'sm' | 'md' | 'lg' | 'xl';
-	export let size: TextInputSize = 'md';
+	export let size: 'sm' | 'md' | 'lg' | 'xl' = 'md';
 	export let value = '';
 
 	const icon = type === 'search' ? 'search' : undefined;
@@ -36,88 +34,92 @@
 	{/if}
 </span>
 
-<style>
+<style lang="postcss">
 	.wrapper {
 		position: relative;
 		display: inline-grid;
 		width: var(--text-input-width, auto);
 		max-width: var(--text-input-max-width, auto);
 		font-weight: 400;
+
+		&.disabled {
+			opacity: 0.65;
+		}
+
+		&.has-icon input {
+			text-indent: 1.125em;
+		}
+
+		& :global svg {
+			position: absolute;
+			top: 50%;
+			transform: translateY(-50%);
+			left: 0.625em;
+			font-size: 0.875em;
+		}
 	}
 
 	.size-sm {
 		font: var(--fs-ui-sm);
+		height: 2rem;
 	}
 
 	.size-md {
 		font: var(--fs-ui-md);
+		height: 2.25rem;
 	}
 
 	.size-lg {
 		font: var(--fs-ui-lg);
+		height: 2.625rem;
 	}
 
 	.size-xl {
 		font: var(--fs-ui-xl);
+		height: 3rem;
 	}
 
 	input {
 		width: inherit;
 		padding: 0 0.5em;
-		height: 2.125em;
-		border-radius: 0.5rem;
 		border: 2px solid var(--c-border-2);
+		border-radius: 0.5rem;
 		background: var(--c-body);
-		letter-spacing: 0.01em;
 		color: var(--c-text-1);
-	}
 
-	input::placeholder {
-		color: var(--c-text-7);
-	}
+		&::placeholder {
+			color: var(--c-text-7);
+		}
 
-	input:disabled {
-		background: var(--c-background-2);
-	}
+		&:disabled {
+			background: var(--c-background-2);
+		}
 
-	input:focus {
-		outline: var(--c-background-4) solid 2px;
-		outline-offset: 0px;
-		box-shadow: 0 0 8px 2px var(--c-background-2);
-	}
+		&:focus {
+			outline: var(--c-background-4) solid 2px;
+			outline-offset: 0px;
+			box-shadow: 0 0 8px 2px var(--c-background-2);
+		}
 
-	.disabled {
-		opacity: 0.65;
-	}
-
-	.has-icon input {
-		text-indent: 1.125em;
-	}
-
-	.wrapper :global(svg) {
-		position: absolute;
-		top: 0.75em;
-		left: 0.625em;
-		font-size: 0.875em;
-	}
-
-	input[type='search']::-webkit-search-cancel-button {
-		-webkit-appearance: none;
+		&[type='search']::-webkit-search-cancel-button {
+			-webkit-appearance: none;
+		}
 	}
 
 	.cancel-btn {
 		display: none;
+
+		& :global svg {
+			left: auto;
+			right: 0.625em;
+			font-size: 0.7em;
+		}
 	}
 
-	.wrapper:focus-within .cancel-btn,
-	.wrapper:hover .cancel-btn {
-		display: inline;
-	}
-
-	.cancel-btn :global(svg) {
-		left: auto;
-		font-size: 0.7em;
-		top: 1em;
-		right: 0.625em;
+	.wrapper {
+		&:focus-within .cancel-btn,
+		&:hover .cancel-btn {
+			display: contents;
+		}
 	}
 </style>
