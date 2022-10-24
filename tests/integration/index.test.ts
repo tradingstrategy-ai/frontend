3d486f673fce379c32208f71ced25cb333c31d65
Ken Kunz <kennethkunz@gmail.com>
2022-10-24 11:57:51 -0500

    add screenshot test to verify custom fonts are loaded

--- tests/integration/index.test.ts
+++ tests/integration/index.test.ts
@@ -1,15 +1,25 @@
 import { expect, test } from '@playwright/test';
 
-test('home page has impressive numbers', async ({ page }) => {
-	await page.goto('/');
-	const section = page.getByText(/Market data and trading strategy framework/);
+test.describe('home page', () => {
+	test.beforeEach(async ({ page }) => {
+		await page.goto('/');
+	});
 
-	const pairs = section.getByText('15,000 trading pairs');
-	await expect(pairs).toHaveAttribute('href', '/trading-view/trading-pairs');
+	test('home page uses custom heading font', async ({ page }) => {
+		const heading = page.getByRole('heading', { level: 1 });
+		await expect(heading).toHaveScreenshot();
+	});
 
-	const liquidity = section.getByText('$1.23B liquidity');
-	await expect(liquidity).toHaveAttribute('href', '/trading-view/trading-pairs');
+	test('home page has impressive numbers', async ({ page }) => {
+		const section = page.getByText(/Market data and trading strategy framework/);
 
-	const blockchains = section.getByText(/3 blockchains/);
-	await expect(blockchains).toHaveAttribute('href', '/trading-view/blockchains');
+		const pairs = section.getByText('15,000 trading pairs');
+		await expect(pairs).toHaveAttribute('href', '/trading-view/trading-pairs');
+
+		const liquidity = section.getByText('$1.23B liquidity');
+		await expect(liquidity).toHaveAttribute('href', '/trading-view/trading-pairs');
+
+		const blockchains = section.getByText(/3 blockchains/);
+		await expect(blockchains).toHaveAttribute('href', '/trading-view/blockchains');
+	});
 });
