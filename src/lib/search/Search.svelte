b05f8a3b30da22ee24d1f0d3ce80b681241d6562
Ken Kunz <kennethkunz@gmail.com>
2022-10-25 18:06:41 -0500

    update navbar search UX on mobile

--- src/lib/search/Search.svelte
+++ src/lib/search/Search.svelte
@@ -10,11 +10,9 @@ Display site-wide search box for use in top-nav.
 ```
 -->
 <script lang="ts">
-	import { fade } from 'svelte/transition';
-	import { goto } from '$app/navigation';
 	import tradingEntities from './trading-entities';
 	import TradingEntityHit from './TradingEntityHit.svelte';
-	import { TextInput, Button } from '$lib/components';
+	import { Button, Icon, TextInput } from '$lib/components';
 
 	let q = '';
 	let hasFocus = false;
@@ -27,69 +25,125 @@ Display site-wide search box for use in top-nav.
 
 	$: hits = q ? $tradingEntities.hits : [];
 
-	function handleKeydown({ key }: { key: string }) {
-		if (key === 'Enter') {
-			goto(`/search?q=${q}`);
-		}
+	async function toggleFocus() {
+		setTimeout(() => (hasFocus = !hasFocus));
 	}
 </script>
 
-<div class="search" on:focus|capture={() => (hasFocus = true)} on:blur|capture={() => (hasFocus = false)}>
-	<TextInput
-		bind:value={q}
-		type="search"
-		placeholder="Search"
-		autocapitalize="none"
-		spellcheck="false"
-		on:keydown={handleKeydown}
-	/>
+<div class="search" class:hasFocus on:focus|capture={toggleFocus} on:blur|capture={toggleFocus}>
+	<label class="mobile-only" for="search-input-mobile">
+		<Icon name="search" />
+	</label>
 
-	{#if hasFocus}
-		<div class="results" transition:fade={{ duration: 250 }}>
+	<form class="desktop-only" action="/search">
+		<!-- prettier-ignore -->
+		<TextInput
+			bind:value={q}
+			name="q"
+			type="search"
+			placeholder="Search"
+			autocomplete="off"
+			autocapitalize="none"
+			spellcheck="false"
+		/>
+	</form>
+
+	<div class="results">
+		<form class="mobile-only" action="/search">
+			<TextInput
+				bind:value={q}
+				id="search-input-mobile"
+				name="q"
+				size="lg"
+				type="search"
+				placeholder="Search"
+				autocomplete="off"
+				autocapitalize="none"
+				spellcheck="false"
+			/>
+		</form>
+
+		{#if q}
+			<ul>
+				{#each hits as { document }, index (document.id)}
+					<TradingEntityHit {document} layout="basic" />
+				{/each}
+			</ul>
+		{/if}
+
+		<div class="buttons">
 			{#if q}
-				<ul>
-					{#each hits as { document }, index (document.id)}
-						<TradingEntityHit {document} layout="basic" />
-					{/each}
-				</ul>
+				<Button label="Show all results" href="/search?q={q}" />
+			{:else}
+				Search exchanges, tokens and trading pairs.
 			{/if}
-
-			<div class="buttons">
-				{#if q}
-					<Button label="Show all results" href="/search?q={q}" />
-				{:else}
-					Search exchanges, tokens and trading pairs.
-				{/if}
-				<Button secondary label="Advanced search" href="/search?q={q}" />
-			</div>
+			<Button secondary label="Advanced search" href="/search?q={q}" />
 		</div>
-	{/if}
+	</div>
 </div>
 
 <style lang="postcss">
+	@custom-media --search-layout-mobile (width < 576px);
+	@custom-media --search-layout-desktop (width >= 576px);
+
+	@media (--search-layout-mobile) {
+		.desktop-only {
+			display: none;
+		}
+	}
+
+	@media (--search-layout-desktop) {
+		.mobile-only {
+			display: none;
+		}
+	}
+
 	.search {
 		--text-input-width: 100%;
 
-		@media (width >= 576px) {
+		@media (--search-layout-mobile) {
+			display: grid;
+			justify-items: end;
+		}
+
+		@media (--search-layout-desktop) {
 			position: relative;
 		}
+	}
+
+	label {
+		display: grid;
+		align-items: center;
+		height: 100%;
+		margin-right: -0.5rem;
+		font-size: 20px;
 	}
 
 	.results {
 		position: absolute;
 		z-index: 1;
 		right: 0;
-		width: 100%;
-		margin-top: 0.25rem;
-
 		display: grid;
 		gap: 1rem;
 		padding: 0.75rem 0.625rem;
 		background: var(--c-body);
 		box-shadow: 0 0 0 1px var(--c-shadow-1), 0 4px 20px var(--c-shadow-1);
+		transition: opacity 0.25s;
+		--text-input-height: 2.875rem;
 
-		@media (width >= 576px) {
+		@media (--search-layout-desktop) {
 			width: 450px;
+			margin-top: 0.25rem;
+		}
+
+		@media (--search-layout-mobile) {
+			left: 0;
+			top: 0;
+		}
+
+		@nest :not(.hasFocus) & {
+			opacity: 0;
+			pointer-events: none;
 		}
 	}
 
