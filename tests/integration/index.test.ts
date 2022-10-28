da08481d9a1b8b41203106737d0b0b9ec9b764b2
Ken Kunz <kennethkunz@gmail.com>
2022-10-28 15:33:58 -0500

    update home page integration tests

--- tests/integration/index.test.ts
+++ tests/integration/index.test.ts
@@ -5,9 +5,15 @@ test.describe('home page', () => {
 		await page.goto('/');
 	});
 
-	test('home page uses custom heading font', async ({ page }) => {
-		const heading = page.getByRole('heading', { level: 1 });
-		await expect(heading).toHaveScreenshot();
+	// This test is useful for validating that custom fonts are installed and loaded.
+	// If there's a legitimate change to the hero banner, generate new screenshots by
+	// deleting the old ones from `tests/integration/index.test.ts-snapshots` and running
+	// the folling commands (2nd one is needed for CI to pass correctly):
+	// $ npm run test:integration
+	// $ docker run -v $(pwd):/work/ mcr.microsoft.com/playwright:v1.27.0-focal bash -c 'cd work && npm run test:integration --skip-build'
+	test('hero banner looks correct', async ({ page }) => {
+		const header = page.locator('main header');
+		await expect(header).toHaveScreenshot();
 	});
 
 	test('home page has impressive numbers', async ({ page }) => {
