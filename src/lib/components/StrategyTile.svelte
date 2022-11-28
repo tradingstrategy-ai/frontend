f9a55ac820e176113bc5c62458c4b106fce085f2
Ken Kunz <kennethkunz@gmail.com>
2022-11-28 15:32:25 -0600

    refactor: use semantic markup/CSS

--- src/lib/components/StrategyTile.svelte
+++ src/lib/components/StrategyTile.svelte
@@ -10,33 +10,37 @@
 	export let isProfitable = true;
 </script>
 
-<li class="strategy-tile" style={isProfitable ? '--profitable: initial;' : isLossy ? '--lossy: initial;' : ''}>
-	<div class="strategy-tile__thumbnail">
+<li style={isProfitable ? '--profitable: initial;' : isLossy ? '--lossy: initial;' : ''}>
+	<div class="thumbnail">
 		<ChartPlaceholder />
 	</div>
-	<div class="strategy-tile__info">
-		<div class="strategy-tile__details">
-			<h2 class="strategy-tile__title">{name}</h2>
-			<div class="strategy-tile__indicators">
-				<div class="strategy-tile__indicator projected-profit">
-					<span class="strategy-tile__indicator-name"> Projected profit </span>
-					<span class="strategy-tile__indicator-value --"> ▲ 52.3% </span>
+	<div class="info">
+		<div class="details">
+			<h2 class="title">{name}</h2>
+			<dl>
+				<div class="projected-profit">
+					<dt>Projected profit</dt>
+					<dd>▲ 52.3%</dd>
 				</div>
-				<div class="strategy-tile__indicator amount-of-assets">
-					<span class="strategy-tile__indicator-name"> Amount of assets </span>
-					<span class="strategy-tile__indicator-value"> $11.2k / 100k </span>
+				<div class="amount-of-assets">
+					<dt>Amount of assets</dt>
+					<dd>$11.2k / 100k</dd>
 				</div>
-			</div>
-			<div class="strategy-tile__description">
-				<p>{description}</p>
+			</dl>
+			<div class="description">
+				{#if !error}
+					<p>{description}</p>
+				{:else}
+					<p>Strategy data not currently available.</p>
+				{/if}
 			</div>
 		</div>
 		<Button href="/strategies/{id}" tertiary lg disabled={!!error}>View strategy details</Button>
 	</div>
 </li>
 
-<style>
-	.strategy-tile {
+<style lang="postcss">
+	li {
 		--profitable: ;
 		--lossy: ;
 		display: grid;
@@ -48,56 +52,58 @@
 		overflow: hidden;
 	}
 
-	.strategy-tile__thumbnail {
+	.thumbnail {
 		align-items: center;
 		display: flex;
 		justify-content: center;
 		overflow: hidden;
+
+		& :global .chart-placeholder {
+			object-fit: cover;
+			width: 100%;
+			height: min(32rem, 100%);
+		}
 	}
 
-	.strategy-tile__thumbnail :global(.chart-placeholder) {
-		object-fit: cover;
-		width: 100%;
-		height: min(32rem, 100%);
-	}
-
-	.strategy-tile__info {
+	.info {
 		display: grid;
 		gap: var(--strategy-tile-info-gap, 1.5rem);
 		padding: 1.5rem;
 	}
 
-	.strategy-tile__details {
+	.details {
 		display: grid;
 		gap: var(--strategy-tile-details-gap, 1rem);
 	}
 
-	.strategy-tile__title {
+	.title {
 		font: var(--fs-ui-xxl);
 		font-weight: 500;
 	}
 
-	.strategy-tile__indicators {
+	dl {
 		display: grid;
 		grid-template-columns: auto auto;
+		margin: 0;
+
+		& > div {
+			display: grid;
+			gap: 0.5rem;
+		}
 	}
 
-	.strategy-tile__indicator {
-		display: grid;
-		gap: 0.5rem;
+	dt {
+		font: var(--f-ui-sm-medium);
+		letter-spacing: var(--f-ui-sm-spacing);
 	}
 
-	.strategy-tile__indicator-name {
-		font: var(--fs-ui-sm);
-		font-weight: 500;
+	dd {
+		font: var(--f-ui-xl-medium);
+		letter-spacing: var(--f-ui-xl-spacing);
+		margin: 0;
 	}
 
-	.strategy-tile__indicator-value {
-		font: var(--fs-ui-xl);
-		font-weight: 500;
-	}
-
-	.projected-profit .strategy-tile__indicator-value {
+	.projected-profit dd {
 		color: var(--lossy, var(--c-bearish)) var(--profitable, var(--c-bullish));
 	}
 </style>
