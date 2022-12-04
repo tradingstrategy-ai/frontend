97dc2f9b00004b5d14d0d6296080a25f8d026db9
Mikko Ohtamaa <mikko@opensourcehacker.com>
2022-12-04 17:22:38 +0100

    Added source code viewer

--- src/routes/strategy/[strategy_id]/source/+page.svelte
+++ src/routes/strategy/[strategy_id]/source/+page.svelte
@@ -6,21 +6,22 @@
 -->
 <script lang="ts">
 	import type { PageData } from './$types';
+    import SourceCode from "./SourceCode.svelte";
+    import { currentStrategy } from 'trade-executor-frontend/state/store';
+
 	export let data: PageData;
 
 	// Currently loaded source code
 	$: source = data.source;
 
+    // TODO: Hack for now, have metadata object to expose real strategy canocical source code URLs
+    $: githubUrl = `http://github.com/tradingstrategy-ai/trade-executor/tree/master/strategies/` + $currentStrategy.id + `.py`;
+
 </script>
 
+<p>
+    The source code of the {$currentStrategy.name} strategy.
+    <a class="body-link" href={githubUrl}>View source code on Github.</a>
+</p>
 
-<pre class="source">
-    {source}
-</pre>
-
-<style lang="postcss">
-    .source {
-        background: var(--cm-light, var(--c-parchment-extra-dark)) var(--cm-dark, var(--c-gray-extra-dark));
-        font-weight: bold;
-    }
-</style>
+<SourceCode {source} />
