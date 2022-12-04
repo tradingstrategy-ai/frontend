c0ae33e0338aee9436d4c99053b97e46e65648e3
Mikko Ohtamaa <mikko@opensourcehacker.com>
2022-12-04 12:07:47 +0100

    Adding skeleton of source code page

---
+++ src/routes/strategy/[strategy_id]/source/+page.svelte
@@ -0,0 +1,26 @@
+<!--
+
+    Page to display the source code of the strategy.
+
+    TODO: Add nice source code formatting widget
+-->
+<script lang="ts">
+	import type { PageData } from './$types';
+	export let data: PageData;
+
+	// Currently loaded source code
+	$: source = data.source;
+
+</script>
+
+
+<pre class="source">
+    {source}
+</pre>
+
+<style lang="postcss">
+    .source {
+        background: var(--cm-light, var(--c-parchment-extra-dark)) var(--cm-dark, var(--c-gray-extra-dark));
+        font-weight: bold;
+    }
+</style>
