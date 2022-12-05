60700b428af3b6d1cfffd1713142998766267824
Mikko Ohtamaa <mikko@opensourcehacker.com>
2022-12-05 20:02:59 +0100

    Added graceful fallback when thinking data is not yet available

--- src/routes/strategy/[strategy_id]/decision-making/+page.svelte
+++ src/routes/strategy/[strategy_id]/decision-making/+page.svelte
@@ -1,22 +1,43 @@
 <!--
 
-    Page to display the source code of the strategy.
+    Page to display the latest strategy thinking chart.
 
-    TODO: Add nice source code formatting widget
 -->
 <script lang="ts">
 	import type { PageData } from './$types';
+    import warning from '$lib/assets/icons/warning.svg';
+	import {writable} from "svelte/store";
+	import {AlertItem, AlertList} from "$lib/components/index.js";
 
 	export let data: PageData;
 
-	// Currently loaded source code
+	const errored = writable(false);
+
+	// The URLs for strategy thinking images
 	$: imageUrls = data.imageUrls;
 
-
+	// https://stackoverflow.com/questions/69020710/fall-back-image-with-sveltekit
+    const fallback = warning;
+    const handleError = ev => {
+        ev.target.src = fallback;
+        errored.set(true);
+    }
 </script>
 
-<p>
-    {imageUrls.light}
-</p>
+{#if errored}
+	<AlertList status="warning">
+		<AlertItem>
+			Could not load strategy decision making data. If the trade executor instance has been
+            restarted recently, this data may not be available until the first strategy decision
+            making cycle is completed.
+		</AlertItem>
+	</AlertList>
+{/if}
 
-<img class="light" scr={imageUrls.light}>
+<img class="light" src={imageUrls.light} on:error={handleError}>
+
+<style>
+	img {
+		margin: 20px 0;
+	}
+</style>
