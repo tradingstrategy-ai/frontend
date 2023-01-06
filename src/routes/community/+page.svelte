856a0679c6a402d9586a9ee2cf110ed6c2fc7a86
Ken Kunz <kennethkunz@gmail.com>
2023-01-06 15:33:47 -0600

    refactor: move contentCardSections data to +page.svelte

--- src/routes/community/+page.svelte
+++ src/routes/community/+page.svelte
@@ -1,14 +1,78 @@
 <script lang="ts">
-	import type { PageData } from './$types';
 	import ContentCardsTemplate from '$lib/components/ContentCardsTemplate.svelte';
 	import heroImage from '$lib/assets/illustrations/communication-1.svg?raw';
 
-	export let data: PageData;
+	const contentCardsSections = [
+		{
+			title: 'Discuss & develop',
+			cards: [
+				{
+					href: 'https://discord.gg/en8tW6MDtw',
+					iconName: 'discord',
+					title: 'Discord chat',
+					subtitle: 'Join our expert community of traders, developers, and analysts and DeFi specialists.'
+				},
+				{
+					href: 'https://github.com/tradingstrategy-ai/',
+					iconName: 'github',
+					title: 'GitHub',
+					subtitle: 'Explore Trading Strategy open-source code on our GitHub repository.'
+				},
+				{
+					href: 'https://discord.gg/en8tW6MDtw',
+					iconName: 'read-glasses',
+					title: 'Algorithm tutorials',
+					subtitle: 'Learn how to write trading algorithms with our documentation and run backtests.'
+				}
+			]
+		},
+		{
+			title: 'Follow us',
+			cards: [
+				{
+					href: '/blog',
+					iconName: 'reading',
+					title: 'Blog',
+					subtitle: 'Join our expert community of traders, developers, and analysts and DeFi specialists.'
+				},
+				{
+					href: 'https://twitter.com/TradingProtocol',
+					iconName: 'twitter',
+					title: 'Twitter',
+					subtitle: 'Follow us on Twitter for trading alerts, DeFi insight and protocol news.'
+				},
+				{
+					href: 'https://www.youtube.com/channel/UCXBQRclPxMY40n52-k3VhYQ',
+					iconName: 'youtube',
+					title: 'YouTube',
+					subtitle: 'Subscribe to our Youtube channel and watch the latest videos on DeFi and algorithmic trading.'
+				},
+				{
+					href: 'https://www.linkedin.com/company/trading-strategy/',
+					iconName: 'linkedin',
+					title: 'LinkedIn',
+					subtitle: 'Follow us on LinkedIn and join our community of traders, developers and quant experts.'
+				},
+				{
+					href: 'https://t.me/trading_protocol',
+					iconName: 'telegram',
+					title: 'Telegram',
+					subtitle: 'Subscribe to our Telegram channel for trading alerts, DeFi insight and protocol news.'
+				},
+				{
+					href: 'https://www.youtube.com/channel/UCXBQRclPxMY40n52-k3VhYQ',
+					iconName: 'newspaper',
+					title: 'Newsletter',
+					subtitle: 'Subscribe to our newsletter and never miss protocol updates, trading tips, news and insights.'
+				}
+			]
+		}
+	];
 </script>
 
 <ContentCardsTemplate
 	cols={3}
-	contentCardsSections={data.contentCardsSections}
+	{contentCardsSections}
 	{heroImage}
 	heroTitle="Community"
 	heroSubtitle="Join our community of expert traders, strategy developers and quants.<br /> Learn more about the Trading Strategy protocol, algorithmic trading and DeFi markets."
--- src/routes/trading-view/+page.svelte
+++ src/routes/trading-view/+page.svelte
@@ -3,10 +3,96 @@
 -->
 <script lang="ts">
 	import type { PageData } from './$types';
+	import { formatAmount, formatSizeGigabytes } from '$lib/helpers/formatters';
 	import heroImage from '$lib/assets/illustrations/data-cloud-1.svg?raw';
 	import ContentCardsTemplate from '$lib/components/ContentCardsTemplate.svelte';
 
 	export let data: PageData;
+	const { impressiveNumbers } = data;
+
+	const contentCardsSections = [
+		{
+			title: 'Explore data',
+			cards: [
+				{
+					href: '/trading-view/blockchains',
+					iconName: 'blockchain',
+					title: 'Blockchains',
+					subtitle: 'Trading Strategy provides powerful market data sets for on-chain trading on several blockchains.',
+					summaryLabel: 'Indexing data from',
+					summaryValue: `${impressiveNumbers.blockchains} blockchains`
+				},
+				{
+					href: '/trading-view/exchanges',
+					iconName: 'exchange',
+					title: 'DEXes',
+					subtitle:
+						'Trading Strategy provides data sets for decentralised exchanges. All market data is sourced from on-chain trades, across multiple DEXs.',
+					summaryLabel: 'Indexing data from',
+					summaryValue: `${formatAmount(impressiveNumbers.exchanges)} DEXes`
+				},
+				{
+					href: '/trading-view/trading-pairs',
+					iconName: 'pair',
+					title: 'Trading pairs',
+					subtitle:
+						'Trading pairs have OHLCV candle data available between 1-minute to 30-day time frames. View historical and current datasets here.',
+					summaryLabel: 'Indexing data from',
+					summaryValue: `${formatAmount(impressiveNumbers.pairs)} trading pairs`
+				},
+				{
+					href: '/trading-view/exchanges',
+					iconName: 'search',
+					title: 'Advanced search',
+					subtitle:
+						'Search tokens across multiple blockchains and exchanges. Sort and filter by liquidity, volume and/or price change.',
+					summaryLabel: 'Browse data and customise results',
+					summaryValue: '32 filters'
+				}
+			]
+		},
+		{
+			title: 'Programmatic access',
+			cards: [
+				{
+					href: '/trading-view/backtesting',
+					iconName: 'backtesting',
+					title: 'Backtesting',
+					subtitle:
+						'Download historical OHLCV data for backtesting your trading algorithms. Liquidity information is available for calculating past slippage. Datasets are served in Parquet file format.',
+					summaryLabel: 'You can download',
+					summaryValue: `${formatSizeGigabytes(impressiveNumbers.database_size)} GB worth of data`
+				},
+				{
+					href: 'https://tradingstrategy.ai/api/explorer/',
+					iconName: '24h',
+					title: 'Realtime API',
+					subtitle:
+						'Connect your trading algorithms for real-time market feeds for live trading.<br /> Real-time API is available in OpenAPI v3 format, no API keys or sign ups needed.',
+					summaryLabel: 'Available format',
+					summaryValue: 'OpenAPI v3'
+				},
+				{
+					href: 'https://tradingstrategy.ai/docs',
+					iconName: 'book',
+					title: 'Documentation',
+					subtitle:
+						'Trading Strategy provides Python libraries for strategy development and execution for decentralised exchanges. Read API documentation and tutorials to learn how to create your own strategies.',
+					summaryLabel: 'Technical documentation',
+					summaryValue: 'For strategy developers'
+				},
+				{
+					href: 'https://tradingstrategy.ai/docs/programming/strategy-examples/index.html',
+					iconName: 'python',
+					title: 'Notebooks',
+					subtitle:
+						'Use popular Jupyter Notebook, Pandas and other data science libraries to model and backtest your strategies. View example notebooks to see how to use DeFi data in your notebooks.',
+					summaryLabel: 'Available examples for',
+					summaryValue: 'Jupyter Notebook, Pandas'
+				}
+			]
+		}
+	];
 </script>
 
 <svelte:head>
@@ -15,7 +101,7 @@
 </svelte:head>
 
 <ContentCardsTemplate
-	contentCardsSections={data.contentCardsSections}
+	{contentCardsSections}
 	{heroImage}
 	heroTitle="Trading data"
 	heroSubtitle="Explore and download trading data from multiple blockchains and decentralised exchanges."
