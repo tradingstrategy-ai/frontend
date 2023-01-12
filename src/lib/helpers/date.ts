8ec0f7d5e61da1cddab9e7069f417e17c0b26fd2
Ken Kunz <kennethkunz@gmail.com>
2023-01-11 23:37:05 -0600

    add strategy chart thumbnail HUD

--- src/lib/helpers/date.ts
+++ src/lib/helpers/date.ts
@@ -4,7 +4,7 @@
  * (future) or negative (past). Range is always returned in ascending order.
  */
 export function getUTCDateRange(date: Date, days: number) {
-	const d1 = getUTCMidnight(date);
+	const d1 = floorUTCDate(date);
 	const d2 = new Date(d1);
 	d2.setUTCDate(d2.getUTCDate() + days - Math.sign(days));
 	return days > 0 ? [d1, d2] : [d2, d1];
@@ -13,6 +13,19 @@ export function getUTCDateRange(date: Date, days: number) {
 /**
  * Return a new date at midnight UTC of the original date (time component stripped)
  */
-export function getUTCMidnight(date: Date) {
-	return new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate()));
+export function floorUTCDate(date: Date) {
+	const floor = new Date(date);
+	floor.setUTCHours(0, 0, 0, 0);
+	return floor;
+}
+
+/**
+ * Return a new date at midnight UTC that is closest to original date
+ */
+export function roundUTCDate(date: Date) {
+	const round = new Date(date);
+	// round-half-up algorithm
+	round.setUTCMilliseconds((24 * 60 * 60 * 1000) / 2);
+	round.setUTCHours(0, 0, 0, 0);
+	return round;
 }
--- src/routes/strategies/ChartThumbnail.svelte
+++ src/routes/strategies/ChartThumbnail.svelte
@@ -1,6 +1,12 @@
+<script context="module" lang="ts">
+	export type ChartTick = [Date, number | undefined];
+</script>
+
 <script lang="ts">
-	type Tick = [Date, number];
-	export let data: Tick[] = [];
+	import { roundUTCDate } from '$lib/helpers/date';
+
+	export let data: ChartTick[] = [];
+	export let active: ChartTick | undefined;
 	export let scaleX: any;
 	export let scaleY: any;
 
@@ -12,11 +18,28 @@
 	const width = Math.max(...scaleX.range());
 	const height = Math.max(...scaleY.range());
 	const y0 = scaleY(0);
+
+	function handleMouseMove({ currentTarget, clientX, clientY }) {
+		const screenPoint = new DOMPointReadOnly(clientX, clientY);
+		const svgPoint = screenPoint.matrixTransform(currentTarget.getScreenCTM().inverse());
+		const date = roundUTCDate(scaleX.invert(svgPoint.x));
+		active = data.find(([d]) => d.valueOf() === date.valueOf()) || [date, undefined];
+	}
 </script>
 
-<svg class="chart-thumbnail" viewBox="0 0 {width} {height}" fill="none" xmlns="http://www.w3.org/2000/svg">
+<svg
+	class="chart-thumbnail"
+	viewBox="0 0 {width} {height}"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+	on:mousemove={handleMouseMove}
+>
 	<line class="x-axis" x1="0" y1={y0} x2={width} y2={y0} />
 	<path class="data" d={commands.join(' ')} />
+	{#if active}
+		{@const x = scaleX(active[0])}
+		<line class="crosshair" x1={x} y1={-height} x2={x} y2={height * 2} />
+	{/if}
 </svg>
 
 <style lang="postcss">
@@ -24,17 +47,27 @@
 		width: 100%;
 		height: 100%;
 		background: var(--c-background-7);
-	}
 
-	.x-axis {
-		stroke: var(--c-text-ultra-light-night);
-		stroke-width: 0.5;
-	}
+		& .x-axis {
+			stroke: var(--c-text-ultra-light-night);
+			stroke-width: 1;
+		}
 
-	.data {
-		stroke: var(--c-text-light-night);
-		stroke-width: 3;
-		stroke-linecap: round;
-		stroke-linejoin: round;
+		& .crosshair {
+			stroke: var(--c-text-ultra-light-night);
+			stroke-width: 1;
+			stroke-dasharray: 6;
+		}
+
+		&:not(:hover) .crosshair {
+			display: none;
+		}
+
+		& .data {
+			stroke: var(--c-text-light-night);
+			stroke-width: 3;
+			stroke-linecap: round;
+			stroke-linejoin: round;
+		}
 	}
 </style>
