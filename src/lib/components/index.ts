75067288b63e8cdf7881209a9643200416d98d87
Ken Kunz <kennethkunz@gmail.com>
2023-01-20 15:10:09 -0600

    refactor banner.css utility class
    - remove ':is' selector to silence vite postcss warnings
    - add support for nesting in external CSS files
    - remove unused Banner component

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,6 +1,5 @@
 export { default as AlertItem } from './AlertItem.svelte';
 export { default as AlertList } from './AlertList.svelte';
-export { default as Banner } from './Banner.svelte';
 export { default as BlogRoll } from './BlogRoll.svelte';
 export { default as Button } from './Button.svelte';
 export { default as ContentCard } from './ContentCard.svelte';
