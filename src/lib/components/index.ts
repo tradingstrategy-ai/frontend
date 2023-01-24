eee6fff42d8e49bfe7ddd3441baa2c40f313a65c
Ken Kunz <kennethkunz@gmail.com>
2023-01-24 15:38:59 -0600

    remove DropDown component (replaced by Select)

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -10,7 +10,6 @@ export { default as DataBox } from './DataBox.svelte';
 export { default as DataBoxes } from './DataBoxes.svelte';
 export { default as DataTable } from './datatable/DataTable.svelte';
 export { default as Dialog } from './Dialog.svelte';
-export { default as DropDown } from './DropDown.svelte';
 export { default as Footer } from './Footer.svelte';
 export { default as Header } from './Header.svelte';
 export { default as Hero } from './Hero.svelte';
