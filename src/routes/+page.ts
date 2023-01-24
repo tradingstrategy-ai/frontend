ad386d13cb8f2a8445ce073c78296a11a2a2ae81
Ken Kunz <kennethkunz@gmail.com>
2023-01-24 14:25:33 -0600

    improve API error handling rendering home page
    - wrap ghost request in try/catch
    - fail gracefully when ghost or backend APIs fail
    - simplify home page load function

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,30 +2,35 @@ import type { PageLoad } from './$types';
 import { backendUrl } from '$lib/config';
 import ghostClient from '$lib/blog/client';
 
+// await API response and return JSON; fail gracefully if there's an error
+async function getRespJson(req: Promise<Response>) {
+	const resp = await req;
+	if (resp.ok) {
+		return resp.json();
+	}
+	console.error(`Backend API ${resp.url} failed; rendering home page without data`);
+}
+
+// fetch blog posts; fail gracefully if there's an error
+async function fetchBlogPosts(options = {}) {
+	try {
+		return await ghostClient.posts?.browse(options);
+	} catch (e) {
+		console.error('Ghost API request failed; rendering home page without blog roll');
+	}
+}
+
 export const load: PageLoad = async ({ fetch, setHeaders }) => {
-	// Load frontpage API calls in parallel to cut that 1 ms
-	// https://stackoverflow.com/q/59663929/315168
-	const [momentumResp, impressiveNumbersResp, posts] = await Promise.all([
-		fetch(`${backendUrl}/top-momentum?summary=true`),
-		fetch(`${backendUrl}/impressive-numbers`),
-		ghostClient.posts?.browse({ limit: 4 })
-	]);
-
-	let topMomentum, impressiveNumbers;
-
-	if (momentumResp.ok) {
-		topMomentum = await momentumResp.json();
-	}
-
-	if (impressiveNumbersResp.ok) {
-		impressiveNumbers = await impressiveNumbersResp.json();
-	}
-
 	// Cache the landing data for 5 minutes at the Cloudflare so pages are
 	// served really fast if they get popular, and also for speed test
 	setHeaders({
 		'cache-control': 'public, max-age=300' // 5 minutes: 5 * 60 = 300
 	});
 
-	return { topMomentum, impressiveNumbers, posts };
+	// SvelteKit handles these in parallel and automatically unwraps top-level promises
+	return {
+		topMomentum: getRespJson(fetch(`${backendUrl}/top-momentum?summary=true`)),
+		impressiveNumbers: getRespJson(fetch(`${backendUrl}/impressive-numbers`)),
+		posts: fetchBlogPosts({ limit: 4 })
+	};
 };
