e316bac21c1daae763cde98795d209e8136fcc90
Ken Kunz <kennethkunz@gmail.com>
2023-02-02 13:04:51 -0600

    fix SiteMode / slow-load page

--- src/routes/slow-load/+page.svelte
+++ src/routes/slow-load/+page.svelte
@@ -4,9 +4,11 @@ by default. Increment the `?page=n` param to delay by n * 2 seconds.
 -->
 <script lang="ts">
 	import type { PageData } from './$types';
+	import { Button, Section } from '$lib/components';
 
 	export let data: PageData;
 	$: page = data.page;
+	$: nextPage = page + 1;
 </script>
 
 <svelte:head>
@@ -14,17 +16,36 @@ by default. Increment the `?page=n` param to delay by n * 2 seconds.
 	<meta name="description" content="Information about the currently running frontend version" />
 </svelte:head>
 
-<div class="container">
-	<div class="content">
-		<h1>You're on Page {page}.</h1>
+<main class="slow-load">
+	<Section layout="boxed" padding="sm" title="You're on Page {page}">
 		<p>
 			In this page's <code>load</code> function, we waited for {page * 2}
 			seconds to mimick a real-world API call before rendering. You can go back to the home page, which should load instantaneously
-			because it doesn't do much server side. Page 2 (and beyond) will have a longer delay of {(page + 1) * 2} seconds.
+			because it doesn't do much server side. Page {nextPage} will have a longer delay of {nextPage * 2} seconds.
 		</p>
-		<div class="links">
-			<a href="/">Home</a>
-			<a href="?page={page + 1}">Page {page + 1}</a>
+
+		<div slot="footer" class="ctas">
+			<Button label="Home" href="/" />
+			<Button secondary label="Page {nextPage}" href="?page={nextPage}" />
 		</div>
-	</div>
-</div>
+	</Section>
+</main>
+
+<style lang="postcss">
+	.slow-load :global .section header {
+		text-align: left;
+	}
+
+	.ctas {
+		display: flex;
+		gap: var(--space-md);
+
+		@media (--viewport-xs) {
+			flex-direction: column;
+		}
+
+		& :global .button {
+			min-width: 10rem;
+		}
+	}
+</style>
