51c2e93451e8447510b493a583ffa5c25308ec48
Ken Kunz <kennethkunz@gmail.com>
2023-02-07 16:03:48 -0600

    improve PairTable column definitions
    - use simple accessor to ensure proper sorting
    - format swap fee
    - disable sort on cta column
    - remove `right` class hack (align using accessor classes)
    - use custom column widths to ensure table headers always visible

--- src/lib/explorer/PairsTable.svelte
+++ src/lib/explorer/PairsTable.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import { readable } from 'svelte/store';
 	import { createRender, createTable } from 'svelte-headless-table';
-	import { addSortBy, addTableFilter, addColumnOrder, addPagination } from 'svelte-headless-table/plugins';
+	import { addSortBy, addPagination } from 'svelte-headless-table/plugins';
 	import { DataTable } from '$lib/components';
-	import { formatDollar, formatAmount, formatPriceChange } from '$lib/helpers/formatters';
+	import { formatDollar, formatPriceChange, formatSwapFee } from '$lib/helpers/formatters';
 	import Button from '$lib/components/Button.svelte';
 
 	export let pairs: any[];
@@ -12,7 +12,7 @@
 
 	const table = createTable(readable(pairs), {
 		sort: addSortBy({
-			initialSortKeys: [{ id: 'usd_volume_30d right', order: 'desc' }],
+			initialSortKeys: [{ id: 'usd_volume_30d', order: 'desc' }],
 			toggleOrder: ['asc', 'desc']
 		}),
 		page: addPagination()
@@ -20,44 +20,42 @@
 
 	const columns = table.createColumns([
 		table.column({
-			id: 'pair_symbol',
 			header: 'Trading pair',
-			accessor: ({ pair_symbol }) => pair_symbol
+			accessor: 'pair_symbol'
 		}),
 		table.column({
-			id: 'exchange_name',
 			header: 'Exchange',
-			accessor: ({ exchange_name }) => exchange_name
+			accessor: 'exchange_name'
 		}),
 		table.column({
-			id: 'pair_swap_fee right',
-			header: 'Swap fee',
-			accessor: ({ pair_swap_fee }) => pair_swap_fee
+			header: 'Fee',
+			accessor: 'pair_swap_fee',
+			cell: ({ value }) => formatSwapFee(value)
 		}),
 		table.column({
-			id: 'usd_price_latest right',
-			header: 'Price (USD)',
-			accessor: ({ usd_price_latest }) => formatDollar(usd_price_latest)
+			header: 'Price USD',
+			accessor: 'usd_price_latest',
+			cell: ({ value }) => formatDollar(value)
 		}),
 		table.column({
-			id: 'price_change_24h right',
-			header: 'Price 24h',
-			accessor: ({ price_change_24h }) => formatPriceChange(price_change_24h)
+			header: 'Price Δ 24h',
+			accessor: 'price_change_24h',
+			cell: ({ value }) => formatPriceChange(value)
 		}),
 		table.column({
-			id: 'usd_volume_30d right',
-			header: 'Vol 30d (USD)',
-			accessor: ({ usd_volume_30d }) => formatDollar(usd_volume_30d)
+			header: 'Vol 30d USD',
+			accessor: 'usd_volume_30d',
+			cell: ({ value }) => formatDollar(value)
 		}),
 		table.column({
-			id: 'usd_liquidity_latest right',
-			header: 'Liq (USD)',
-			accessor: ({ usd_liquidity_latest }) => formatDollar(usd_liquidity_latest)
+			header: 'Liq USD',
+			accessor: 'usd_liquidity_latest',
+			cell: ({ value }) => formatDollar(value)
 		}),
 		table.column({
-			id: 'liquidity_change_24h right',
-			header: 'Liq 24h',
-			accessor: ({ liquidity_change_24h }) => formatPriceChange(liquidity_change_24h)
+			header: 'Liq Δ 24h',
+			accessor: 'liquidity_change_24h',
+			cell: ({ value }) => formatPriceChange(value)
 		}),
 		table.column({
 			id: 'cta',
@@ -67,11 +65,53 @@
 				createRender(Button, {
 					label: 'View pair',
 					href: `/trading-view/${value.chain_slug}/${value.exchange_slug}/${value.pair_slug}`
-				})
+				}),
+			plugins: { sort: { disable: true } }
 		})
 	]);
 
 	const tableViewModel = table.createViewModel(columns);
 </script>
 
-<DataTable isResponsive {tableViewModel} />
+<div class="pairs-table">
+	<DataTable isResponsive {tableViewModel} />
+</div>
+
+<style lang="postcss">
+	.pairs-table :global {
+		@media (--viewport-md-up) {
+			&
+				:is(.pair_swap_fee, .usd_price_latest, .price_change_24h, .usd_volume_30d, .usd_liquidity_latest, .liquidity_change_24h) {
+				text-align: right;
+				&:is(td) {
+					overflow: hidden;
+					text-overflow: ellipsis;
+				}
+			}
+
+			& tr:hover td {
+				position: relative;
+				overflow: visible;
+			}
+
+			& .pair_swap_fee {
+				max-width: 6rem;
+			}
+			& .usd_price_latest {
+				max-width: 8.5rem;
+			}
+			& .price_change_24h {
+				max-width: 9rem;
+			}
+			& .usd_volume_30d {
+				max-width: 9rem;
+			}
+			& .usd_liquidity_latest {
+				max-width: 7rem;
+			}
+			& .liquidity_change_24h {
+				max-width: 8rem;
+			}
+		}
+	}
+</style>
