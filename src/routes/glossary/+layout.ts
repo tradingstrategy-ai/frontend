ec2b00201a23044c1ae2ae708da333de861e3f83
Mikko Ohtamaa <mikko@opensourcehacker.com>
2023-02-19 17:02:04 +0100

    Add error handling for the glossary

--- src/routes/glossary/+layout.ts
+++ src/routes/glossary/+layout.ts
@@ -2,9 +2,15 @@
  * Data loader for all /glossary routes
  */
 import type { LayoutLoad } from './$types';
+import {publicApiError} from "$lib/helpers/publicApiError";
 
 export const load: LayoutLoad = async ({ fetch }) => {
 	const resp = await fetch('/glossary/api');
+
+	if (!resp.ok) {
+		console.log(resp.text());
+		throw new Error(`Could not load glossary. See console log for details.`);
+	}
 
 	return {
 		glossary: resp.json()
