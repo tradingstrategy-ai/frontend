fa23b0dc6adb43a4838f26467476fb3d58618616
Ken Kunz <kennethkunz@gmail.com>
2023-03-13 10:47:31 -0500

    redirect home page to /strategies

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,36 +1,6 @@
 import type { PageLoad } from './$types';
-import { backendUrl } from '$lib/config';
-import ghostClient from '$lib/blog/client';
+import { redirect } from '@sveltejs/kit';
 
-// await API response and return JSON; fail gracefully if there's an error
-async function getRespJson(req: Promise<Response>) {
-	const resp = await req;
-	if (resp.ok) {
-		return resp.json();
-	}
-	console.error(`Backend API ${resp.url} failed; rendering home page without data`);
-}
-
-// fetch blog posts; fail gracefully if there's an error
-async function fetchBlogPosts(options = {}) {
-	try {
-		return await ghostClient.posts?.browse(options);
-	} catch (e) {
-		console.error('Ghost API request failed; rendering home page without blog roll');
-	}
-}
-
-export const load = (async ({ fetch, setHeaders }) => {
-	// Cache the landing data for 5 minutes at the Cloudflare so pages are
-	// served really fast if they get popular, and also for speed test
-	setHeaders({
-		'cache-control': 'public, max-age=300' // 5 minutes: 5 * 60 = 300
-	});
-
-	// SvelteKit handles these in parallel and automatically unwraps top-level promises
-	return {
-		topMomentum: getRespJson(fetch(`${backendUrl}/top-momentum?summary=true`)),
-		impressiveNumbers: getRespJson(fetch(`${backendUrl}/impressive-numbers`)),
-		posts: fetchBlogPosts({ limit: 4 })
-	};
+export const load = (() => {
+	throw redirect(302, '/strategies');
 }) satisfies PageLoad;
