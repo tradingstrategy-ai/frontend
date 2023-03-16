ed4821d314ee5fdef2f2802046be1d486b914762
Ken Kunz <kennethkunz@gmail.com>
2023-03-16 14:53:09 -0500

    fix nav panel layout on tablets
    - refactor Footer / SocialIcon
    - closes #396

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -7,82 +7,47 @@
 
 <footer class:small>
 	{#if !skip}
-		<a href="https://github.com/tradingstrategy-ai" title="GitHub">
-			<SocialIcon name="github" />
-		</a>
+		<div class="icon-group">
+			<SocialIcon {small} name="github" href="https://github.com/tradingstrategy-ai" title="GitHub" />
+			<SocialIcon {small} name="discord" href="https://discord.gg/en8tW6MDtw" title="Discord" />
+			<SocialIcon {small} name="telegram" href="https://t.me/trading_protocol" title="Telegram" />
+			<SocialIcon {small} name="twitter" href="https://twitter.com/tradingprotocol" title="Twitter" />
+		</div>
 
-		<a href="https://discord.gg/en8tW6MDtw" title="Discord">
-			<SocialIcon name="discord" />
-		</a>
-
-		<a href="https://t.me/trading_protocol" title="Telegram">
-			<SocialIcon name="telegram" />
-		</a>
-
-		<a href="https://twitter.com/tradingprotocol" title="Twitter">
-			<SocialIcon name="twitter" />
-		</a>
-
-		<a href="https://www.linkedin.com/company/trading-strategy/" title="LinkedIn">
-			<SocialIcon name="linkedin" />
-		</a>
-
-		<a title="Newsletter" href="/newsletter">
-			<SocialIcon name="newspaper" />
-		</a>
-
-		<a title="Newsletter" href="/blog">
-			<SocialIcon name="reading" />
-		</a>
-
-		<a href="/blog/rss.xml" rel="external" title="RSS">
-			<SocialIcon name="rss" />
-		</a>
+		<div class="icon-group">
+			<SocialIcon {small} name="linkedin" href="https://www.linkedin.com/company/trading-strategy/" title="LinkedIn" />
+			<SocialIcon {small} name="newspaper" title="Newsletter" href="/newsletter" />
+			<SocialIcon {small} name="reading" title="Newsletter" href="/blog" />
+			<SocialIcon {small} external name="rss" href="/blog/rss.xml" title="RSS" />
+		</div>
 	{/if}
 </footer>
 
 <style lang="postcss">
 	footer {
-		--footer-height: 12rem;
-
-		@media (--nav-collapsed) {
-			--footer-height: 6rem;
-		}
-
-		display: grid;
+		display: flex;
+		flex-wrap: wrap;
 		place-content: center;
 		gap: var(--space-2xl);
-		min-height: var(--footer-height);
+		padding-inline: var(--space-lg);
+		padding-block: var(--space-7xl);
 
-		@media (--viewport-sm-up) {
-			grid-auto-flow: column;
+		@media (--nav-collapsed) {
+			padding-block: var(--space-lg);
 		}
 
 		@media (--viewport-sm-down) {
-			grid-template-columns: repeat(4, auto);
-			padding: var(--space-5xl) var(--space-md);
-			place-content: center;
-			place-items: center;
-
-			& :global .icon {
-				--icon-size: 1.125rem !important;
-			}
+			gap: var(--space-ll);
 		}
 
 		&.small {
-			gap: var(--space-ls);
-			padding: 0;
+			gap: var(--space-ll);
+			padding-block: 0;
 		}
-	}
 
-	a {
-		transition: opacity 0.15s;
-		border: none;
-		background: transparent;
-
-		&:hover {
-			opacity: 0.9;
-			cursor: pointer;
+		& .icon-group {
+			display: flex;
+			gap: inherit;
 		}
 	}
 </style>
--- src/lib/components/SocialIcon.svelte
+++ src/lib/components/SocialIcon.svelte
@@ -2,27 +2,47 @@
 	import { Icon } from '$lib/components';
 
 	export let name: string;
+	export let title = name;
+	export let href: string | undefined = undefined;
+	export let external = false;
+	export let small = false;
+
+	$: tag = href ? 'a' : 'span';
+	$: rel = external ? 'external' : undefined;
 </script>
 
-<div><Icon {name} /></div>
+<svelte:element this={tag} class="social-icon" class:small {href} {title} {rel}>
+	<Icon {name} />
+</svelte:element>
 
-<style>
-	div {
-		--size: var(--social-icon-size, 2.5rem);
+<style lang="postcss">
+	.social-icon {
+		--social-icon-size: 3rem;
+		--icon-size: 1.5rem;
 		display: flex;
 		justify-content: center;
 		align-items: center;
-		height: var(--size);
-		width: var(--size);
-		border-radius: calc(var(--size) / 2);
+		height: var(--social-icon-size);
+		width: var(--social-icon-size);
+		border: none;
+		border-radius: calc(var(--social-icon-size) / 2);
 		background: hsla(var(--hsl-text));
 		color: hsla(var(--hsl-body));
-		--icon-size: calc(var(--size) * var(--social-icon-scale, 0.5));
+		transition: opacity 0.15s;
+
+		@media (--viewport-sm-down) {
+			--social-icon-size: 2.5rem;
+			--icon-size: 1.125rem;
+		}
+
+		&.small {
+			--social-icon-size: 2.5rem;
+			--icon-size: 1.125rem;
+		}
 	}
 
-	@media (--viewport-md-up) {
-		div {
-			--size: var(--social-icon-size, 48px);
-		}
+	a:hover {
+		opacity: 0.9;
+		cursor: pointer;
 	}
 </style>
