4f30a27e7383717f7958c75982bfbdee90a03489
Aleksander Grygier <aleksander.grygier@resider.pl>
2023-03-28 10:34:49 +0200

    style: Update blog headings' sizing

--- src/routes/blog/BlogPostContent.svelte
+++ src/routes/blog/BlogPostContent.svelte
@@ -55,17 +55,26 @@
 			font: var(--f-heading-lg-medium);
 			letter-spacing: var(--f-heading-lg-spacing, normal);
 			margin: var(--space-5xl) 0 var(--space-lg);
+			@media (--viewport-xs) {
+				font: var(--f-heading-md-medium);
+			}
 		}
 
 		& > h3 {
 			font: var(--f-heading-md-medium);
 			letter-spacing: var(--f-heading-md-spacing, normal);
 			margin: var(--space-xl) 0 var(--space-md) 0;
+			@media (--viewport-xs) {
+				font: var(--f-heading-sm-medium);
+			}
 		}
 		& > h4 {
 			font: var(--f-heading-sm-medium);
 			letter-spacing: var(--f-heading-sm-spacing, normal);
 			margin: var(--space-xl) 0 var(--space-md) 0;
+			@media (--viewport-xs) {
+				font: var(--f-heading-xs-medium);
+			}
 		}
 
 		& :is(ol, ul) {
