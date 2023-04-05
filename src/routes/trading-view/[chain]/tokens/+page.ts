66de4c137ab64ec3ff7dd8f09d7761b9b573cfe2
Ken Kunz <kennethkunz@gmail.com>
2023-04-05 10:16:49 -0500

    refactor: minor tweaks to tokens/+page.ts

--- src/routes/trading-view/[chain]/tokens/+page.ts
+++ src/routes/trading-view/[chain]/tokens/+page.ts
@@ -3,14 +3,11 @@ import { fetchTokens } from '$lib/explorer/token-client';
 
 export const load = (async ({ fetch, params, url }) => {
 	const { searchParams } = url;
+	const page = Number(searchParams.get('page')) || 0;
+	const sort = searchParams.get('sort') || 'volume_24h';
+	const direction = searchParams.get('direction') || 'desc';
 
-	const apiParams = {
-		page: Number(searchParams.get('page')) || 0,
-		sort: searchParams.get('sort') || 'volume_24h',
-		direction: searchParams.get('direction') || 'desc'
-	};
+	const data = await fetchTokens(fetch, { chain_slug: params.chain, page, sort, direction });
 
-	const data = await fetchTokens(fetch, { chain_slug: params.chain, ...apiParams });
-
-	return { ...data, ...apiParams };
+	return { ...data, page, sort, direction };
 }) satisfies PageLoad;
