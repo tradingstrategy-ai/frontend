1da19f3003a6e4955dd4a1e03a0d4951b6d75a8f
Ken Kunz <kennethkunz@gmail.com>
2023-04-05 15:25:25 -0500

    add stub route/page for reserve details

---
+++ src/routes/trading-view/[chain]/lending/[protocol]/[reserve]/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import { Section } from '$lib/components';
+
+	export let data: PageData;
+</script>
+
+<Section padding="md">
+	<h2>Reserve details page (coming soon)</h2>
+	<pre>{JSON.stringify(data, null, 4)}</pre>
+</Section>
+
+<style lang="postcss">
+	pre {
+		padding: var(--space-lg);
+		background: var(--c-background-7);
+		border-radius: var(--radius-md);
+		color: var(--c-text-default-night);
+		font: var(--f-mono-md-regular);
+		letter-spacing: var(--f-mono-md-spacing, normal);
+	}
+</style>
---
+++ src/routes/trading-view/[chain]/lending/[protocol]/[reserve]/+page.ts
@@ -0,0 +1,10 @@
+import type { PageLoad } from './$types';
+import { fetchPublicApi } from '$lib/helpers/public-api';
+
+export const load = (async ({ params, fetch }) => {
+	const chain_slug = params.chain;
+	const protocol_slug = params.protocol;
+	const reserve_slug = params.reserve;
+
+	return fetchPublicApi(fetch, 'reserve/details', { chain_slug, protocol_slug, reserve_slug });
+}) satisfies PageLoad;
