ebe6ef2cc7774b46e7d0ce5df5cb2f1eb7ef92b4
Ken Kunz <kennethkunz@gmail.com>
2023-06-06 21:50:30 -0500

    add share balance/value to deposit success page

--- src/routes/wizard/deposit/success/+page.svelte
+++ src/routes/wizard/deposit/success/+page.svelte
@@ -1,15 +1,29 @@
 <script lang="ts">
 	import wizard from '../store';
-	import { Button, WizardActions } from '$lib/components';
+	import { wallet } from '$lib/wallet/client';
+	import { fetchBalance } from '@wagmi/core';
+	import { getAccountNetValue } from '$lib/eth-defi/enzyme';
+	import { Button, DataBox, Grid, WizardActions } from '$lib/components';
+	import TokenBalance from '$lib/wallet/TokenBalance.svelte';
+
+	$: ({ contracts, strategyName } = $wizard.data);
 </script>
 
-<div class="deposit-success">
-	<p>
-		Congratulations! You've successfully invested in <strong>{$wizard?.data.strategyName}</strong>. Keep an eye on your
-		progress and returns. Please note that investing in crypto / DeFi trading strategies carries significant risk. Click
-		"Done" to return to the strategy.
+<Grid cols={2} gap="lg">
+	<DataBox label="Number of shares">
+		<TokenBalance data={fetchBalance({ token: contracts.vault, address: $wallet.address })} />
+	</DataBox>
+
+	<DataBox label="Value of shares">
+		<TokenBalance data={getAccountNetValue(contracts, $wallet.address)} />
+	</DataBox>
+
+	<p style:grid-column="1 / -1">
+		Congratulations! You've successfully invested in <strong>{strategyName}</strong>. Keep an eye on your progress and
+		returns. Please remember that investing in crypto / DeFi trading strategies carries significant risk. Click "Done"
+		to return to the strategy.
 	</p>
-</div>
+</Grid>
 
 <WizardActions>
 	<Button secondary label="Back" href="payment" />
