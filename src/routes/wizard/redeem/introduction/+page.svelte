32171fc36471777ce3adf38afe92c9948cdc9bf7
Ken Kunz <kennethkunz@gmail.com>
2023-06-08 23:35:42 -0500

    add redeem intro copy

--- src/routes/wizard/redeem/introduction/+page.svelte
+++ src/routes/wizard/redeem/introduction/+page.svelte
@@ -6,7 +6,11 @@
 </script>
 
 <div class="deposit-introduction">
-	<p>Redeem copy goes here</p>
+	<p>
+		Ready to redeem shares of <strong>{$wizard.data.strategyName}</strong>? Upon redemption, you'll receive USDC and any
+		tokens the strategy is currently invested in. Connect your wallet, review your share balance, enter your desired
+		redemption amount, and complete your transaction. Let's get started!
+	</p>
 </div>
 
 <WizardActions>
