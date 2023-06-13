4ffa46c6a4abc5b60ab27cb9d3a0a69045a66765
Ken Kunz <kennethkunz@gmail.com>
2023-06-13 16:44:00 -0500

    add share balance to redemption page

--- src/routes/wizard/redeem/shares-redemption/+page.svelte
+++ src/routes/wizard/redeem/shares-redemption/+page.svelte
@@ -1,21 +1,25 @@
 <script lang="ts">
 	import wizard from '../store';
 	import { tweened } from 'svelte/motion';
+	import { cubicOut } from 'svelte/easing';
 	import fsm from 'svelte-fsm';
+	import type { FetchBalanceResult } from '@wagmi/core';
+	import { TokenBalance } from '$lib/wallet';
 	import {
 		AlertItem,
 		AlertList,
 		Button,
 		CryptoAddressWidget,
 		DataBox,
-		EntitySymbol,
 		MoneyInput,
 		WizardActions
 	} from '$lib/components';
 
-	let redemptionValue: number;
+	const vaultShares: FetchBalanceResult = $wizard.data.vaultShares;
 
-	const redemptionProgress = tweened(0, { duration: 2000 });
+	let redemptionValue: MaybeNumber;
+
+	const progressBar = tweened(0, { easing: cubicOut });
 
 	const redemption = fsm('initial', {
 		initial: {
@@ -26,7 +30,7 @@
 		},
 		processing: {
 			_enter() {
-				redemptionProgress.set(100).then(redemption.finish);
+				progressBar.set(100, { duration: 2000 }).then(redemption.finish);
 			},
 			finish: 'done'
 		},
@@ -41,7 +45,7 @@
 		<h3>Your current balance</h3>
 
 		<DataBox label="Number of shares">
-			<EntitySymbol label="SHR" slug="uni" type="token">123.45 SHR</EntitySymbol>
+			<TokenBalance data={vaultShares} />
 		</DataBox>
 	</section>
 
@@ -50,7 +54,7 @@
 			<h3>Enter amount of shares to redeem</h3>
 
 			<form action="" class="redemption-form">
-				<MoneyInput conversionRatio={10} showConversionLabel size="xl" tokenUnit="SHR" bind:value={redemptionValue} />
+				<MoneyInput conversionRatio={10} showConversionLabel size="xl" tokenUnit={vaultShares.symbol} bind:value={redemptionValue} />
 
 				<Button disabled={!redemptionValue} size="lg" on:click={redemption.confirm}>Redeem</Button>
 			</form>
@@ -69,7 +73,7 @@
 			</div>
 		{:else if $redemption === 'processing'}
 			<h3>Confirming transaction...</h3>
-			<progress max="100" value={$redemptionProgress} />
+			<progress max="100" value={$progressBar} />
 			<p class="disclaimer">
 				Ullamco esse adipisicing ut reprehenderit Lorem elit occaecat eiusmod tempor nulla aliquip.
 			</p>
