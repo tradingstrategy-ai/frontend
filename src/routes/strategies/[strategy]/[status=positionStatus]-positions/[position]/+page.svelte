68ee50dbe7cf25f7bda9d1b54883486f77f14b76
Alex <74208897+AlexTheLion123@users.noreply.github.com>
2023-06-27 16:33:20 +0200

    add if check

--- src/routes/strategies/[strategy]/[status=positionStatus]-positions/[position]/+page.svelte
+++ src/routes/strategies/[strategy]/[status=positionStatus]-positions/[position]/+page.svelte
@@ -71,13 +71,15 @@
 
 			<DataBox label="Highest value" value={formatDollar(getValueAtPeak(positionStats))} />
 
-			<DataBox label="Buy price">
-				{formatDollar(trades[0].executed_price)}
-			</DataBox>
+			{#if position.closed_at}
+				<DataBox label="Buy price">
+					{formatDollar(trades[0].executed_price)}
+				</DataBox>
 
-			<DataBox label="Sell price">
-				{formatDollar(trades.at(-1).executed_price)}
-			</DataBox>
+				<DataBox label="Sell price">
+					{formatDollar(trades.at(-1).executed_price)}
+				</DataBox>
+			{/if}
 		</DataBoxes>
 
 		{#if hasFailedTrades}
