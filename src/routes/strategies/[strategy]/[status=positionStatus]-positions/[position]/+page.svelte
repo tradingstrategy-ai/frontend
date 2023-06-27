2fbf187fec61e37ea1d3c3e8aafc47e97f666397
Alex <74208897+AlexTheLion123@users.noreply.github.com>
2023-06-27 16:30:43 +0200

    prettify

--- src/routes/strategies/[strategy]/[status=positionStatus]-positions/[position]/+page.svelte
+++ src/routes/strategies/[strategy]/[status=positionStatus]-positions/[position]/+page.svelte
@@ -70,15 +70,14 @@
 			{/if}
 
 			<DataBox label="Highest value" value={formatDollar(getValueAtPeak(positionStats))} />
-			
+
 			<DataBox label="Buy price">
 				{formatDollar(trades[0].executed_price)}
-			</DataBox>	
+			</DataBox>
 
 			<DataBox label="Sell price">
 				{formatDollar(trades.at(-1).executed_price)}
 			</DataBox>
-			
 		</DataBoxes>
 
 		{#if hasFailedTrades}
