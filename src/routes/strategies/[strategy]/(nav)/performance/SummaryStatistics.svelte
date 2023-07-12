2ce4a85e9fef47295e82c84a29433393018493b2
Ken Kunz <kennethkunz@gmail.com>
2023-07-11 19:54:32 -0500

    remove old compatibility prop name

--- src/routes/strategies/[strategy]/(nav)/performance/SummaryStatistics.svelte
+++ src/routes/strategies/[strategy]/(nav)/performance/SummaryStatistics.svelte
@@ -6,8 +6,7 @@ Render the statistics from the portfolio server-side calculated state.
 	import { formatDollar, formatDuration, formatPercent } from '$lib/helpers/formatters';
 	import { DataBox, SummaryBox } from '$lib/components';
 
-	// TODO: Eventually migrated away
-	// Old statistics
+	// TODO: Eventually migrate away (when required stats are in summaryStatistics)
 	// See trade_analyzer.py TradeSummary class
 	export let oldLatestStats: any;
 
@@ -15,36 +14,34 @@ Render the statistics from the portfolio server-side calculated state.
 	// https://github.com/tradingstrategy-ai/trade-executor/blob/b3b2181acca90cc9873bb0ebf0778095e5c80523/tradeexecutor/strategy/summary.py#L148
 	export let summaryStatistics: any;
 
-	// TODO: Will be eventually migrated away
-	$: summary = oldLatestStats?.summary || {};
-
-	// compatibility layer for old property name in TradeSummary (remove after 01.04.2023)
-	$: summary.total_positions ??= summary.total_trades;
+	$: oldStatsSummary = oldLatestStats?.summary || {};
 </script>
 
-{#if summary}
-	<SummaryBox title="Performance summary">
-		<div class="summary-statistics">
-			<DataBox size="sm" label="Trade period" value={formatDuration(summary.duration)} />
-			<DataBox size="sm" label="Realised profit" value={formatDollar(summary.realised_profit)} />
-			<DataBox size="sm" label="Open positions" value={formatDollar(summary.open_value)} />
-			<DataBox size="sm" label="Lifetime return" value={formatPercent(summaryStatistics?.return_all_time)} />
-			<DataBox size="sm" label="Annualised return" value={formatPercent(summaryStatistics?.return_annualised)} />
-			<DataBox size="sm" label="Total positions" value={summary.total_positions} />
-			<DataBox size="sm" label="Trades won" value={summary.won} />
-			<DataBox size="sm" label="Trades lost" value={summary.lost} />
-			<DataBox size="sm" label="Stop losses triggered" value={summary.stop_losses} />
-			<DataBox size="sm" label="Positions closed neutral" value={summary.zero_loss} />
-			<DataBox
-				size="sm"
-				label="Average winning trade profit %"
-				value={formatPercent(summary.average_winning_trade_profit_pc)}
-			/>
-			<DataBox size="sm" label="Biggest winning trade %" value={formatPercent(summary.biggest_winning_trade_pc)} />
-			<DataBox size="sm" label="Biggest losing trade %" value={formatPercent(summary.biggest_losing_trade_pc)} />
-		</div>
-	</SummaryBox>
-{/if}
+<SummaryBox title="Performance summary">
+	<div class="summary-statistics">
+		<DataBox size="sm" label="Trade period" value={formatDuration(oldStatsSummary.duration)} />
+		<DataBox size="sm" label="Realised profit" value={formatDollar(oldStatsSummary.realised_profit)} />
+		<DataBox size="sm" label="Open positions" value={formatDollar(oldStatsSummary.open_value)} />
+		<DataBox size="sm" label="Lifetime return" value={formatPercent(summaryStatistics?.return_all_time)} />
+		<DataBox size="sm" label="Annualised return" value={formatPercent(summaryStatistics?.return_annualised)} />
+		<DataBox size="sm" label="Total positions" value={oldStatsSummary.total_positions} />
+		<DataBox size="sm" label="Trades won" value={oldStatsSummary.won} />
+		<DataBox size="sm" label="Trades lost" value={oldStatsSummary.lost} />
+		<DataBox size="sm" label="Stop losses triggered" value={oldStatsSummary.stop_losses} />
+		<DataBox size="sm" label="Positions closed neutral" value={oldStatsSummary.zero_loss} />
+		<DataBox
+			size="sm"
+			label="Average winning trade profit %"
+			value={formatPercent(oldStatsSummary.average_winning_trade_profit_pc)}
+		/>
+		<DataBox
+			size="sm"
+			label="Biggest winning trade %"
+			value={formatPercent(oldStatsSummary.biggest_winning_trade_pc)}
+		/>
+		<DataBox size="sm" label="Biggest losing trade %" value={formatPercent(oldStatsSummary.biggest_losing_trade_pc)} />
+	</div>
+</SummaryBox>
 
 <style lang="postcss">
 	.summary-statistics {
