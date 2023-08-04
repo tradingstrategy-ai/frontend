daad2ae687ec6fbe8406de8a893800df4288ea47
Ken Kunz <kennethkunz@gmail.com>
2023-08-03 19:23:16 -0500

    add tooltips to lending reserve info table

--- src/routes/trading-view/[chain]/lending/[protocol]/[reserve]/InfoTable.svelte
+++ src/routes/trading-view/[chain]/lending/[protocol]/[reserve]/InfoTable.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { TradingDataInfo, TradingDataInfoRow } from '$lib/components';
+	import { Tooltip, TradingDataInfo, TradingDataInfoRow } from '$lib/components';
 	import type { Reserve } from '$lib/helpers/lending-reserve';
 	import { formatInterestRate } from '$lib/helpers/formatters';
 
@@ -10,27 +10,60 @@
 </script>
 
 <TradingDataInfo>
-	<TradingDataInfoRow label="Supply rate" value="{formatInterestRate(details.supply_apr_latest)} APR" />
-	<TradingDataInfoRow label="Borrow rate – variable">
+	<TradingDataInfoRow value="{formatInterestRate(details.supply_apr_latest)} APR">
+		<Tooltip slot="label">
+			<span slot="trigger" class="underline">Supply rate</span>
+			<div slot="popup">APR earned when supplying this token as collateral</div>
+		</Tooltip>
+	</TradingDataInfoRow>
+	<TradingDataInfoRow>
+		<Tooltip slot="label">
+			<span slot="trigger" class="underline">Borrow rate – variable</span>
+			<div slot="popup">
+				APR paid when borrowing this token using the reserve's <em>variable rate</em>. This is based on the offer and
+				demand in {reserve.protocol_name}. This rate will fluctuate over time and could be the optimal rate depending on
+				market conditions.
+			</div>
+		</Tooltip>
+
 		<svelte:fragment slot="value">
 			{#if borrowable}
 				{formatInterestRate(details.variable_borrow_apr_latest)} APR
 			{:else}
-				N/A
+				<Tooltip>
+					<span slot="trigger" class="underline">N/A</span>
+					<div slot="popup">This reserve is not currently borrowable.</div>
+				</Tooltip>
 			{/if}
 		</svelte:fragment>
 	</TradingDataInfoRow>
-	<TradingDataInfoRow label="Borrow rate – stable">
+	<TradingDataInfoRow>
+		<Tooltip slot="label">
+			<span slot="trigger" class="underline">Borrow rate – stable</span>
+			<div slot="popup">
+				APR paid when borrowing this token using the reserve's <em>stable rate</em>. Stable rates act as a fixed rate in
+				the short-term, but can be re-balanced in the long-term in response to changes in market conditions.
+			</div>
+		</Tooltip>
+
 		<svelte:fragment slot="value">
 			{#if borrowable}
 				{formatInterestRate(details.stable_borrow_apr_latest)} APR
 			{:else}
-				N/A
+				<Tooltip>
+					<span slot="trigger" class="underline">N/A</span>
+					<div slot="popup">This reserve is not currently borrowable.</div>
+				</Tooltip>
 			{/if}
 		</svelte:fragment>
 	</TradingDataInfoRow>
 	<TradingDataInfoRow label="Protocol" value={reserve.protocol_name} />
-	<TradingDataInfoRow label="Asset Token">
+	<TradingDataInfoRow>
+		<Tooltip slot="label">
+			<span slot="trigger" class="underline">Asset Token</span>
+			<div slot="popup">Underlying asset token to be supplied as collateral and/or borrowed.</div>
+		</Tooltip>
+
 		<a slot="value" href="/trading-view/{reserve.chain_slug}/tokens/{reserve.asset_address}">
 			{reserve.asset_name}
 		</a>
@@ -39,3 +72,9 @@
 		<a slot="value" href="/trading-view/{reserve.chain_slug}">{reserve.chain_name}</a>
 	</TradingDataInfoRow>
 </TradingDataInfo>
+
+<style lang="postcss">
+	div[slot='popup'] {
+		max-width: 25rem;
+	}
+</style>
