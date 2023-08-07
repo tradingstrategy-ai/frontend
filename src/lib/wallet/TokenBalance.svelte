172942565d2e5ecb18e3701a0aed857b87925cc4
Ken Kunz <kennethkunz@gmail.com>
2023-08-06 22:17:51 -0500

    use consistent rounding method in TokenBalance

--- src/lib/wallet/TokenBalance.svelte
+++ src/lib/wallet/TokenBalance.svelte
@@ -1,22 +1,16 @@
 <script lang="ts">
 	import type { FetchBalanceResult } from '@wagmi/core';
+	import { formatNumber } from '$lib/helpers/formatters';
 	import { Alert, EntitySymbol } from '$lib/components';
 	import Spinner from 'svelte-spinner';
 
 	export let data: MaybePromise<FetchBalanceResult>;
-	export let maxDigits = 2;
-
-	function format({ formatted }: FetchBalanceResult) {
-		return Number(formatted).toLocaleString('en', {
-			maximumFractionDigits: maxDigits
-		});
-	}
 </script>
 
 {#await data}
 	<Spinner size="2rem" color="hsla(var(--hsl-text-light))" />
-{:then token}
-	<EntitySymbol slug={token.symbol.toLowerCase()} type="token">{format(token)} {token.symbol}</EntitySymbol>
+{:then { symbol, formatted }}
+	<EntitySymbol slug={symbol.toLowerCase()} type="token">{formatNumber(Number(formatted), 2, 4)} {symbol}</EntitySymbol>
 {:catch error}
 	<Alert size="xs">Error loading data</Alert>
 {/await}
