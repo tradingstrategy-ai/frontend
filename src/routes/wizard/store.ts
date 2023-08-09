1f845c6d5ee73bbf315a0f4dec1d212510e4712f
Ken Kunz <kennethkunz@gmail.com>
2023-08-09 16:45:10 -0500

    persist wizard store data to sessionStorage

--- src/routes/wizard/store.ts
+++ src/routes/wizard/store.ts
@@ -4,7 +4,9 @@
  * Each wizard uses this to create a custom store that encapsulates the wizard's
  * title, steps, returnTo path and completed state.
  */
+import { browser } from '$app/environment';
 import { writable, type Writable } from 'svelte/store';
+import { stringify, parse } from 'devalue';
 import { goto } from '$app/navigation';
 
 export type WizardValue = {
@@ -18,16 +20,38 @@ export type Step = {
 	label: string;
 };
 
+const storage = browser ? window.sessionStorage : undefined;
+const storageKey = 'ts:wizard';
+
+function getSession(slug: string) {
+	try {
+		const serialized = storage?.getItem(storageKey);
+		const data = serialized && parse(serialized)?.[slug];
+		return data ?? {};
+	} catch (e) {
+		console.error('Error deserializing wizard data from sessionStorage.');
+		console.error(e);
+	}
+}
+
+function setSession(slug: string, data: WizardValue) {
+	storage?.setItem(storageKey, stringify({ [slug]: data }));
+}
+
 export function createWizardStore(slug: string, title: string, steps: Step[]) {
+	const sessionData = getSession(slug);
+
 	const { set, update, subscribe }: Writable<WizardValue> = writable({
-		returnTo: undefined,
-		data: {},
-		completed: new Set()
+		returnTo: sessionData.returnTo,
+		data: sessionData.data ?? {},
+		completed: sessionData.completed ?? new Set()
 	});
 
 	function init(returnTo: string, data: any = {}) {
-		set({ returnTo, data, completed: new Set() });
-
+		const completed: Set<string> = new Set();
+		const wizard = { returnTo, data, completed };
+		set(wizard);
+		setSession(slug, wizard);
 		goto(`/wizard/${slug}/${steps[0].slug}`);
 	}
 
@@ -35,6 +59,7 @@ export function createWizardStore(slug: string, title: string, steps: Step[]) {
 		const action = completed ? 'add' : 'delete';
 		update((wizard) => {
 			wizard.completed[action](step);
+			setSession(slug, wizard);
 			return wizard;
 		});
 	}
@@ -42,11 +67,12 @@ export function createWizardStore(slug: string, title: string, steps: Step[]) {
 	function updateData(data: any) {
 		update((wizard: WizardValue) => {
 			wizard.data = { ...wizard.data, ...data };
+			setSession(slug, wizard);
 			return wizard;
 		});
 	}
 
-	return { init, toggleComplete: toggleComplete, updateData, subscribe, title, steps };
+	return { init, toggleComplete, updateData, subscribe, title, steps };
 }
 
 export type Wizard = ReturnType<typeof createWizardStore>;
