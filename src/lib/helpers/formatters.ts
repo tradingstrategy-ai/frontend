7837701912ca579b664c00bfceaca542feebdd5c
Mikko Ohtamaa <mikko@opensourcehacker.com>
2023-08-15 14:13:39 +0300

    Format

--- src/lib/helpers/formatters.ts
+++ src/lib/helpers/formatters.ts
@@ -131,12 +131,11 @@ export function formatDollar(n: MaybeNumber, minFrag = 2, maxFrag = 2, prefix = 
 			'k'
 		);
 	} else {
-
-    // Make sure we do not exception out
-    // when requesting more accuracy for US dollar prices
-    if(maxFrag < minFrag) {
-      maxFrag = minFrag;
-    }
+		// Make sure we do not exception out
+		// when requesting more accuracy for US dollar prices
+		if (maxFrag < minFrag) {
+			maxFrag = minFrag;
+		}
 
 		return (
 			prefix +
