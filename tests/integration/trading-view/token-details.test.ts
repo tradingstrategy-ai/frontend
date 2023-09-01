7898cbe31aced320d0be036236344b06b34c7c11
Ken Kunz <kennethkunz@gmail.com>
2023-09-01 18:02:04 -0500

    fix token details integration test (pairs table)

--- tests/integration/trading-view/token-details.test.ts
+++ tests/integration/trading-view/token-details.test.ts
@@ -25,7 +25,7 @@ test.describe('token details page', () => {
 		await page.waitForSelector(`${selector} .pair_symbol`);
 		const rows = page.locator(`${selector} tr`);
 		expect(await rows.count()).toBe(10);
-		const rowData = ['ETH-USDC', 'Uniswap v3', '\\$1.50k', '▲ 11.2%', '\\$7.89B', '\\$0', '▼ 0.0%'];
+		const rowData = ['ETH-USDC', 'Uniswap v3', '\\$1.50k', '▲ 11.2%', '\\$7.89B', '\\$0', '---'];
 		await expect(rows.first()).toHaveText(new RegExp(rowData.join('.*')));
 	});
 
