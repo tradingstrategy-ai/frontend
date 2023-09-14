52168b898f7dffe5793ccc3b7b79c38057000548
Ken Kunz <kennethkunz@gmail.com>
2023-09-14 17:22:27 -0500

    improve data/chains endpoint docs

--- src/routes/data/chains/+server.ts
+++ src/routes/data/chains/+server.ts
@@ -1,10 +1,8 @@
 /**
- * This endpoint prerenders (during build time) chain lookup data so it can be
+ * This endpoint prerenders (at build time) chain lookup data so it can be
  * available app-wide at runtime without hitting the API. This is route is
- * fetched by the root +layout.ts load function and returned as `chains`.
- *
+ * fetched by the root +layout.ts load function and returned as `chainInfo`.
  */
-
 import { json } from '@sveltejs/kit';
 import type { ApiChain } from '$lib/helpers/chain';
 import { fetchPublicApi } from '$lib/helpers/public-api';
@@ -15,7 +13,8 @@ export const prerender = true;
 export async function GET({ fetch }) {
 	// Fetch chains from API
 	const apiChains = (await fetchPublicApi(fetch, 'chains')) as ApiChain[];
-	// Convert to simplified ChainInfo objects
+
+	// Convert to simplified ChainInfo objects (see ambient.d.ts)
 	const chains: ChainInfo[] = apiChains.map((c) => ({
 		id: c.chain_id,
 		slug: c.chain_slug,
