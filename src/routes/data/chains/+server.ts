db3dc733e3606aa6a01b1ffae9190d0358a89b0a
Ken Kunz <kennethkunz@gmail.com>
2023-10-13 10:09:56 -0500

    use SWR cache instead of pre-rendering for chains API

--- src/routes/data/chains/+server.ts
+++ src/routes/data/chains/+server.ts
@@ -1,32 +1,42 @@
 /**
- * This endpoint prerenders (at build time) chain lookup data so it can be
- * available app-wide at runtime without hitting the API. This is route is
- * fetched by the root +layout.ts load function and returned as `chainInfo`.
+ * Endpoint with server-side SWR caching of chains API
+ *
+ * Since we want the site to load (where possible) even if the API is down,
+ * we fallback to a local chains cache when the API request fails.
+ *
+ * This endpoint is fetched by the root layout and returned as `chainInfo`,
+ * making chain lookup data available to all layouts/pages.
  */
 import { json } from '@sveltejs/kit';
+import swrCache from '$lib/swrCache.js';
 import type { ApiChain } from '$lib/helpers/chain';
 import { fetchPublicApi } from '$lib/helpers/public-api';
 
-// fetch and render at build-time
-export const prerender = true;
+// Fetch chains from API; fallback to local chains cache if API is unavailable
+async function fetchChainsWithFallback(fetch: Fetch): Promise<ApiChain[]> {
+	try {
+		const chains = (await fetchPublicApi(fetch, 'chains')) as ApiChain[];
+		// limit cached properties to chain_id, chain_slug, chain_name
+		return chains.map(({ chain_id, chain_slug, chain_name }) => ({ chain_id, chain_slug, chain_name }));
+	} catch (e) {
+		console.warn('API unavailable, falling back to local chains cache', e);
+		return (await import('./cache.json')).default;
+	}
+}
+
+// Create a SWR cache for chains with 5 minute TTL
+const cacheTimeSeconds = 5 * 60;
+const getCachedChains = swrCache(fetchChainsWithFallback, cacheTimeSeconds);
 
 export async function GET({ fetch }) {
-	let apiChains: ApiChain[];
+	const chains = await getCachedChains(fetch);
 
-	try {
-		// fetch chains from API
-		apiChains = (await fetchPublicApi(fetch, 'chains')) as ApiChain[];
-	} catch (e) {
-		// fallback to local chain cache if API is unavailable at build time
-		apiChains = (await import('./cache.json')).default;
-	}
-
-	// limit prerendered chain properties to: chain_id, chain_slug, and chain_name
-	const chains: ApiChain[] = apiChains.map((chain) => ({
-		chain_id: chain.chain_id,
-		chain_slug: chain.chain_slug,
-		chain_name: chain.chain_name
-	}));
-
-	return json(chains);
+	return json(chains, {
+		// Setting cache-control and age headers to limit re-fetching
+		// of this resource by browser and reverse proxy / CDN
+		headers: {
+			'cache-control': `public, max-age=${cacheTimeSeconds}`,
+			age: getCachedChains.getAge(fetch).toFixed(0)
+		}
+	});
 }
