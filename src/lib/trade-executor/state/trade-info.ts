e906ec8a30a21d6b5a59a7108f139dad2a822fb3
Ken Kunz <kennethkunz@gmail.com>
2023-12-28 18:19:48 -0600

    refactor: use prototype inheritance for TradeInfo

--- src/lib/trade-executor/state/trade-info.ts
+++ src/lib/trade-executor/state/trade-info.ts
@@ -1,5 +1,5 @@
 /**
- * Factor to create a wrapped/enriched TradeInfo object
+ * Factory to create a wrapped/enriched TradeInfo object
  *
  * Based on Python class found in:
  * https://github.com/tradingstrategy-ai/trade-executor/blob/master/tradeexecutor/state/trade.py
@@ -8,32 +8,38 @@
 import type { TradeExecution } from './trade';
 import type { USDollarAmount } from './utility-types';
 
+// Prototype object that can be applied to a TradeExecution object to enrich
+// it with additional properties. Yields an object with all the properties
+// (and types) of the original plus the inherited prototype properties/types
+// (which is non-trivial with TypeScript classes)
+const tradeInfoPrototype = {
+	// Estimate the USD value of this trade
+	// `get_executed_value` in trade.py
+	get executedValue(): USDollarAmount {
+		const quantity = this.executed_quantity ?? 0;
+		const price = this.executed_price ?? 0;
+		return Math.abs(quantity) * price;
+	},
+
+	// Determine trade direction ('Buy' vs. 'Sell') based on planned_quantity
+	// TODO: replace with method that supports trade flags (open, close, increase, reduce, etc.)
+	get direction() {
+		return this.planned_quantity > 0 ? 'Buy' : 'Sell';
+	},
+
+	get failed() {
+		return this.failed_at != null;
+	},
+
+	get failedTx() {
+		return this.blockchain_transactions.find((tx) => tx.revert_reason);
+	}
+} satisfies ThisType<TradeExecution>;
+
+export type TradeInfo = TradeExecution & typeof tradeInfoPrototype;
+
+// Factory function to create a TradeInfo object
 export function createTradeInfo(data: TradeExecution) {
-	return {
-		...data,
-
-		// Estimate the USD value of this trade
-		// `get_executed_value` in trade.py
-		get executedValue(): USDollarAmount {
-			const quantity = data.executed_quantity ?? 0;
-			const price = data.executed_price ?? 0;
-			return Math.abs(quantity) * price;
-		},
-
-		// Determine trade direction ('Buy' vs. 'Sell') based on planned_quantity
-		// TODO: replace with method that supports trade flags (open, close, increase, reduce, etc.)
-		get direction() {
-			return data.planned_quantity > 0 ? 'Buy' : 'Sell';
-		},
-
-		get failed() {
-			return data.failed_at != null;
-		},
-
-		get failedTx() {
-			return data.blockchain_transactions.find((tx) => tx.revert_reason);
-		}
-	};
+	const tradeInfo = Object.create(tradeInfoPrototype) as TradeInfo;
+	return Object.assign(tradeInfo, data);
 }
-
-export type TradeInfo = ReturnType<typeof createTradeInfo>;
