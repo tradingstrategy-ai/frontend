ee90e8396a24046777a4c2e1f88882484ec477b7
Ken Kunz <kennethkunz@gmail.com>
2024-02-14 17:04:20 -0600

    add live/backtesting toggle to performance page

--- src/lib/trade-executor/state/statistics.ts
+++ src/lib/trade-executor/state/statistics.ts
@@ -51,12 +51,15 @@ const longShortDatasets = z.object({
 	backtested_stats: longShortTableSchema
 });
 
+const longShortMetricsSchema = z.union([longShortTableSchema, longShortDatasets]);
+export type LongShortMetrics = z.infer<typeof longShortMetricsSchema>;
+
 // TODO: remove long_short_metrics_latest compatibility layer once trade-executor updates deployed
 // see: https://github.com/tradingstrategy-ai/trade-executor/issues/760
 export const statisticsSchema = z.object({
 	portfolio: portfolioStatisticsSchema.array(),
 	positions: z.record(primaryKeyString, positionStatisticsSchema.array()),
 	closed_positions: z.record(primaryKeyString, finalPositionStatisticsSchema),
-	long_short_metrics_latest: z.union([longShortTableSchema, longShortDatasets]).nullish()
+	long_short_metrics_latest: longShortMetricsSchema.nullish()
 });
 export type Statistics = z.infer<typeof statisticsSchema>;
