d81d7153a11b8ffb1d139861d9589772e8de3172
Ken Kunz <kennethkunz@gmail.com>
2024-03-21 08:58:01 -0500

    WIP tech-details layout (nav)

---
+++ src/routes/strategies/[strategy]/tech-details/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { Menu, MenuItem } from '$lib/components';
+
+	$: currentTab = $page.url.pathname.split('/').at(-1);
+</script>
+
+<section>
+	<Menu horizontal>
+		<MenuItem label="Status" targetUrl="status" active={currentTab === 'status'} />
+		<MenuItem label="Logs" targetUrl="logs" active={currentTab === 'logs'} />
+		<MenuItem label="Decision making" targetUrl="decision-making" active={currentTab === 'decision-making'} />
+	</Menu>
+	<slot />
+</section>
