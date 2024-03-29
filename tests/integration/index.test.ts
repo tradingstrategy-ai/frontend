86d50cb1790ca25b7e6c71462b984964478171de
Ken Kunz <kennethkunz@gmail.com>
2024-03-29 11:15:29 -0500

    add script for updating test screenshots

--- tests/integration/index.test.ts
+++ tests/integration/index.test.ts
@@ -6,11 +6,8 @@ test.describe('home page', () => {
 	});
 
 	// This test is useful for validating that custom fonts are installed and loaded.
-	// If there's a legitimate change to the hero banner, generate new screenshots by
-	// deleting the old ones from `tests/integration/index.test.ts-snapshots` and running
-	// the folling commands (2nd one is needed for CI to pass correctly):
-	// $ npm run test:integration
-	// $ docker run --rm -v $(pwd):/work/ mcr.microsoft.com/playwright:v1.27.0-focal bash -c 'cd work && npm run test:integration --skip-build'
+	// If there's a legitimate change to the hero banner, generate new screenshots:
+	// $ ./scripts/update-test-screenshots.sh
 	test('hero banner looks correct', async ({ page }) => {
 		const header = page.getByTestId('home-hero-banner');
 		await expect(header).toHaveScreenshot();
