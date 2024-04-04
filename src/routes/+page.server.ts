fa15dd85d2dfae8a0fe65fcc6503d7d5850e7909
Ken Kunz <kennethkunz@gmail.com>
2024-04-04 18:14:26 -0500

    add "Top strategies" to front page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,7 +3,8 @@ import { getCachedStrategies } from 'trade-executor/strategy/runtime-state';
 export async function load({ fetch }) {
 	const strategies = await getCachedStrategies(fetch);
 
+	// return first 3 live strategies
 	return {
-		strategies: strategies.filter((s) => s.tags?.includes('live'))
+		strategies: strategies.filter((s) => s.tags?.includes('live')).slice(0, 3)
 	};
 }
