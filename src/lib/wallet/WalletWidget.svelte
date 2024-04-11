34c24ee21ba81da9bb6e4963d615aefaaaaddae0
Ken Kunz <kennethkunz@gmail.com>
2024-04-11 18:26:52 -0500

    show web3modal account view from WalletWidget

--- src/lib/wallet/WalletWidget.svelte
+++ src/lib/wallet/WalletWidget.svelte
@@ -2,7 +2,7 @@
 	import type { ConnectedStrategyRuntimeState } from 'trade-executor/strategy/runtime-state';
 	import { goto } from '$app/navigation';
 	import { wizard } from 'wizard/store';
-	import { disconnect, wallet } from '$lib/wallet';
+	import { modal, wallet } from '$lib/wallet';
 	import { Button, HashAddress, Icon } from '$lib/components';
 
 	export let strategy: ConnectedStrategyRuntimeState;
@@ -21,16 +21,12 @@
 
 <div class="wallet-widget">
 	{#if $wallet.isConnected}
-		<Button size="sm" on:click={disconnect}>
+		<Button size="sm" on:click={() => modal.open({ view: 'Account' })}>
 			<span class="address">
 				<Icon name="wallet" --icon-size="1.25em" />
 				<HashAddress address={$wallet.address} endChars={7} />
 			</span>
 		</Button>
-		<span class="hover-info">
-			<Icon name="unlink" --icon-size="1.25em" />
-			Disconnect wallet
-		</span>
 	{:else}
 		<Button size="sm" icon="wallet" label="Connect wallet" on:click={launchConnectWizard}>
 			<Icon slot="icon" name="wallet" --icon-size="1.25em" />
@@ -39,10 +35,6 @@
 </div>
 
 <style lang="postcss">
-	.wallet-widget {
-		position: relative;
-	}
-
 	.address {
 		display: inline-grid;
 		grid-auto-flow: column;
@@ -54,25 +46,6 @@
 
 		:global(.icon svg) {
 			margin-top: -3px;
-		}
-	}
-
-	.hover-info {
-		position: absolute;
-		left: 0;
-		right: 0;
-		bottom: -2em;
-		display: flex;
-		gap: 1ex;
-		justify-content: center;
-		font: var(--f-ui-sm-medium);
-		letter-spacing: var(--ls-ui-sm);
-		color: var(--c-text-extra-light);
-		opacity: 0;
-		transition: opacity var(--time-xs) ease-out;
-
-		.wallet-widget:hover &:not(:hover) {
-			opacity: 1;
 		}
 	}
 </style>
