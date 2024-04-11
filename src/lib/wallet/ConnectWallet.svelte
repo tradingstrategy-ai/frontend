447f335a3fcee5f570d4067c2a9637a6a29d7e78
Ken Kunz <kennethkunz@gmail.com>
2024-04-11 16:58:22 -0500

    customize copy for mobile vs. desktop

--- src/lib/wallet/ConnectWallet.svelte
+++ src/lib/wallet/ConnectWallet.svelte
@@ -11,35 +11,48 @@
 
 <div class="connect-wallet">
 	{#if $wallet.isConnected}
-		<div class="connected-wallet">
+		<div class="is-connected">
 			<WalletSummary wallet={$wallet} {chain} />
 			<Button size="sm" label="Change wallet" on:click={disconnect} />
 		</div>
 	{:else}
-		<div class="wallet-options">
-			Connect your preferred browser-based, mobile or desktop wallet.
-			<Button icon="wallet" label="Connect wallet" on:click={() => modal.open()} />
+		<div class="not-connected">
+			<div class="desktop">Connect your preferred browser-based, mobile or desktop wallet.</div>
+			<div class="mobile">Connect your preferred mobile wallet.</div>
+			<Button size="md" icon="wallet" label="Connect wallet" on:click={() => modal.open()} />
 		</div>
 	{/if}
 </div>
 
 <style lang="postcss">
-	.connect-wallet {
-		display: grid;
-		gap: var(--space-xl);
-	}
-
-	.connected-wallet {
-		display: grid;
-		place-items: start;
-		gap: var(--space-ls);
-
-		@media (--viewport-sm-down) {
-			gap: var(--space-xl);
+	.desktop {
+		@media (--viewport-xs) {
+			display: none;
 		}
 	}
 
-	.wallet-options {
+	.mobile {
+		@media (--viewport-sm-up) {
+			display: none;
+		}
+	}
+
+	.connect-wallet {
+		display: grid;
+		gap: 2rem;
+	}
+
+	.is-connected {
+		display: grid;
+		place-items: start;
+		gap: 1.25rem;
+
+		@media (--viewport-sm-down) {
+			gap: 2rem;
+		}
+	}
+
+	.not-connected {
 		display: grid;
 		gap: 2.5rem;
 		margin-bottom: 2.5rem;
