49438bc8922766c19bb3964736634e8506af3e55
Ken Kunz <kennethkunz@gmail.com>
2024-05-15 23:42:53 -0500

    refactor - use sitemap.js for glossary/sitemap.xml

--- src/routes/glossary/sitemap.xml/+server.ts
+++ src/routes/glossary/sitemap.xml/+server.ts
@@ -3,44 +3,26 @@
  *
  * - All glossary terms are boosted to crawling priority 1.0
  */
+import { SitemapStream } from 'sitemap';
+import { Readable } from 'stream';
 import { getCachedGlossary } from '../glossary.js';
 
-/**
- * Add the default XML headers around sitemap body
- *
- */
-function renderSitemapHeaders(body: string): string {
-	return `<?xml version="1.0" encoding="utf-8"?>
-		<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-		xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-		xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-		${body}
-		</urlset>`;
-}
-
-/**
- * Handle sitemap.xml generation
- *
- */
-export async function GET({ fetch }) {
-	const baseUrl = 'https://tradingstrategy.ai/glossary';
+export async function GET({ fetch, setHeaders, url }) {
 	const glossary = await getCachedGlossary(fetch);
 
-	// Build sitemap XML
-	const sitemapXmlBody = Object.keys(glossary)
-		.map((slug) => {
-			const fullUrl = `${baseUrl}/${slug}`;
-			return `<url><loc>${fullUrl}</loc><priority>1.0</priority></url>`;
-		})
-		.join('');
+	const stream = new SitemapStream({ hostname: url.origin });
+	const entries = Object.keys(glossary).map((slug) => ({
+		url: `glossary/${slug}`,
+		priority: 1.0
+	}));
+	Readable.from(entries).pipe(stream);
 
-	const sitemapXml = renderSitemapHeaders(sitemapXmlBody);
-
-	const headers = {
+	setHeaders({
 		'content-type': 'application/xml',
 		'cache-control': `public, max-age=${getCachedGlossary.ttl}`,
 		age: getCachedGlossary.getAge(fetch).toFixed(0)
-	};
+	});
 
-	return new Response(sitemapXml, { headers });
+	// coerce stream to ReadableStream to make TypeScript happy
+	return new Response(stream as unknown as ReadableStream<Uint8Array>);
 }
