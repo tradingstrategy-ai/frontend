d077a70087813855b18fb07f2b7e9464c4d7e771
Ken Kunz <kennethkunz@gmail.com>
2024-06-02 23:59:34 -0500

    improve strategy tvl chart formatting

--- src/routes/strategies/StrategyTvlChart.svelte
+++ src/routes/strategies/StrategyTvlChart.svelte
@@ -24,33 +24,49 @@
 	}
 </script>
 
-<ChartContainer title="Strategy TVL">
-	<span slot="subtitle">
-		<a class="body-link" target="_blank" href="/glossary/total-value-locked"> Total value locked </a>
-		in live strategies (past {spanDays} days)
-	</span>
+<div class="strategy-tvl-chart">
+	<ChartContainer title="Strategy TVL">
+		<span slot="subtitle">
+			<a class="body-link" target="_blank" href="/glossary/total-value-locked"> Total value locked </a>
+			in live strategies (past {spanDays} days)
+		</span>
 
-	{#if tvlData}
-		<PerformanceChart
-			options={chartOptions}
-			data={normalizeDataForInterval(tvlData ?? [], utcDay)}
-			formatValue={formatDollar}
-			{spanDays}
-			periodicity={{ period: 1, interval: 1, timeUnit: 'day' }}
-		/>
-	{:else}
-		<div class="error">
-			<Alert size="md" title="Error">Strategy TVL data failed to load.</Alert>
-		</div>
-	{/if}
-</ChartContainer>
+		{#if tvlData}
+			<PerformanceChart
+				options={chartOptions}
+				data={normalizeDataForInterval(tvlData ?? [], utcDay)}
+				formatValue={formatDollar}
+				{spanDays}
+				periodicity={{ period: 1, interval: 1, timeUnit: 'day' }}
+			/>
+		{:else}
+			<div class="error">
+				<Alert size="md" title="Error">Strategy TVL data failed to load.</Alert>
+			</div>
+		{/if}
+	</ChartContainer>
+</div>
 
 <style lang="postcss">
-	.error {
-		padding: 1.5rem;
+	.strategy-tvl-chart {
+		:global([data-css-props]) {
+			--chart-aspect-ratio: 2.5;
 
-		@media (--viewport-md-down) {
-			padding: 1rem;
+			@media (--viewport-sm-down) {
+				--chart-aspect-ratio: 2.25;
+			}
+
+			@media (--viewport-xs) {
+				--chart-aspect-ratio: 2;
+			}
+		}
+
+		.error {
+			padding: 1.5rem;
+
+			@media (--viewport-md-down) {
+				padding: 1rem;
+			}
 		}
 	}
 </style>
