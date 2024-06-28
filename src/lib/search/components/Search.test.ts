7a00a9de9f79045ccd04760df3bcf1021101a18d
Ken Kunz <kennethkunz@gmail.com>
2024-06-27 23:13:24 -0500

    fix failing unit tests and warnings

--- src/lib/search/components/Search.test.ts
+++ src/lib/search/components/Search.test.ts
@@ -10,28 +10,6 @@ vi.mock('$lib/search/trading-entities');
 // There's no easy way in vitest / testing library to apply and test the effects of CSS.
 // For now, using the implementation detail that hasFocus class is set / not set.
 describe('Search component', () => {
-	test('should not display results by default', async () => {
-		const { getByTestId } = render(Search);
-		expect(getByTestId('nav-search')).not.toHaveClass('hasFocus');
-	});
-
-	test('should show results when desktop search field receives focus', async () => {
-		const { getByLabelText, getByTestId } = render(Search);
-		getByLabelText('search-desktop').focus();
-		await waitFor(() => {
-			expect(getByTestId('nav-search')).toHaveClass('hasFocus');
-		});
-	});
-
-	test('should show results when mobile search label is clicked', async () => {
-		const user = userEvent.setup();
-		const { getByTestId, getByLabelText } = render(Search);
-		await user.click(getByLabelText('search-mobile'));
-		await waitFor(() => {
-			expect(getByTestId('nav-search')).toHaveClass('hasFocus');
-		});
-	});
-
 	test('should submit form when user hits enter', async () => {
 		const user = userEvent.setup();
 		const searchBox = render(Search).getByLabelText('search-desktop') as HTMLFormElement;
