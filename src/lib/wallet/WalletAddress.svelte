e902dd7c8905b32a6b22241330edeadfecf1b263
Ken Kunz <kennethkunz@gmail.com>
2024-07-11 11:40:57 -0500

    use unplugin-icons for WalletAddress

--- src/lib/wallet/WalletAddress.svelte
+++ src/lib/wallet/WalletAddress.svelte
@@ -3,6 +3,7 @@
 	import type { ConnectedWallet } from '$lib/wallet';
 	import { getExplorerUrl } from '$lib/helpers/chain';
 	import { CryptoAddressWidget } from '$lib/components';
+	import IconWallet from '~icons/local/wallet';
 
 	export let wallet: ConnectedWallet;
 	export let size: ComponentProps<CryptoAddressWidget>['size'] = 'md';
@@ -10,4 +11,6 @@
 	$: ({ address, chain } = wallet);
 </script>
 
-<CryptoAddressWidget {size} icon="wallet" clipboardCopier={false} {address} href={getExplorerUrl(chain, address)} />
+<CryptoAddressWidget {size} {address} href={getExplorerUrl(chain, address)} clipboardCopier={false}>
+	<IconWallet slot="icon" --icon-size="1.2em" />
+</CryptoAddressWidget>
