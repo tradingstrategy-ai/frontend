96728fed50c171d408a89136110a581ac7c08a18
Ken Kunz <kennethkunz@gmail.com>
2024-08-02 14:43:33 -0500

    fix unhandled promise when loading bogus chain slug
    see Sentry Event ID: c7d9a06777fc4f5faed54e004d616694

--- src/routes/trading-view/[chain=slug]/TopEntities.svelte
+++ src/routes/trading-view/[chain=slug]/TopEntities.svelte
@@ -3,16 +3,24 @@
 	import type { ApiChain } from '$lib/helpers/chain';
 	import { Alert, Button, SummaryBox } from '$lib/components';
 
+	type EntityData = { error: object; rows?: [] } | { error?: undefined; rows: Record<string, any>[] };
+
 	export let type: string;
 	export let label = type.replaceAll('-', ' ');
 	export let title: string;
 	export let chain: ApiChain;
-	export let data: Promise<Record<string, any>[]>;
+	export let data: Promise<EntityData>;
 	export let tableComponent: ComponentType;
 	export let rightColHeader = '';
 
 	let hasData = true;
-	$: data.then((rows) => (hasData = rows.length > 0));
+
+	$: data.then(({ rows, error }) => {
+		hasData = !error && rows.length > 0;
+		if (error) {
+			console.error(`Error loading ${label}:`, error);
+		}
+	});
 </script>
 
 <SummaryBox {title} ctaPosition="bottom">
@@ -25,15 +33,15 @@
 
 	{#await data}
 		<svelte:component this={tableComponent} loading rows={Array(5).fill({})} />
-	{:then rows}
-		<svelte:component this={tableComponent} {rows} />
-	{:catch}
-		<Alert>An error occurred loading {label}. Try reloading the page.</Alert>
+	{:then { rows, error }}
+		{#if error}
+			<Alert size="md" title="Error">An error occurred loading {label}. Try reloading the page.</Alert>
+		{:else if hasData}
+			<svelte:component this={tableComponent} {rows} />
+		{:else}
+			<p>No {label} available for {chain.chain_name}</p>
+		{/if}
 	{/await}
-
-	{#if !hasData}
-		<p>No {label} available for {chain.chain_name}</p>
-	{/if}
 
 	<Button slot="cta" href="/trading-view/{chain.chain_slug}/{type}" disabled={!hasData}>
 		View all {chain.chain_name}
