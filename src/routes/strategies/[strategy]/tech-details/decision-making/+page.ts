5e9949a84e41c4e9729a6ead3f1a7ca7c7246a79
Ken Kunz <kennethkunz@gmail.com>
2024-08-02 17:16:37 -0500

    fix unhandled fetch error in strategy sub pages
    see Sentry Event ID: 0c07f033142c4926883035e6fca5826d

--- src/routes/strategies/[strategy]/tech-details/decision-making/+page.ts
+++ src/routes/strategies/[strategy]/tech-details/decision-making/+page.ts
@@ -3,10 +3,12 @@
  *
  * Generate a variant of decision making status image URL for both color schemes.
  */
+import { error } from '@sveltejs/kit';
 import { configuredStrategies } from 'trade-executor/strategy/configuration';
 
 export async function load({ params }) {
-	const strategy = configuredStrategies.get(params.strategy)!;
+	const strategy = configuredStrategies.get(params.strategy);
+	if (!strategy) error(404, 'Not found');
 
 	const imageUrls: Record<string, string> = {};
 
--- src/routes/strategies/[strategy]/tech-details/logs/+page.ts
+++ src/routes/strategies/[strategy]/tech-details/logs/+page.ts
@@ -1,11 +1,13 @@
 /**
  * Fetch the server logs on the page load.
  */
+import { error } from '@sveltejs/kit';
 import { publicApiError } from '$lib/helpers/public-api';
 import { configuredStrategies } from 'trade-executor/strategy/configuration';
 
 export async function load({ params, fetch }) {
-	const strategy = configuredStrategies.get(params.strategy)!;
+	const strategy = configuredStrategies.get(params.strategy);
+	if (!strategy) error(404, 'Not found');
 
 	const resp = await fetch(`${strategy.url}/logs`);
 	if (!resp.ok) throw await publicApiError(resp);
