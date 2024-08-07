ef860c973d7151f0273f62285d76de3ccee6fa64
Ken Kunz <kennethkunz@gmail.com>
2024-08-07 17:55:46 -0500

    exclude outdated strategies from front page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,14 @@
 import { getCachedStrategies } from 'trade-executor/strategy/runtime-state';
 
 export async function load({ fetch }) {
-	const strategies = await getCachedStrategies(fetch);
+	// get first 3 live strategies (excluding outdated)
+	const strategies = (await getCachedStrategies(fetch))
+		.filter((s) => {
+			const isLive = s.tags?.includes('live');
+			const isCurrent = !s.new_version_id;
+			return isLive && isCurrent;
+		})
+		.slice(0, 3);
 
-	// return first 3 live strategies
-	return {
-		strategies: strategies.filter((s) => s.tags?.includes('live')).slice(0, 3)
-	};
+	return { strategies };
 }
