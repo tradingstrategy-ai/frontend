6b03393b99f85c595641193028d514c7c6915087
Ken Kunz <kennethkunz@gmail.com>
2024-09-27 15:32:48 -0500

    display correct denomination token on redemption intro

--- src/routes/wizard/redeem/introduction/+page.svelte
+++ src/routes/wizard/redeem/introduction/+page.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
 	import { wizard } from 'wizard/store';
 
+	export let data;
+	const { denominationToken } = data;
+
 	wizard.toggleComplete('introduction');
 </script>
 
 <div class="deposit-introduction">
 	<p>
-		Ready to redeem shares of <strong>{$wizard.data.strategyName}</strong>? Upon redemption, you'll receive USDC.e and
-		any tokens the strategy is currently invested in. Connect your wallet, review your share balance, enter your desired
-		redemption amount, and complete your transaction. Let's get started!
+		Ready to redeem shares of <strong>{$wizard.data.strategyName}</strong>? Upon redemption, you'll receive
+		<strong>{denominationToken.label}</strong> and any tokens the strategy is currently invested in. Connect your wallet,
+		review your share balance, enter your desired redemption amount, and complete your transaction. Let's get started!
 	</p>
 </div>
---
+++ src/routes/wizard/redeem/introduction/+page.ts
@@ -0,0 +1,16 @@
+import type { EnzymeSmartContracts } from 'trade-executor/strategy/summary';
+import { type ConfiguredChainId, config } from '$lib/wallet';
+import { get } from 'svelte/store';
+import { wizard } from 'wizard/store';
+import { getAccount } from '@wagmi/core';
+import { getDenominationToken } from '$lib/eth-defi/helpers';
+
+export async function load() {
+	const { address } = getAccount(config) as { address: Address };
+	const { chainId, contracts } = get(wizard).data! as { chainId: ConfiguredChainId; contracts: EnzymeSmartContracts };
+	const comptroller = contracts.comptroller!;
+
+	return {
+		denominationToken: await getDenominationToken(config, { address, comptroller, chainId })
+	};
+}
