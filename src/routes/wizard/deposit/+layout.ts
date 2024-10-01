0943987c03ad1dfd1c2beebefcd12006ddf01e4b
Ken Kunz <kennethkunz@gmail.com>
2024-10-01 16:17:18 -0500

    fetch denomination token when deposit wizard loads

--- src/routes/wizard/deposit/+layout.ts
+++ src/routes/wizard/deposit/+layout.ts
@@ -1,7 +1,18 @@
 import type { EnzymeSmartContracts } from 'trade-executor/strategy/summary';
+import { type ConfiguredChainId, config } from '$lib/wallet';
 import { get } from 'svelte/store';
 import { wizard } from 'wizard/store';
+import { getAccount } from '@wagmi/core';
 import { assertNotGeoBlocked } from '$lib/helpers/geo';
+import { type GetTokenBalanceReturnType, getDenominationToken } from '$lib/eth-defi/helpers';
+
+export type DepositWizardData = {
+	chainId: ConfiguredChainId;
+	strategyName: string;
+	contracts: EnzymeSmartContracts;
+	canForwardPayment: boolean;
+	denominationToken: GetTokenBalanceReturnType;
+};
 
 export async function load({ parent }) {
 	const { admin, ipCountry } = await parent();
@@ -18,11 +29,24 @@ export async function load({ parent }) {
 		{ slug: 'success', label: 'Success' }
 	];
 
+	const { chainId, contracts } = get(wizard).data as DepositWizardData;
+
 	// skip "Terms of service" step if no terms_of_service contract
-	const contracts: EnzymeSmartContracts = get(wizard).data!.contracts;
 	if (!contracts.terms_of_service) {
 		steps = steps.filter(({ slug }) => slug !== 'tos');
 	}
+
+	// get denomination token info and balance
+	const denominationToken = await getDenominationToken(config, {
+		chainId,
+		address: getAccount(config).address!,
+		comptroller: contracts.comptroller!
+	});
+
+	// USDC can forward payment using transferWithAuthorizations; other tokens can't (yet)
+	const canForwardPayment = denominationToken.symbol === 'USDC';
+
+	wizard.updateData({ denominationToken, canForwardPayment });
 
 	return { title, steps };
 }
--- src/routes/wizard/deposit/introduction/+page.svelte
+++ src/routes/wizard/deposit/introduction/+page.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
+	import type { DepositWizardData } from '../+layout';
 	import { wizard } from 'wizard/store';
+
+	const { strategyName, denominationToken } = $wizard.data as DepositWizardData;
 
 	wizard.toggleComplete('introduction');
 </script>
 
 <div class="deposit-introduction">
 	<p>
-		Ready to deposit in <strong>{$wizard.data.strategyName}</strong>? Deposit USDC.e to add this strategy to your DeFi
-		trading portfolio. Connect your wallet, review your balances, enter your desired deposit amount, and make your
-		payment. Let's get started!
+		Ready to deposit in <strong>{strategyName}</strong>? Deposit <strong>{denominationToken.label}</strong> to add this strategy
+		to your DeFi trading portfolio. Connect your wallet, review your balances, enter your desired deposit amount, and make
+		your payment. Let's get started!
 	</p>
 </div>
