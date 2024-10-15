26f148ab8eaca5965152a08df192f470524e145c
Ken Kunz <kennethkunz@gmail.com>
2024-10-15 06:49:47 -0500

    update "Buy [TOKEN]" URL to Uniswap "swap" page
    close #835

--- src/lib/wallet/helpers.ts
+++ src/lib/wallet/helpers.ts
@@ -4,16 +4,21 @@
 export function buyNativeCurrencyUrl(chainId: number): string | undefined {
 	const urls: Record<number, string> = {
 		1: 'https://ethereum.org/en/get-eth/',
-		137: 'https://wallet.polygon.technology/polygon/gas-swap'
+		137: 'https://wallet.polygon.technology/polygon/gas-swap',
+		42161: 'https://bridge.arbitrum.io/?destinationChain=arbitrum-one&sourceChain=ethereum'
 	};
 
 	return urls[chainId];
 }
 
 /**
- * Return URL for Uniswap v3 page to purchase tokens
+ * Return URL for Uniswap v3 "swap" page to purchase tokens
+ *
+ * @param chain - chain slug ('ethereum', 'arbitrum', etc.)
+ * @param outputCurrency - address for desired ERC-20 token
  */
-export function buyTokenUrl(chainSlug: string, tokenAddress: Address) {
-	const baseUrl = 'https://app.uniswap.org/tokens';
-	return `${baseUrl}/${chainSlug}/${tokenAddress}?chain=${chainSlug}`;
+export function buyTokenUrl(chain: string, outputCurrency: Address) {
+	const baseUrl = 'https://app.uniswap.org/swap';
+	const params = new URLSearchParams({ chain, outputCurrency });
+	return `${baseUrl}?${params}`;
 }
