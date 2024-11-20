0aeb77bdebc368eba518df4463fa76febf81b79e
Ken Kunz <kennethkunz@gmail.com>
2024-11-20 17:23:56 -0600

    fix pair details e2e test

--- tests/e2e/trading-view/pair-details.test.ts
+++ tests/e2e/trading-view/pair-details.test.ts
@@ -3,8 +3,8 @@ import { expect, test } from '@playwright/test';
 test('pair details should include pair summary info', async ({ page }) => {
 	await page.goto('/trading-view/ethereum/uniswap-v2/wise-eth');
 	const pairInfo = page.getByTestId('pair-info');
-	await expect(pairInfo).toHaveText(/Token WISE/);
-	await expect(pairInfo).toHaveText(/Quoted in ETH/);
-	await expect(pairInfo).toHaveText(/Price [\d.,]+ USD/);
-	await expect(pairInfo).toHaveText(/Token price [\d.,]+ ETH/);
+	await expect(pairInfo).toHaveText(/Token\s+WISE/);
+	await expect(pairInfo).toHaveText(/Quoted in\s+ETH/);
+	await expect(pairInfo).toHaveText(/Price\s+[\d.,]+\s+USD/);
+	await expect(pairInfo).toHaveText(/Token price\s+[\d.,]+\s+ETH/);
 });
