3195aacb5b40a4413a82ff0aaf0c414741dbadc5
Ken Kunz <kennethkunz@gmail.com>
2024-11-27 18:35:22 -0600

    restict strategy microsite routes

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,24 @@
-// Make admin, ipCountry and announcementDismissedAt available to all layouts/pages
-export async function load({ locals }) {
+import { strategyMicrosite } from '$lib/config';
+import { error, redirect } from '@sveltejs/kit';
+
+export async function load({ locals, route }) {
+	// restrict routes on custom strategy site
+	if (strategyMicrosite) {
+		restrictMicrositeRoutes(route.id!, strategyMicrosite);
+	}
+
+	// Make admin, ipCountry and announcementDismissedAt available to all layouts/pages
 	const { admin, ipCountry, announcementDismissedAt } = locals;
 	return { admin, ipCountry, announcementDismissedAt };
+}
+
+function restrictMicrositeRoutes(routeId: string, strategyId: string) {
+	// redirect home page to strategy page
+	if (routeId === '/') redirect(302, `/strategies/${strategyId}`);
+
+	// return 404 for all pages other than strategies, glossary and diagnostics
+	const segments = routeId.split('/').slice(1);
+	if (!['strategies', 'glossary', 'diagnostics'].includes(segments[0])) {
+		error(404, 'Not found');
+	}
 }
