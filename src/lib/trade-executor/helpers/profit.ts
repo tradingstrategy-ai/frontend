dc05a679ac670f3883887b577dd2cdc826926363
Ken Kunz <kennethkunz@gmail.com>
2024-12-12 18:33:04 -0600

    remove unused determineProfitability helper

--- src/lib/trade-executor/helpers/profit.ts
+++ src/lib/trade-executor/helpers/profit.ts
@@ -1,22 +1,4 @@
-import { isNumber, toFloatingPoint, formatPercent } from '$lib/helpers/formatters';
-
-/**
- * Determine profitability above/below threshold
- *
- * Neutral if profitability rounds to 0.0%
- *
- * @return -1 (loss) | 0 (neutral) | +1 (profit)
- *
- */
-export function determineProfitability(n: MaybeNumberlike): number {
-	const value = toFloatingPoint(n) ?? 0;
-
-	// use formatter to get properly rounded value
-	const percentStr = formatPercent(value);
-
-	// neutral if rounds to zero; otherwise return sign of the value
-	return percentStr === '0.0%' ? 0 : Math.sign(value);
-}
+import { isNumber } from '$lib/helpers/formatters';
 
 /**
  * Calculate a relative profitability percent for a given time period
