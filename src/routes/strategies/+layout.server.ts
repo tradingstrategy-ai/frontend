30a7cbab7b7b3709c254707d22a1590e16943fd2
Ken Kunz <kennethkunz@gmail.com>
2025-02-04 12:18:31 -0600

    add `geoBlockDemo` URL param to demo geo-block

--- src/routes/strategies/+layout.server.ts
+++ src/routes/strategies/+layout.server.ts
@@ -1,6 +1,13 @@
 import { assertNotGeoBlocked } from '$lib/helpers/geo';
 
-export async function load({ locals }) {
+export async function load({ locals, url }) {
 	const { admin, ipCountry } = locals;
 	assertNotGeoBlocked('strategies:view', ipCountry, admin);
+
+	// URL param to demo the geo-block feature, e.g.:
+	// https://tradingstrategy.ai/strategies?geoBlockDemo=RU
+	const demoIpCountry = url.searchParams.get('geoBlockDemo');
+	if (demoIpCountry) {
+		assertNotGeoBlocked('strategies:view', demoIpCountry);
+	}
 }
