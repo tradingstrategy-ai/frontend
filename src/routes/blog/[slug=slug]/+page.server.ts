0db9cd5a81953532ded3357a8a20f2af702dd4d3
Ken Kunz <kennethkunz@gmail.com>
2025-02-26 08:53:49 -0600

    use server-side requests for individual blog post pages

--- src/routes/blog/[slug=slug]/+page.server.ts
+++ src/routes/blog/[slug=slug]/+page.server.ts
@@ -1,11 +1,15 @@
 import ghostClient from '$lib/blog/client';
 import { error } from '@sveltejs/kit';
 
-export async function load({ params }) {
+export async function load({ params, setHeaders }) {
 	// See post data model: https://ghost.org/docs/content-api/#posts
 	const post = await ghostClient.posts.read({ slug: params.slug }, { formats: ['html'] }).catch((e: any) => {
 		const status = [404, 422].includes(e.response?.status) ? 404 : 503;
 		error(status, e.message);
+	});
+
+	setHeaders({
+		'cache-control': 'public, max-age=1800' // 30 minutes: 30 * 60 = 1800
 	});
 
 	return { post };
