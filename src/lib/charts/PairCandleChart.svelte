926df564919d6c4ccd7bc238e75d7802284dabd2
Ken Kunz <kennethkunz@gmail.com>
2025-03-10 18:33:55 -0500

    add v1 TradingView Lightweight candles chart

---
+++ src/lib/charts/PairCandleChart.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import type { CandlestickData } from 'lightweight-charts';
+	import { createChart, CandlestickSeries } from 'lightweight-charts';
+
+	type Props = {
+		candles: CandlestickData[];
+	};
+
+	let { candles }: Props = $props();
+
+	function initChart(node: HTMLElement) {
+		const chart = createChart(node);
+
+		const candlestickSeries = chart.addSeries(CandlestickSeries, {
+			upColor: '#26a69a',
+			downColor: '#ef5350',
+			wickUpColor: '#26a69a',
+			wickDownColor: '#ef5350',
+			borderVisible: false
+		});
+
+		candlestickSeries.setData(candles);
+
+		chart.timeScale().fitContent();
+	}
+</script>
+
+<div class="chart-container" use:initChart></div>
+
+<style>
+	.chart-container {
+		height: 500px;
+	}
+</style>
