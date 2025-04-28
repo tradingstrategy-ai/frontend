1bc9c730a3b0742c5ac23e8cc7bc2dd196ef469d
Ken Kunz <kennethkunz@gmail.com>
2025-04-28 17:10:03 -0500

    add captcha server validation to subscribe action

--- src/routes/newsletter/+page.server.ts
+++ src/routes/newsletter/+page.server.ts
@@ -1,35 +1,53 @@
-import { fail } from '@sveltejs/kit';
-import type { Subscriber } from '$lib/newsletter/client';
+import type { Actions } from './$types';
+import { env } from '$env/dynamic/private';
+import { fail, isActionFailure } from '@sveltejs/kit';
 import { getClient } from '$lib/newsletter/client';
+
+async function verifyCaptcha(fetch: Fetch, data: FormData, headers: Headers) {
+	const url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify';
+
+	const body = new URLSearchParams({
+		secret: env.TS_PRIVATE_TURNSTILE_SECRET_KEY,
+		response: data.get('cf-turnstile-response') as string,
+		remoteip: headers.get('CF-Connecting-IP') ?? '',
+		idempotency_key: crypto.randomUUID()
+	});
+
+	const resp = await fetch(url, { method: 'POST', body });
+	const respPayload = await resp.json();
+
+	if (!respPayload.success) {
+		return fail(400, { message: 'CAPTCHA verification failed' });
+	}
+
+	return { success: true };
+}
+
+async function subscribeTosNewsletter(fetch: Fetch, data: FormData, headers: Headers) {
+	const newsletter = getClient(fetch);
+
+	const resp = await newsletter.subscribe({
+		email: data.get('email') as string,
+		ip_address: headers.get('CF-Connecting-IP'),
+		fields: {
+			country: headers.get('CF-IPCountry')
+		}
+	});
+
+	if (!resp.ok) return fail(resp.status, resp.payload);
+
+	return { success: true };
+}
 
 export const actions = {
 	subscribe: async ({ request, fetch }) => {
-		const newsletter = getClient(fetch);
-
 		const data = await request.formData();
 		const headers = request.headers;
-		const email = data.get('email');
 
-		if (!email) {
-			return fail(400, { message: 'email is required' });
-		}
+		const captchaResult = await verifyCaptcha(fetch, data, headers);
 
-		// include IP address and IP country from Cloudflare headers if available:
-		// https://developers.cloudflare.com/fundamentals/get-started/reference/http-request-headers
-		const subscriber = {
-			email,
-			ip_address: headers.get('CF-Connecting-IP'),
-			fields: {
-				country: headers.get('CF-IPCountry')
-			}
-		} as Subscriber;
+		if (isActionFailure(captchaResult)) return captchaResult;
 
-		const resp = await newsletter.subscribe(subscriber);
-
-		if (!resp.ok) {
-			return fail(resp.status, resp.payload);
-		}
-
-		return { success: true };
+		return subscribeTosNewsletter(fetch, data, headers);
 	}
-};
+} satisfies Actions;
