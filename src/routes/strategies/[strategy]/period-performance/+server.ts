a453d3e701fdba4cce7ade25feb1a48111f35697
Ken Kunz <kennethkunz@gmail.com>
2025-07-08 16:41:53 -0500

    Use share price data on period-performance endpoint

--- src/routes/strategies/[strategy]/period-performance/+server.ts
+++ src/routes/strategies/[strategy]/period-performance/+server.ts
@@ -1,39 +1,42 @@
+import { error, json } from '@sveltejs/kit';
 import { dateToTs, timeBucketToIntervalParts } from '$lib/charts/helpers';
-import { parseDate } from '$lib/helpers/date.js';
+import { getDateParam } from '$lib/helpers/url-params.js';
 import { relativeReturn, annualizedReturn } from '$lib/helpers/financial.js';
 import { timeBucketEnum } from '$lib/schemas/utility.js';
-import { error, json } from '@sveltejs/kit';
-import { getStrategyInfo } from 'trade-executor/client/strategy-info.js';
+import { fetchChartData } from 'trade-executor/client/chart.js';
 import { configuredStrategies } from 'trade-executor/schemas/configuration';
 
 const periodTimeBuckets = timeBucketEnum.exclude(['1m', '5m', '15m']).options;
 
 type PeriodTimeBucket = (typeof periodTimeBuckets)[number];
 
-// return an array of period performance summaries
+/**
+ * Return an array of period performance summaries
+ */
 export async function GET({ fetch, params, url }) {
-	const strategyConf = configuredStrategies.get(params.strategy);
-	if (!strategyConf) error(404, 'Not found');
+	// verify strategy is configured
+	const strategy = configuredStrategies.get(params.strategy);
+	if (!strategy) error(404, 'Not found');
 
-	const strategy = await getStrategyInfo(fetch, strategyConf);
+	// parse `end` param, default to current date
+	const endDate = getDateParam(url.searchParams, 'end') ?? new Date();
 
-	// parse `end` param (if provided) and set `endDate` to provided value or current date
-	const end = url.searchParams.get('end');
-	const endDate = end ? parseDate(end) : new Date();
-
-	// return error if invalid `end` param supplied
-	if (!endDate) {
-		error(400, 'param `end` must be a valid date string or timestamp (or omitted for current date)');
-	}
+	// get performance data
+	const { data } = await fetchChartData(fetch, strategy.url, {
+		source: 'live_trading',
+		type: strategy.useSharePrice ? 'share_price_based_return' : 'compounding_unrealised_trading_profitability_sampled'
+	});
 
 	// get performance summaries for specified time buckets
-	const data = strategy.summary_statistics?.compounding_unrealised_trading_profitability ?? [];
 	const performanceSummaries = periodTimeBuckets.map((timeBucket) => getPerformanceSummary(data, endDate, timeBucket));
 
+	// return JSON response object
 	return json(performanceSummaries);
 }
 
-// check performance of strategy over time interval
+/**
+ * Summarize performance of strategy for a given time interval
+ */
 function getPerformanceSummary(data: [number, number][], end: Date, timeBucket: PeriodTimeBucket) {
 	const [interval, duration] = timeBucketToIntervalParts(timeBucket);
 	const start = interval.offset(end, -duration);
