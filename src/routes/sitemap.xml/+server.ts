b853ca3b8965388dc10d911508ef53699c23c05e
Ken Kunz <kennethkunz@gmail.com>
2025-07-11 17:51:10 -0500

    Fix pair sitemap page count issue

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,6 +1,7 @@
 /**
  * Generate sitemap index with entries for all the sitemaps
  */
+import { backendUrl } from '$lib/config';
 import { fetchPublicApi } from '$lib/helpers/public-api';
 import { SitemapIndexStream } from 'sitemap';
 import { Readable } from 'stream';
@@ -29,8 +30,21 @@ const baseSitemaps = [
 async function getPairSitemaps(fetch: Fetch) {
 	const datasets = await fetchPublicApi(fetch, 'datasets');
 	const pairUniverse = datasets.find((d: any) => d.designation === 'pair_universe');
-	const pages = Math.ceil(pairUniverse.entries / PAIR_SITEMAP_PAGE_SIZE);
-	return Array.from({ length: pages }).map((_, page) => `api/sitemap/pairs/paged/${page}.xml`);
+	let pageCount = Math.ceil(pairUniverse.entries / PAIR_SITEMAP_PAGE_SIZE);
+
+	// confirm whether last pair sitemap page exists (since the counts can be off)
+	const lastPageExists = await sitemapExists(`${backendUrl}/sitemap/pairs/paged/${pageCount - 1}.xml`);
+	if (!lastPageExists) pageCount--;
+
+	return Array.from({ length: pageCount }).map((_, page) => `api/sitemap/pairs/paged/${page}.xml`);
+}
+
+async function sitemapExists(sitemapUrl) {
+	try {
+		return (await fetch(sitemapUrl, { method: 'HEAD' })).ok;
+	} catch {
+		return false;
+	}
 }
 
 export async function GET({ fetch, setHeaders, url }) {
