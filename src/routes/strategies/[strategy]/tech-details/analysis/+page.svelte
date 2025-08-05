aaae05dc752a89f0183a3d6ea0d9e2d7cf700cee
Ken Kunz <kennethkunz@gmail.com>
2025-08-05 16:04:27 -0500

    Handle analysis content types and errors

--- src/routes/strategies/[strategy]/tech-details/analysis/+page.svelte
+++ src/routes/strategies/[strategy]/tech-details/analysis/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import Alert from '$lib/components/Alert.svelte';
+	import Spinner from '$lib/components/Spinner.svelte';
 
 	let { data } = $props();
-	let { strategy, chartRegistrations, chartId, content } = $derived(data);
+	let { strategy, chartRegistrations, chartId, contentPromise } = $derived(data);
 
 	function handleAnalysisChange(this: HTMLSelectElement) {
 		goto(`?chart_id=${this.value}`, { noScroll: true });
@@ -23,17 +25,27 @@
 			{/each}
 		</select>
 	</p>
-	<p>
-		{#await content}
-			Loading…
-		{:then imageUrl}
-			{#if imageUrl}
-				<img src={imageUrl} alt="Analysis content" />
+
+	<div class="content">
+		{#await contentPromise}
+			<div class="loading">
+				<Spinner size="60" />
+			</div>
+		{:then content}
+			{#if content?.type === 'image/png'}
+				<img src={URL.createObjectURL(content.data)} alt="Analysis content" />
+			{:else if content?.type === 'text/html'}
+				<!--  eslint-disable-next-line svelte/no-at-html-tags -->
+				{@html content.data}
 			{:else}
 				Nothing to display
 			{/if}
+		{:catch error}
+			<Alert size="md" status="error" title="Error loading analysis">
+				<pre class="error-detail">{error}</pre>
+			</Alert>
 		{/await}
-	</p>
+	</div>
 </section>
 
 <style>
@@ -42,6 +54,21 @@
 			border-radius: var(--radius-sm);
 			padding: 0.5rem;
 			margin-bottom: 1rem;
+		}
+
+		.content {
+			min-height: 500px;
+		}
+
+		.loading {
+			display: grid;
+			place-content: center;
+			min-height: inherit;
+		}
+
+		.error-detail {
+			white-space: pre-wrap;
+			font: var(--f-code-md-regular);
 		}
 	}
 </style>
--- src/routes/strategies/[strategy]/tech-details/analysis/+page.ts
+++ src/routes/strategies/[strategy]/tech-details/analysis/+page.ts
@@ -1,7 +1,33 @@
-async function fetchAnalysis(fetch: Fetch, strategyUrl: string, chartId: string) {
+// Use discriminated union so data type is correctly inferred
+type AnalysisContent =
+	| {
+			type: 'image/png';
+			data: Blob;
+	  }
+	| {
+			type: 'text/html';
+			data: string;
+	  };
+
+async function fetchAnalysisContent(fetch: Fetch, strategyUrl: string, chartId: string): Promise<AnalysisContent> {
 	const response = await fetch(`${strategyUrl}/chart-registry/render?chart_id=${chartId}`);
-	const blob = await response.blob();
-	return URL.createObjectURL(blob);
+	const type = response.headers.get('content-type') ?? 'unknown';
+
+	if (type.startsWith('image/png')) {
+		return {
+			type: 'image/png',
+			data: await response.blob()
+		};
+	}
+
+	if (type.startsWith('text/html')) {
+		return {
+			type: 'text/html',
+			data: await response.text()
+		};
+	}
+
+	throw new Error(await response.text());
 }
 
 export async function load({ fetch, parent, url }) {
@@ -12,7 +38,7 @@ export async function load({ fetch, parent, url }) {
 		chartId = undefined;
 	}
 
-	const content = chartId ? fetchAnalysis(fetch, strategy.url, chartId) : undefined;
+	const contentPromise = chartId ? fetchAnalysisContent(fetch, strategy.url, chartId) : undefined;
 
-	return { chartRegistrations, chartId, content };
+	return { chartRegistrations, chartId, contentPromise };
 }
