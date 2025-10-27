770986c99dd65c0a37873e86fe06c81dbc03a9ea
Ken Kunz <kennethkunz@gmail.com>
2025-10-27 00:48:34 -0500

    Convert top vaults table to DataTable

---
+++ src/lib/top-vaults/LastDepositCell.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import Timestamp from '$lib/components/Timestamp.svelte';
+	import Tooltip from '$lib/components/Tooltip.svelte';
+
+	interface Props {
+		last_deposit: string;
+	}
+
+	let { last_deposit }: Props = $props();
+</script>
+
+<div class="last-deposit">
+	<Tooltip>
+		<Timestamp slot="trigger" date={last_deposit} relative={{ strict: true, unit: 'day' }}>
+			{#snippet children({ relativeStr })}
+				{relativeStr.replace(' days', 'd')}
+			{/snippet}
+		</Timestamp>
+		<span slot="popup">
+			{last_deposit.replace('T', ' ')} UTC
+		</span>
+	</Tooltip>
+</div>
+
+<style>
+	.last-deposit {
+		display: contents;
+
+		:global(.popup) {
+			white-space: nowrap;
+			left: -100%;
+		}
+	}
+</style>
--- src/lib/top-vaults/TopVaultsTable.svelte
+++ src/lib/top-vaults/TopVaultsTable.svelte
@@ -1,66 +1,174 @@
 <script lang="ts">
-	import type { ApiChain } from '$lib/helpers/chain';
 	import type { TopVaults } from './schemas';
+	import { type ApiChain, getChain, getExplorerUrl } from '$lib/helpers/chain';
 	import Alert from '$lib/components/Alert.svelte';
+	import CryptoAddressWidget from '$lib/components/CryptoAddressWidget.svelte';
 	import Timestamp from '$lib/components/Timestamp.svelte';
-	import TopVaultsRow from './TopVaultsRow.svelte';
+	import DataTable from '$lib/components/datatable/DataTable.svelte';
 	import TopVaultsOptIn from './TopVaultsOptIn.svelte';
+	import VaultCell from './VaultCell.svelte';
+	import FeesCell from './FeesCell.svelte';
+	import LastDepositCell from './LastDepositCell.svelte';
+	import { readable } from 'svelte/store';
+	import { createRender, createTable } from 'svelte-headless-table';
+	import { formatAmount, formatDollar, formatNumber, formatPercent, formatValue } from '$lib/helpers/formatters';
 
 	interface Props {
 		topVaults: TopVaults;
-		chain?: ApiChain;
+		apiChain?: ApiChain;
 	}
 
-	const { topVaults, chain }: Props = $props();
+	const { topVaults, apiChain }: Props = $props();
+
+	const vaultsStore = readable(topVaults.vaults.map((row, index) => ({ ...row, index })));
+
+	const table = createTable(vaultsStore);
+
+	const tableColumns = table.createColumns([
+		table.column({
+			accessor: 'index',
+			header: ''
+		}),
+		table.column({
+			id: 'vault',
+			header: 'Vault',
+			accessor: ({ chain: chainId, name, protocol }) => {
+				const chain = getChain(chainId);
+				return {
+					showChain: !apiChain,
+					chainId,
+					chain,
+					name,
+					protocol,
+					// accessor.toString() used when searching table
+					toString: () => `${chainId} ${chain?.name} ${name} ${protocol}`
+				};
+			},
+			cell: ({ value }) => createRender(VaultCell, value)
+		}),
+		table.column({
+			accessor: 'current_tvl_usd',
+			header: 'Current TVL (USD)',
+			cell: ({ value }) => formatDollar(value, 2, 2)
+		}),
+		table.column({
+			id: 'return_ann_1m',
+			accessor: '1m_return_ann',
+			header: '1M return (ann.)',
+			cell: ({ value }) => formatPercent(value, 2)
+		}),
+		table.column({
+			id: 'return_1m',
+			accessor: '1m_return',
+			header: '1M return',
+			cell: ({ value }) => formatPercent(value, 2)
+		}),
+		table.column({
+			id: 'return_ann_3m',
+			accessor: '3m_return_ann',
+			header: '3M return (ann.)',
+			cell: ({ value }) => formatPercent(value, 2)
+		}),
+		table.column({
+			id: 'return_3m',
+			accessor: '3m_return',
+			header: '3M return',
+			cell: ({ value }) => formatPercent(value, 2)
+		}),
+		table.column({
+			id: 'sharpe_3m',
+			accessor: '3m_sharpe',
+			header: '3M Sharpe',
+			cell: ({ value }) => formatNumber(value, 1)
+		}),
+		table.column({
+			id: 'volatility_3m',
+			accessor: '3m_volatility',
+			header: '3M vola-tility',
+			cell: ({ value }) => formatPercent(value)
+		}),
+		table.column({
+			accessor: 'lifetime_return_ann',
+			header: 'Lifetime return (ann.)',
+			cell: ({ value }) => formatPercent(value, 2)
+		}),
+		table.column({
+			accessor: 'lifetime_return',
+			header: 'Lifetime return',
+			cell: ({ value }) => formatPercent(value, 2)
+		}),
+		table.column({
+			accessor: 'age_years',
+			header: 'Age (years)',
+			cell: ({ value }) => formatNumber(value, 2)
+		}),
+		table.column({
+			header: 'Last deposit',
+			accessor: 'last_deposit',
+			cell: ({ value }) => createRender(LastDepositCell, { last_deposit: value })
+		}),
+		table.column({
+			accessor: 'denomination',
+			header: 'Denom-ination',
+			cell: ({ value }) => formatValue(value)
+		}),
+		table.column({
+			accessor: 'peak_tvl_usd',
+			header: 'Peak TVL (USD)',
+			cell: ({ value }) => formatDollar(value, 2)
+		}),
+		table.column({
+			accessor: 'deposit_redeem_count',
+			header: 'Deposits & Redeems', // NOTE: non-breaking space after "&"
+			cell: ({ value }) => formatAmount(value)
+		}),
+		table.column({
+			id: 'fees',
+			header: 'Fees',
+			accessor: ({ management_fee, performance_fee }) => ({ management_fee, performance_fee }),
+			cell: ({ value }) => createRender(FeesCell, value)
+		}),
+		table.column({
+			id: 'address',
+			header: 'Vault address',
+			accessor: ({ address, chain: chainId }) => ({
+				address,
+				chainId,
+				// accessor.toString() used when searching table
+				toString: () => address
+			}),
+			cell: ({ value }) =>
+				createRender(CryptoAddressWidget, {
+					class: 'vault-address',
+					size: 'sm',
+					address: value.address,
+					href: getExplorerUrl(getChain(value.chainId), value.address)
+				})
+		})
+	]);
+
+	const tableViewModel = table.createViewModel(tableColumns);
 </script>
 
-<div class="top-vaults-table">
+<div class="top-vaults">
 	<TopVaultsOptIn />
 
 	{#if !topVaults.vaults.length}
 		<Alert title="Error">No vault data available.</Alert>
 	{:else}
 		<div class="table-meta">
-			<span>{topVaults.vaults.length} {chain ? chain.chain_name : 'total'} vaults</span>
+			<span>{topVaults.vaults.length} {apiChain ? apiChain.chain_name : 'total'} vaults</span>
 			<span>Updated <Timestamp date={topVaults.generated_at} relative /></span>
 		</div>
 
 		<div class="table-wrapper">
-			<table>
-				<thead>
-					<tr>
-						<th></th>
-						<th class="name">Vault</th>
-						<th>Current TVL (USD)</th>
-						<th>1M return (ann.)</th>
-						<th>1M return</th>
-						<th>3M return (ann.)</th>
-						<th>3M return</th>
-						<th>3M Sharpe</th>
-						<th>3M volatility</th>
-						<th>Lifetime return (ann.)</th>
-						<th>Lifetime return</th>
-						<th>Age (years)</th>
-						<th>Last deposit</th>
-						<th>Denom-ination</th>
-						<th>Peak TVL (USD)</th>
-						<th>Deposits &&nbsp;Redeems</th>
-						<th>Fees</th>
-						<th>Vault address</th>
-					</tr>
-				</thead>
-				<tbody>
-					{#each topVaults.vaults as vault, idx (vault.id)}
-						<TopVaultsRow {vault} index={idx + 1} showChain={!chain} />
-					{/each}
-				</tbody>
-			</table>
+			<DataTable class="top-vaults-table" {tableViewModel} />
 		</div>
 	{/if}
 </div>
 
 <style>
-	.top-vaults-table {
+	.top-vaults {
 		display: grid;
 		gap: 1rem;
 
@@ -83,7 +191,7 @@
 			overflow-x: auto;
 		}
 
-		table {
+		:global(.top-vaults-table) {
 			width: 100%;
 			border-collapse: collapse;
 			min-width: 75rem;
@@ -118,21 +226,69 @@
 				}
 			}
 
-			th {
+			:global(td:has(.tooltip)) {
+				position: relative;
+			}
+
+			:global(th) {
 				background: var(--c-box-4);
 				border-bottom: 3px solid var(--c-text-extra-light);
 				padding: 0.5rem;
 				font-weight: 900;
 				text-transform: uppercase;
 				text-align: left;
-
-				&:first-child {
-					background: none;
-				}
 			}
 
-			.name {
+			:global(th.vault) {
 				text-indent: 1.875rem;
+			}
+
+			:global(.multiline) {
+				display: grid;
+				gap: 0.125rem;
+			}
+
+			:global(.index) {
+				text-align: center;
+				vertical-align: middle;
+			}
+
+			:global(th.index) {
+				background: none;
+			}
+
+			:global(.vault-address) {
+				min-width: 8rem;
+				padding: 0;
+				background: none;
+				font: inherit;
+				letter-spacing: inherit;
+				border-radius: 0;
+			}
+
+			:global(
+				:is(
+					td.current_tvl_usd,
+					td.return_ann_1m,
+					td.return_1m,
+					td.return_ann_3m,
+					td.return_3m,
+					td.sharpe_3m,
+					td.volatility_3m,
+					td.lifetime_return_ann,
+					td.lifetime_return,
+					td.age_years,
+					td.last_deposit,
+					td.peak_tvl_usd,
+					td.deposit_redeem_count,
+					td.fees
+				)
+			) {
+				text-align: right;
+			}
+
+			:global(td.denomination) {
+				text-align: center;
 			}
 		}
 	}
