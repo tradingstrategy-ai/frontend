8094af9f9b2b6992fba57c48750c1b0cc27e9e94
Ken Kunz <kennethkunz@gmail.com>
2025-11-03 18:16:35 -0600

    Add chain logo to chain top vaults page header

--- src/routes/trading-view/[chain=slug]/vaults/+page.svelte
+++ src/routes/trading-view/[chain=slug]/vaults/+page.svelte
@@ -3,6 +3,7 @@
 	import DataBadge from '$lib/components/DataBadge.svelte';
 	import HeroBanner from '$lib/components/HeroBanner.svelte';
 	import Section from '$lib/components/Section.svelte';
+	import { getLogoUrl } from '$lib/helpers/assets';
 	import TopVaultsTable from '$lib/top-vaults/TopVaultsTable.svelte';
 
 	const { data } = $props();
@@ -21,8 +22,11 @@
 	<Section tag="header">
 		<HeroBanner subtitle="The best performing DeFi vaults on {chainName} with minimum $50k USD TVL">
 			{#snippet title()}
-				<span>Top {chainName} Vaults</span>
-				<DataBadge class="badge" status="warning">Beta</DataBadge>
+				<span>
+					<img src={getLogoUrl('blockchain', chain.chain_slug)} alt={chain.chain_name} />
+					<span>Top {chainName} Vaults</span>
+					<DataBadge class="badge" status="warning">Beta</DataBadge>
+				</span>
 			{/snippet}
 		</HeroBanner>
 	</Section>
@@ -34,10 +38,19 @@
 
 <style>
 	.chain-vaults {
+		span {
+			display: inline-flex;
+			flex-wrap: wrap;
+			gap: 0.25em;
+			align-items: center;
+		}
+
+		img {
+			height: 1em;
+		}
+
 		:global(.badge) {
 			font-size: 0.5em;
-			margin-inline: 0.25em;
-			transform: translate(0, -0.375em);
 		}
 	}
 </style>
