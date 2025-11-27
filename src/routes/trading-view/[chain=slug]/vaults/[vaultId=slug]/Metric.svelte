2a3948c5d3b62df0aa1c0faac9f9749a21b02703
Ken Kunz <kennethkunz@gmail.com>
2025-11-26 18:53:17 -0600

    Address CSS build warning

--- src/routes/trading-view/[chain=slug]/vaults/[vaultId=slug]/Metric.svelte
+++ src/routes/trading-view/[chain=slug]/vaults/[vaultId=slug]/Metric.svelte
@@ -29,40 +29,34 @@
 		display: grid;
 		align-content: start;
 
+		--label-font: var(--f-ui-md-medium);
+		--value-font: var(--f-heading-lg-medium);
+		&.lg {
+			--value-font: var(--f-heading-xl-medium);
+		}
+		&.xl {
+			--value-font: var(--f-heading-xxl-medium);
+		}
+
+		@media (--viewport-sm-down) {
+			--label-font: var(--f-ui-sm-medium);
+			--value-font: var(--f-heading-md-medium);
+			&.lg {
+				--value-font: var(--f-heading-lg-medium);
+			}
+			&.xl {
+				--value-font: var(--f-heading-xl-medium);
+			}
+		}
+
 		.label {
 			margin-bottom: 0.5rem;
-			font: var(--f-ui-md-medium);
+			font: var(--label-font);
 			color: var(--c-text-extra-light);
 		}
 
 		.value {
-			font: var(--f-heading-lg-medium);
-
-			.lg & {
-				font: var(--f-heading-xl-medium);
-			}
-
-			.xl & {
-				font: var(--f-heading-xxl-medium);
-			}
-		}
-
-		@media (--viewport-sm-down) {
-			.label {
-				font: var(--f-ui-sm-medium);
-			}
-
-			.value {
-				font: var(--f-heading-md-medium);
-
-				.lg & {
-					font: var(--f-heading-lg-medium);
-				}
-
-				.xl & {
-					font: var(--f-heading-xl-medium);
-				}
-			}
+			font: var(--value-font);
 		}
 	}
 </style>
