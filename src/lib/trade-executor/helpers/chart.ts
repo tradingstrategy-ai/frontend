2123dc2fcbaa4018bbab8bf4163bfb050e8978b8
Mikko Ohtamaa <mikko@redinnovation.com>
2026-03-14 19:33:48 +0100

    Use share price for strategy chart ranges

--- src/lib/trade-executor/helpers/chart.ts
+++ src/lib/trade-executor/helpers/chart.ts
@@ -6,8 +6,11 @@ import { tsToDate } from '$lib/charts/helpers';
 // Determine the chart min/max dates from all strategies to standardize x-axis range
 export function getStrategyChartDateRange(strategies: StrategyInfo[]): [Date, Date] {
 	// get the latest final date from all strategies
-	const latestEndTs = max(strategies, ({ summary_statistics }) => {
-		return summary_statistics?.compounding_unrealised_trading_profitability?.at?.(-1)?.[0];
+	const latestEndTs = max(strategies, ({ useSharePrice, summary_statistics }) => {
+		const chartSeries = useSharePrice
+			? summary_statistics?.share_price_returns_90_days
+			: summary_statistics?.compounding_unrealised_trading_profitability;
+		return chartSeries?.at?.(-1)?.[0];
 	});
 
 	// beginning of day (UTC) of the last end date (or current time)
